9d89c6c1ab43eaab768bc0c1a66737bef9f512fb
TomPallister <TomPallister@users.noreply.github.com>
2016-10-12 21:40:42 +0100

    updated scripts

--- run-benchmarks.bat
+++ run-benchmarks.bat
@@ -1,4 +1,3 @@
- #!/bin/bash 
 echo -------------------------
 
 echo Running Ocelot.Benchmarks
---
+++ run-tests.bat
@@ -0,0 +1,13 @@
+echo -------------------------
+
+echo Running Ocelot.UnitTests
+
+dotnet restore test/Ocelot.UnitTests/
+dotnet test test/Ocelot.UnitTests/
+
+echo Running Ocelot.AcceptanceTests
+
+cd test/Ocelot.AcceptanceTests/
+dotnet restore 
+dotnet test 
+cd ../../
