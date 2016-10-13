17b24d3da505fc6dd652e10a50c5a0a309e97902
TomPallister <TomPallister@users.noreply.github.com>
2016-10-13 20:40:03 +0100

    changes to get ftp deploy working from appveyor

--- build.bat
+++ build.bat
@@ -17,6 +17,6 @@ echo Building Ocelot
 dotnet restore src/Ocelot
 dotnet restore src/Ocelot.Library
 dotnet build src/Ocelot
-dotnet publish src/Ocelot -o artifacts/Ocelot
+dotnet publish src/Ocelot -o site/wwwroot
 
 
