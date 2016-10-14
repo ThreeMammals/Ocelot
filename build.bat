67af8841b20e836ceed9ca22ddb6798790f9fe69
TomPallister <TomPallister@users.noreply.github.com>
2016-10-14 20:20:03 +0100

    updated build scriptos

--- build.bat
+++ build.bat
@@ -5,7 +5,6 @@ echo Building Ocelot
 dotnet restore src/Ocelot
 dotnet restore src/Ocelot.Library
 dotnet build src/Ocelot
-dotnet pack src/Ocelot/project.json --no-build --output nupkgs
-dotnet publish src/Ocelot -o site/wwwroot
+dotnet publish src/Ocelot -o artifacts/
 
 
