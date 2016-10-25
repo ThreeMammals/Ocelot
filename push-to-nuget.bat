385aea8fdd891b8eeb1ff5f31fa8b1ef9442ed04
TomPallister <TomPallister@users.noreply.github.com>
2016-10-25 21:00:11 +0100

    ooops

--- push-to-nuget.bat
+++ push-to-nuget.bat
@@ -4,7 +4,7 @@ echo Packing Ocelot Version %1
 nuget pack .\Ocelot.nuspec -version %1
 
 echo Publishing Ocelot
- nuget push Ocelot.%1.nupkg -ApiKey adc6c39d-ae94-496c-823e-b96f64ee23ff -Source https://www.nuget.org/api/v2/package
+ nuget push Ocelot.%1.nupkg -ApiKey %2 -Source https://www.nuget.org/api/v2/package
 
 
 
