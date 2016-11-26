536db480499ea6ed2e2b199ca50cf9019147178b
TomPallister <TomPallister@users.noreply.github.com>
2016-11-26 14:08:09 +0000

    fixed problems where routes were not mathing

---
+++ src/Ocelot/Utilities/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Ocelot.Utilities
+{
+    public static class StringExtensions
+    {
+        public static string SetLastCharacterAs(this string valueToSetLastChar, 
+            char expectedLastChar)
+        {
+            var last = valueToSetLastChar[valueToSetLastChar.Length - 1];
+
+            if (last != expectedLastChar)
+            {
+                valueToSetLastChar = $"{valueToSetLastChar}{expectedLastChar}";
+            }
+            return valueToSetLastChar;
+        }
+    }
+}
