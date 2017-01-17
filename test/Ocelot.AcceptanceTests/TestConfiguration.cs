a8878b4a198a4fcd5658c23f07dca14f895d9605
Tom Gardham-Pallister <tom.pallister@fourth.com>
2017-01-17 10:20:49 +0000

    test passing on mac and windows

--- test/Ocelot.AcceptanceTests/TestConfiguration.cs
+++ test/Ocelot.AcceptanceTests/TestConfiguration.cs
@@ -1,8 +1,34 @@
 ﻿namespace Ocelot.AcceptanceTests
 {
+    using System.Runtime.InteropServices;
+
     public static class TestConfiguration
     {
         public static double Version => 1.4;
-        public static string ConfigurationPath => $"./bin/Debug/netcoreapp{Version}/win10-x64/configuration.json";
+        //public static string Runtime => "win10-x64";
+        public static string ConfigurationPath => GetConfigurationPath();
+
+        public static string GetConfigurationPath()
+        {
+            var osArchitecture = RuntimeInformation.OSArchitecture.ToString();
+            
+            var oSDescription = string.Empty;
+
+            if(RuntimeInformation.OSDescription.ToLower().Contains("darwin"))
+            {
+                oSDescription = "osx.10.11";
+            }
+
+            if(RuntimeInformation.OSDescription.ToLower().Contains("windows"))
+            {
+                oSDescription = "win10";
+            }
+
+            var runTime = $"{oSDescription}-{osArchitecture}".ToLower();
+
+            var configPath = $"./bin/Debug/netcoreapp{Version}/{runTime}/configuration.json";
+
+            return configPath;
+        }
     }
 }
