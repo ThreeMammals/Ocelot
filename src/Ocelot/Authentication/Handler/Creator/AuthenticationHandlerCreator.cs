1c98dcbca3a9326c995c757c37e13071fa502782
Tom Pallister <Tom.Pallister@moonpig.com>
2017-09-25 11:53:56 +0100

    updated packages and project versions...12 errors!

--- src/Ocelot/Authentication/Handler/Creator/AuthenticationHandlerCreator.cs
+++ src/Ocelot/Authentication/Handler/Creator/AuthenticationHandlerCreator.cs
@@ -1,5 +1,6 @@
 using System;
 using IdentityServer4.AccessTokenValidation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Ocelot.Responses;
