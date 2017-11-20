6c5c6495c09e91f4d0933f9a55a8156d9367e479
hemantkd <10883076+hemantkd@users.noreply.github.com>
2017-11-20 12:41:45 +0000

    Initial draft for the issue 147 fix (#155)
    * Initial draft for the issue 147 fix
    
    * Added unit tests for scenarios for getting and setting file configuration if the environment name is unavailable.

--- test/Ocelot.UnitTests/Configuration/FileConfigurationRepositoryTests.cs
+++ test/Ocelot.UnitTests/Configuration/FileConfigurationRepositoryTests.cs
@@ -1,56 +1,35 @@
 using System;
 using System.Collections.Generic;
 using Moq;
-using Ocelot.Configuration;
 using Ocelot.Configuration.File;
-using Ocelot.Responses;
 using Shouldly;
 using TestStack.BDDfy;
 using Xunit;
 using Newtonsoft.Json;
 using System.IO;
+using Microsoft.AspNetCore.Hosting;
 using Ocelot.Configuration.Repository;
 
 namespace Ocelot.UnitTests.Configuration
 {
     public class FileConfigurationRepositoryTests
     {
-        private readonly IFileConfigurationRepository _repo;
+        private readonly Mock<IHostingEnvironment> _hostingEnvironment = new Mock<IHostingEnvironment>();
+        private IFileConfigurationRepository _repo;
         private FileConfiguration _result;
         private FileConfiguration _fileConfiguration;
+        private string _environmentName = "DEV";
 
         public FileConfigurationRepositoryTests()
         {
-            _repo = new FileConfigurationRepository();
+            _hostingEnvironment.Setup(he => he.EnvironmentName).Returns(_environmentName);
+            _repo = new FileConfigurationRepository(_hostingEnvironment.Object);
         }
 
         [Fact]
         public void should_return_file_configuration()
         {
-            var reRoutes = new List<FileReRoute>
-            {
-                new FileReRoute
-                {
-                    DownstreamHost = "localhost",
-                    DownstreamPort = 80,
-                    DownstreamScheme = "https",
-                    DownstreamPathTemplate = "/test/test/{test}"
-                }
-            };
-
-            var globalConfiguration = new FileGlobalConfiguration
-            {
-                AdministrationPath = "testy",
-                ServiceDiscoveryProvider = new FileServiceDiscoveryProvider
-                {
-                    Port = 198,
-                    Host = "blah"
-                }
-            };
-
-            var config = new FileConfiguration();
-            config.GlobalConfiguration = globalConfiguration;
-            config.ReRoutes.AddRange(reRoutes);
+            var config = FakeFileConfigurationForGet();
 
             this.Given(x => x.GivenTheConfigurationIs(config))
                 .When(x => x.WhenIGetTheReRoutes())
@@ -59,37 +38,44 @@ namespace Ocelot.UnitTests.Configuration
         }
 
         [Fact]
+        public void should_return_file_configuration_if_environment_name_is_unavailable()
+        {
+            var config = FakeFileConfigurationForGet();
+
+            this.Given(x => x.GivenTheEnvironmentNameIsUnavailable())
+                .And(x => x.GivenTheConfigurationIs(config))
+                .When(x => x.WhenIGetTheReRoutes())
+                .Then(x => x.ThenTheFollowingIsReturned(config))
+                .BDDfy();
+        }
+
+        [Fact]
         public void should_set_file_configuration()
         {
-             var reRoutes = new List<FileReRoute>
-            {
-                new FileReRoute
-                {
-                    DownstreamHost = "123.12.12.12",
-                    DownstreamPort = 80,
-                    DownstreamScheme = "https",
-                    DownstreamPathTemplate = "/asdfs/test/{test}"
-                }
-            };
-
-            var globalConfiguration = new FileGlobalConfiguration
-            {
-                AdministrationPath = "asdas",
-                ServiceDiscoveryProvider = new FileServiceDiscoveryProvider
-                {
-                    Port = 198,
-                    Host = "blah"
-                }
-            };
-
-            var config = new FileConfiguration();
-            config.GlobalConfiguration = globalConfiguration;
-            config.ReRoutes.AddRange(reRoutes);
+            var config = FakeFileConfigurationForSet();
 
             this.Given(x => GivenIHaveAConfiguration(config))
                 .When(x => WhenISetTheConfiguration())
                 .Then(x => ThenTheConfigurationIsStoredAs(config))
                 .BDDfy();
+        }
+
+        [Fact]
+        public void should_set_file_configuration_if_environment_name_is_unavailable()
+        {
+            var config = FakeFileConfigurationForSet();
+            this.Given(x => GivenIHaveAConfiguration(config))
+                .And(x => GivenTheEnvironmentNameIsUnavailable())
+                .When(x => WhenISetTheConfiguration())
+                .Then(x => ThenTheConfigurationIsStoredAs(config))
+                .BDDfy();
+        }
+
+        private void GivenTheEnvironmentNameIsUnavailable()
+        {
+            _environmentName = null;
+            _hostingEnvironment.Setup(he => he.EnvironmentName).Returns(_environmentName);
+            _repo = new FileConfigurationRepository(_hostingEnvironment.Object);
         }
 
         private void GivenIHaveAConfiguration(FileConfiguration fileConfiguration)
@@ -119,9 +105,9 @@ namespace Ocelot.UnitTests.Configuration
             }
         }
 
-        private void GivenTheConfigurationIs(FileConfiguration fileConfiguration)
+        private void  GivenTheConfigurationIs(FileConfiguration fileConfiguration)
         {
-            var configurationPath = $"{AppContext.BaseDirectory}/configuration.json";
+            var configurationPath = $"{AppContext.BaseDirectory}/configuration{(string.IsNullOrEmpty(_environmentName) ? string.Empty : ".")}{_environmentName}.json";
 
             var jsonConfiguration = JsonConvert.SerializeObject(fileConfiguration);
 
@@ -152,6 +138,66 @@ namespace Ocelot.UnitTests.Configuration
                 _result.ReRoutes[i].DownstreamPort.ShouldBe(expected.ReRoutes[i].DownstreamPort);
                 _result.ReRoutes[i].DownstreamScheme.ShouldBe(expected.ReRoutes[i].DownstreamScheme);
             }
+        }
+
+        private FileConfiguration FakeFileConfigurationForSet()
+        {
+            var reRoutes = new List<FileReRoute>
+            {
+                new FileReRoute
+                {
+                    DownstreamHost = "123.12.12.12",
+                    DownstreamPort = 80,
+                    DownstreamScheme = "https",
+                    DownstreamPathTemplate = "/asdfs/test/{test}"
+                }
+            };
+
+            var globalConfiguration = new FileGlobalConfiguration
+            {
+                AdministrationPath = "asdas",
+                ServiceDiscoveryProvider = new FileServiceDiscoveryProvider
+                {
+                    Port = 198,
+                    Host = "blah"
+                }
+            };
+
+            return new FileConfiguration
+            {
+                GlobalConfiguration = globalConfiguration,
+                ReRoutes = reRoutes
+            };
+        }
+
+        private FileConfiguration FakeFileConfigurationForGet()
+        {
+            var reRoutes = new List<FileReRoute>
+            {
+                new FileReRoute
+                {
+                    DownstreamHost = "localhost",
+                    DownstreamPort = 80,
+                    DownstreamScheme = "https",
+                    DownstreamPathTemplate = "/test/test/{test}"
+                }
+            };
+
+            var globalConfiguration = new FileGlobalConfiguration
+            {
+                AdministrationPath = "testy",
+                ServiceDiscoveryProvider = new FileServiceDiscoveryProvider
+                {
+                    Port = 198,
+                    Host = "blah"
+                }
+            };
+
+            return new FileConfiguration
+            {
+                GlobalConfiguration = globalConfiguration,
+                ReRoutes = reRoutes
+            };
         }
     }
 }
