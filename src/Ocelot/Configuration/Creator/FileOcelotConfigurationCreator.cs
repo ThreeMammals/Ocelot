1d9f2611eb551caefdd0b4e7267aa20cd2737b57
Tom Gardham-Pallister <tom.pallister@fourth.com>
2017-11-22 08:42:40 +0000

    change config creator to not throw exception in there is an error......lord i hate this config creator code I need to sort it out.

--- src/Ocelot/Configuration/Creator/FileOcelotConfigurationCreator.cs
+++ src/Ocelot/Configuration/Creator/FileOcelotConfigurationCreator.cs
@@ -70,31 +70,22 @@ namespace Ocelot.Configuration.Creator
         public async Task<Response<IOcelotConfiguration>> Create()
         {     
             var config = await SetUpConfiguration(_options.Value);
-
-            return new OkResponse<IOcelotConfiguration>(config);
+            return config;
         }
 
         public async Task<Response<IOcelotConfiguration>> Create(FileConfiguration fileConfiguration)
         {     
             var config = await SetUpConfiguration(fileConfiguration);
-
-            return new OkResponse<IOcelotConfiguration>(config);
+            return config;
         }
 
-        private async Task<IOcelotConfiguration> SetUpConfiguration(FileConfiguration fileConfiguration)
+        private async Task<Response<IOcelotConfiguration>> SetUpConfiguration(FileConfiguration fileConfiguration)
         {
             var response = await _configurationValidator.IsValid(fileConfiguration);
 
             if (response.Data.IsError)
             {
-                var errorBuilder = new StringBuilder();
-
-                foreach (var error in response.Errors)
-                {
-                    errorBuilder.AppendLine(error.Message);
-                }
-
-                throw new Exception($"Unable to start Ocelot..configuration, errors were {errorBuilder}");
+                return new ErrorResponse<IOcelotConfiguration>(response.Data.Errors);
             }
 
             var reRoutes = new List<ReRoute>();
@@ -107,7 +98,9 @@ namespace Ocelot.Configuration.Creator
 
             var serviceProviderConfiguration = _serviceProviderConfigCreator.Create(fileConfiguration.GlobalConfiguration);
             
-            return new OcelotConfiguration(reRoutes, fileConfiguration.GlobalConfiguration.AdministrationPath, serviceProviderConfiguration);
+            var config = new OcelotConfiguration(reRoutes, fileConfiguration.GlobalConfiguration.AdministrationPath, serviceProviderConfiguration);
+
+            return new OkResponse<IOcelotConfiguration>(config);
         }
 
         private ReRoute SetUpReRoute(FileReRoute fileReRoute, FileGlobalConfiguration globalConfiguration)
