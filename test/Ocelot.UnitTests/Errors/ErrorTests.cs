e1010a7d5aa2e8b26c166a944312f640c3b0769a
Tom Gardham-Pallister <tom.pallister@fourth.com>
2017-11-24 20:35:11 +0000

    bs test for code coverage

---
+++ test/Ocelot.UnitTests/Errors/ErrorTests.cs
@@ -0,0 +1,18 @@
+using Ocelot.Errors;
+using Ocelot.Infrastructure.RequestData;
+using Shouldly;
+using Xunit;
+
+namespace Ocelot.UnitTests.Errors
+{
+    public class ErrorTests
+    {
+        [Fact]
+        public void should_return_message()
+        {
+            var error = new CannotAddDataError("message");
+            var result = error.ToString();
+            result.ShouldBe("message");
+        }
+    }
+}
