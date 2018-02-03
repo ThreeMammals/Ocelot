a4bb0dd13b3ab4c9664a8bff4566706572828b5b
Tom Pallister <TomPallister@users.noreply.github.com>
2018-02-03 09:02:01 +0000

    fix for #214 and some tests for the class it was in but not for the error (cant be arsed to test as have to spin up IIS) (#218)

---
+++ test/Ocelot.UnitTests/Responder/HttpContextResponderTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using Ocelot.Headers;
+using Ocelot.Responder;
+using Shouldly;
+using Xunit;
+
+namespace Ocelot.UnitTests.Responder
+{
+    public class HttpContextResponderTests
+    {
+        private readonly HttpContextResponder _responder;
+        private RemoveOutputHeaders _removeOutputHeaders;
+
+        public HttpContextResponderTests()
+        {
+            _removeOutputHeaders = new RemoveOutputHeaders();
+            _responder = new HttpContextResponder(_removeOutputHeaders);
+        }
+
+        [Fact]
+        public void should_remove_transfer_encoding_header()
+        {
+            var httpContext = new DefaultHttpContext();
+            var httpResponseMessage = new HttpResponseMessage {Content = new StringContent("")};
+            httpResponseMessage.Headers.Add("Transfer-Encoding", "woop");
+            _responder.SetResponseOnHttpContext(httpContext, httpResponseMessage).GetAwaiter().GetResult();
+            var header = httpContext.Response.Headers["Transfer-Encoding"];
+            header.ShouldBeEmpty();
+        }
+
+        [Fact]
+        public void should_have_content_length()
+        {
+            var httpContext = new DefaultHttpContext();
+            var httpResponseMessage = new HttpResponseMessage { Content = new StringContent("test") };
+            _responder.SetResponseOnHttpContext(httpContext, httpResponseMessage).GetAwaiter().GetResult();
+            var header = httpContext.Response.Headers["Content-Length"];
+            header.First().ShouldBe("4");
+        }
+
+        [Fact]
+        public void should_add_header()
+        {
+            var httpContext = new DefaultHttpContext();
+            var httpResponseMessage = new HttpResponseMessage { Content = new StringContent("test") };
+            httpResponseMessage.Headers.Add("test", "test");
+            _responder.SetResponseOnHttpContext(httpContext, httpResponseMessage).GetAwaiter().GetResult();
+            var header = httpContext.Response.Headers["test"];
+            header.First().ShouldBe("test");
+        }
+
+        [Fact]
+        public void should_call_without_exception()
+        {
+            var httpContext = new DefaultHttpContext();
+            _responder.SetErrorResponseOnContext(httpContext, 500);
+        }
+    }
+}
