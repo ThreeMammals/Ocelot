f3948922768dd400024e953bfffeceb600d1aceb
geffzhang <geffzhang@qq.com>
2018-02-10 13:06:45 +0800

    fix： Requester\HttpClientBuilder.cs(10,14): error CS0234: The type or namespace name 'Tracing' does not exist in the namespace

--- src/Ocelot/Requester/HttpClientBuilder.cs
+++ src/Ocelot/Requester/HttpClientBuilder.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Ocelot.Logging;
 using Ocelot.Requester.QoS;
-using Microsoft.Extensions.DependencyInjection;
-using Ocelot.Tracing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Ocelot.Requester
 {
