4313110ae2b929a23509d5222a7d338a39d06d45
Tom Gardham-Pallister <tom.pallister@fourth.com>
2018-02-19 18:38:16 +0000

    scoped data repo back in

--- src/Ocelot/Infrastructure/RequestData/IRequestScopedDataRepository.cs
+++ src/Ocelot/Infrastructure/RequestData/IRequestScopedDataRepository.cs
@@ -1,11 +1,11 @@
-// using Ocelot.Responses;
+using Ocelot.Responses;
 
-// namespace Ocelot.Infrastructure.RequestData
-// {
-//     public interface IRequestScopedDataRepository
-//     {
-//         Response Add<T>(string key, T value);
-//         Response Update<T>(string key, T value);
-//         Response<T> Get<T>(string key);
-//     }
-// }
+namespace Ocelot.Infrastructure.RequestData
+{
+    public interface IRequestScopedDataRepository
+    {
+        Response Add<T>(string key, T value);
+        Response Update<T>(string key, T value);
+        Response<T> Get<T>(string key);
+    }
+}
