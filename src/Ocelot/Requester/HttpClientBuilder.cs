a2559a889a4ea056ae521a295b75cd5e17935cc6
桂素伟 <axzxs2001@163.com>
2018-02-26 19:51:24 +0800

    Update HttpClientBuilder.cs
    Resolves the issue of accessing the Second API Project report HTTP status 500 when using butterfly tracing.

--- src/Ocelot/Requester/HttpClientBuilder.cs
+++ src/Ocelot/Requester/HttpClientBuilder.cs
@@ -33,7 +33,10 @@ namespace Ocelot.Requester
                 .ForEach(handler =>
                 {
                     var delegatingHandler = handler();
-                    delegatingHandler.InnerHandler = httpMessageHandler;
+                    if( delegatingHandler.InnerHandler==null)
+                    {
+                        delegatingHandler.InnerHandler = httpMessageHandler;
+                    }
                     httpMessageHandler = delegatingHandler;
                 });
             return httpMessageHandler;
