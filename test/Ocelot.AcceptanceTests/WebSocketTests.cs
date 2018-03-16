db7c45463dcafc1ad604e173f878596b68a6c25b
Tom Gardham-Pallister <tom.pallister@fourth.com>
2018-03-16 06:59:43 +0000

    #212 hacking away :(

--- test/Ocelot.AcceptanceTests/WebSocketTests.cs
+++ test/Ocelot.AcceptanceTests/WebSocketTests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.IO;
 using System.Net.WebSockets;
 using System.Text;
@@ -14,64 +16,171 @@ using Xunit;
 
 namespace Ocelot.AcceptanceTests
 {
+    public class SocketSend
+    {
+        public SocketSend(ArraySegment<byte> buffer, WebSocketMessageType messageType, bool endOfMessage)
+        {
+            Buffer = buffer;
+            this.MessageType = messageType;
+            this.EndOfMessage = endOfMessage;
+        }
+
+        public ArraySegment<byte> Buffer { get; private set; }
+        public WebSocketMessageType MessageType { get; private set; }
+        public bool EndOfMessage { get; private set; }
+    }
+
+    public class SocketResult
+    {
+        public SocketResult(byte[] result, WebSocketMessageType messageType, bool endOfMessage, int count)
+        {
+            Count = count;
+            this.Result = result;
+            this.MessageType = messageType;
+            this.EndOfMessage = endOfMessage;
+        }
+
+        public byte[] Result { get; private set; }
+        public WebSocketMessageType MessageType { get; private set; }
+        public bool EndOfMessage { get; private set; }
+        public int Count {get;private set;}
+    }
+
+    public class SocketQueue
+    {
+        private ConcurrentQueue<SocketSend> _queue;
+        private Thread _processing;
+
+        public string Name { get; }
+
+        private CancellationToken _cancellationToken;
+        private WebSocket _socket;
+
+        public SocketQueue(WebSocket socket, CancellationToken cancellationToken, string name)
+        {
+            Name = name;
+            _cancellationToken = cancellationToken;
+            _socket = socket;
+            _queue = new ConcurrentQueue<SocketSend>();
+            Responses = new ConcurrentQueue<SocketResult>();
+            _processing = new Thread(async () => await Process());
+            _processing.Start();
+        }
+
+        public bool Processing { get; private set; }
+
+        public ConcurrentQueue<SocketResult> Responses { get; private set; }
+
+        public void Send(ArraySegment<byte> buffer, WebSocketMessageType messageType, bool endOfMessage)
+        {
+            Console.WriteLine($"{Name} Enqueued message");
+            _queue.Enqueue(new SocketSend(buffer, messageType, endOfMessage));
+        }
+
+        private async Task Process()
+        {
+            try
+            {
+                Processing = true;
+
+                while (Processing)
+                {
+                    if (_queue.TryPeek(out var next))
+                    {
+                        Console.WriteLine($"{Name} Sending message...");
+                        await _socket.SendAsync(next.Buffer, next.MessageType, next.EndOfMessage, _cancellationToken);
+                    }
+
+                    var buffer = new byte[1024 * 4];
+                    var response = await _socket.ReceiveAsync(buffer, _cancellationToken);
+
+                    if (response.MessageType == WebSocketMessageType.Text)
+                    {
+                        var data = Encoding.UTF8.GetString(buffer, 0, response.Count);
+                        Console.WriteLine($"{Name} Received message...{data}");
+                        byte[] dest = new byte[buffer.Length]; 
+                        Array.Copy(buffer, dest, buffer.Length);
+                        Responses.Enqueue(new SocketResult(dest, response.MessageType, response.EndOfMessage, response.Count));
+                        Console.WriteLine($"{Name} Enqueued message...{data}");
+                    }
+                    else if (response.MessageType == WebSocketMessageType.Close)
+                    {
+                        Console.WriteLine($"{Name} Closing socket...");
+                        Processing = false;
+                        await _socket.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
+                    }
+
+                    Thread.Sleep(50);
+                    Console.WriteLine($"{Name} Finished loop...");
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+
     public class WebSocketTests : IDisposable
     {
         private WebHostBuilder _ocelotBuilder;
         private IWebHost _ocelotHost;
         private IWebHost _downstreamHost;
+        private SocketQueue _clientQueue;
+        private SocketQueue _upstreamQueue;
+        private SocketQueue _downstreamQueue;
+        private Thread _receiveFromUpstreamSendToDownstream;
+        private Thread _receiveFromDownstreamAndSendToUpstream;
+
+        public WebSocketTests()
+        {
+        }
 
         [Fact]
-        // [Fact(Skip = "Do not run will loop forever at the moment need to handle close signal on proxy")]
         public async Task should_proxy_websocket_input_to_downstream_service()
         {
             await StartFakeOcelot();
 
             await StartFakeDownstreamService();
 
-            //create the websockets client
             var client = new ClientWebSocket();
-            
+
             await client.ConnectAsync(new Uri("ws://localhost:5000/ws"), CancellationToken.None);
 
-            Console.WriteLine("Connected!");
-            //int count = 0;
-            //function that sends from the client to the server
+            _clientQueue = new SocketQueue(client, CancellationToken.None, "upstreamclient");
+
             var sending = Task.Run(async () =>
             {
                 string line = "test";
-                for (int i = 0; i < 10; i++)
+                for (int i = 0; i < 1; i++)
                 {
                     var bytes = Encoding.UTF8.GetBytes(line);
 
-                    await client.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true,
-                        CancellationToken.None);
-                    await Task.Delay(1000);
-                }
+                    _clientQueue.Send(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true);
 
-                await client.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
+                    await Task.Delay(50);
+                }
             });
 
-            //function that receives on the client from the server
             var receiving = Task.Run(async () =>
             {
                 var buffer = new byte[1024 * 4];
-
-                while (true)
+                int count = 0;
+                while (count < 1)
                 {
-                    var result = await client.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-
-                    if (result.MessageType == WebSocketMessageType.Text)
-                        Console.WriteLine(Encoding.UTF8.GetString(buffer, 0, result.Count));
-
-                    else if (result.MessageType == WebSocketMessageType.Close)
+                    if (_clientQueue.Responses.TryDequeue(out var message))
                     {
-                        await client.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
-                        break;
+                        Console.WriteLine(message.Result);
+                        count++;
                     }
+
+                    await Task.Delay(50);
                 }
             });
 
             await Task.WhenAll(sending, receiving);
+
+            Console.WriteLine("Fin!");
         }
 
         private async Task StartFakeOcelot()
@@ -80,7 +189,6 @@ namespace Ocelot.AcceptanceTests
             _ocelotBuilder.ConfigureServices(s =>
             {
                 s.AddSingleton(_ocelotBuilder);
-                //s.AddOcelot();
             });
             _ocelotBuilder.UseKestrel()
                 .UseUrls("http://localhost:5000")
@@ -108,11 +216,7 @@ namespace Ocelot.AcceptanceTests
                         {
                             if (context.WebSockets.IsWebSocketRequest)
                             {
-                                //WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
-                                //context.WebSockets.AcceptWebSocketAsync(Proxy("http://localhost:5001/ws"));
-
                                 await Proxy(context, "ws://localhost:5001/ws");
-                                //await Proxy(context, webSocket);
                             }
                             else
                             {
@@ -124,7 +228,6 @@ namespace Ocelot.AcceptanceTests
                             await next();
                         }
                     });
-                    //app.UseOcelot().Wait();
                 })
                 .UseIISIntegration();
             _ocelotHost = _ocelotBuilder.Build();
@@ -133,7 +236,7 @@ namespace Ocelot.AcceptanceTests
 
         private async Task StartFakeDownstreamService()
         {
-//start fake downstream service
+            //start fake downstream service
             _downstreamHost = new WebHostBuilder()
                 .ConfigureServices(s => { }).UseKestrel()
                 .UseUrls("http://localhost:5001")
@@ -180,106 +283,52 @@ namespace Ocelot.AcceptanceTests
 
         private async Task Proxy(HttpContext context, string serverEndpoint)
         {
+            //upstream
             var wsToUpstreamClient = await context.WebSockets.AcceptWebSocketAsync();
+            _upstreamQueue = new SocketQueue(wsToUpstreamClient, CancellationToken.None, "upstreamproxy");
 
+            //downstream
             var wsToDownstreamService = new ClientWebSocket();
             var uri = new Uri(serverEndpoint);
             await wsToDownstreamService.ConnectAsync(uri, CancellationToken.None);
+            _downstreamQueue = new SocketQueue(wsToDownstreamService, CancellationToken.None, "downstreamproxy");
 
-            var receiveFromUpstreamSendToDownstream = Task.Run(async () =>
+            _receiveFromUpstreamSendToDownstream = new Thread(async () =>
             {
-                var buffer = new byte[1024 * 4];
-
-                var receiveSegment = new ArraySegment<byte>(buffer);
-
-                while (wsToUpstreamClient.State == WebSocketState.Open || wsToUpstreamClient.State == WebSocketState.CloseSent)
+                while (_downstreamQueue.Processing && _upstreamQueue.Processing)
                 {
-                    // MUST read if we want the state to get updated...
-                    try
+                    //Console.WriteLine($"receiveFromUpstreamSendToDownstream proccesing");
+
+                    if (_upstreamQueue.Responses.TryDequeue(out var result))
                     {
-                        //receive from upstream
-                        var result = await wsToUpstreamClient.ReceiveAsync(receiveSegment, CancellationToken.None);
-
-                        var sendSegment = new ArraySegment<byte>(buffer, 0, result.Count);
-
-                        //kill the things
-                        if(result.MessageType == WebSocketMessageType.Close)
-                        {
-                            Console.WriteLine("receiveFromUpstreamSendToDownstream received closed from upstream service, break");
-
-                            await wsToUpstreamClient.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, "",
-                            CancellationToken.None);
-                            
-                            break;
-                        }
-                        //send to downstream
-                        await wsToDownstreamService.SendAsync(sendSegment, result.MessageType, result.EndOfMessage,
-                            CancellationToken.None);
+                        Console.WriteLine($"receiveFromUpstreamSendToDownstream dequeued");
+                        var data = Encoding.UTF8.GetString(result.Result, 0, result.Count);
+                        Console.WriteLine($"receiveFromUpstreamSendToDownstream {data}");
+                        var sendSegment = new ArraySegment<byte>(result.Result, 0, result.Count);
+                        _downstreamQueue.Send(sendSegment, result.MessageType, result.EndOfMessage);
                     }
 
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex);
-                        return;
-                    }
-
-                    if (wsToUpstreamClient.State != WebSocketState.Open)
-                    {
-                        await wsToDownstreamService
-                        .CloseAsync(WebSocketCloseStatus.Empty, "", CancellationToken.None);
-                        break;
-                    }
+                    await Task.Delay(50);
                 }
             });
+            _receiveFromUpstreamSendToDownstream.Start();
 
-            var receiveFromDownstreamAndSendToUpstream = Task.Run(async () =>
+            _receiveFromDownstreamAndSendToUpstream = new Thread(async () =>
             {
-                try
+                while (_downstreamQueue.Processing && _upstreamQueue.Processing)
                 {
-                    var buffer = new byte[1024 * 4];
+                    //Console.WriteLine($"receiveFromDownstreamAndSendToUpstream proccesing");
 
-                    //loop forever unless state to upstream client closes
-                    while (wsToDownstreamService.State == WebSocketState.Open || wsToDownstreamService.State == WebSocketState.CloseSent)
+                    if (_downstreamQueue.Responses.TryDequeue(out var result))
                     {
-                        if (wsToUpstreamClient.State != WebSocketState.Open)
-                        {
-                            break;
-                        }
-                        else
-                        {
-                            //receive from downstream service 
-                            var receiveSegment = new ArraySegment<byte>(buffer);
-                            var result = await wsToDownstreamService.ReceiveAsync(receiveSegment, CancellationToken.None);
-
-                            if (result.MessageType == WebSocketMessageType.Close)
-                            {
-                                Console.WriteLine("receiveFromDownstreamAndSendToUpstream received close from downstream service, break");
-                                break;
-                            }
-
-                            var sendSegment = new ArraySegment<byte>(buffer, 0, result.Count);
-
-                            try
-                            {
-                                //send to upstream client
-                                await wsToUpstreamClient.SendAsync(sendSegment, result.MessageType, result.EndOfMessage,
-                                CancellationToken.None);
-                            }
-                            catch(Exception e)
-                            {
-                                Console.WriteLine(e);                                
-                            }
-                        }
+                        Console.WriteLine($"receiveFromDownstreamAndSendToUpstream dequeued");
+                        var sendSegment = new ArraySegment<byte>(result.Result, 0, result.Count);
+                        _upstreamQueue.Send(sendSegment, result.MessageType, result.EndOfMessage);
                     }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                }
 
+                    await Task.Delay(50);
+                }
             });
-
-            await Task.WhenAll(receiveFromDownstreamAndSendToUpstream, receiveFromUpstreamSendToDownstream);
         }
 
         private async Task Echo(HttpContext context, WebSocket webSocket)
@@ -296,12 +345,15 @@ namespace Ocelot.AcceptanceTests
                 }
 
                 await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+                while(webSocket.State != WebSocketState.Closed)
+                {
+                    Console.WriteLine("Waiting for socket to close");
+                }
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
             }
-
         }
 
         public void Dispose()
