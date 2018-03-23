9ed70638011d93ec126625e2857407510fdce6ea
TomPallister <TomPallister@users.noreply.github.com>
2018-03-23 11:42:04 +0000

    #212 more code coverage

---
+++ test/Ocelot.UnitTests/Infrastructure/StringExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+using Ocelot.Infrastructure.Extensions;
+using Shouldly;
+
+namespace Ocelot.UnitTests.Infrastructure
+{
+    public class StringExtensionsTests
+    {
+        [Fact]
+        public void should_trim_start()
+        {
+            var test = "/string";
+
+            test = test.TrimStart("/");
+
+            test.ShouldBe("string");
+        }
+
+        [Fact]
+        public void should_return_source()
+        {
+            var test = "string";
+
+            test = test.LastCharAsForwardSlash();
+
+            test.ShouldBe("string/");
+        }
+    }
+}
