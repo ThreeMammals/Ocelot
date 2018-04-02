8f05d103741687e8aff52398751dfa1a05827f84
Tom Gardham-Pallister <tom.pallister@fourth.com>
2018-04-02 17:39:52 +0100

    #227 small refactor

---
+++ src/Ocelot/Logging/IOcelotLogger.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Ocelot.Logging
+{
+    /// <summary>
+    /// Thin wrapper around the DotNet core logging framework, used to allow the scopedDataRepository to be injected giving access to the Ocelot RequestId
+    /// </summary>
+    public interface IOcelotLogger
+    {
+        void LogTrace(string message, params object[] args);
+        void LogDebug(string message, params object[] args);
+        void LogInformation(string message, params object[] args);
+        void LogError(string message, Exception exception);
+        void LogError(string message, params object[] args);
+        void LogCritical(string message, Exception exception);
+
+        /// <summary>
+        /// The name of the type the logger has been built for.
+        /// </summary>
+        string Name { get; }
+    }
+}
