741145d8e4c3265463cbd135de3e0d079093fd8d
Tom Pallister <tom@threemammals.com>
2018-04-08 14:56:57 +0100

    #295 wait longer for this test

--- test/Ocelot.AcceptanceTests/ButterflyTracingTests.cs
+++ test/Ocelot.AcceptanceTests/ButterflyTracingTests.cs
@@ -104,7 +104,7 @@ namespace Ocelot.AcceptanceTests
                  .And(x => _steps.ThenTheResponseBodyShouldBe("Hello from Tom"))
                 .BDDfy();
 
-            var commandOnAllStateMachines = WaitFor(5000).Until(() => _butterflyCalled == 4);
+            var commandOnAllStateMachines = WaitFor(10000).Until(() => _butterflyCalled == 4);
 
             commandOnAllStateMachines.ShouldBeTrue();
         }
