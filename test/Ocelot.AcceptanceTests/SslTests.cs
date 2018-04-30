f9e618387c8b50b8802179422df6a0a199143e26
Paul Dilley <paul.dilley@equiniti.com>
2018-04-30 09:18:56 +0100

    Fix test that broke because of the change from 404 to 500 when delegate changes

--- test/Ocelot.AcceptanceTests/SslTests.cs
+++ test/Ocelot.AcceptanceTests/SslTests.cs
@@ -92,7 +92,7 @@ namespace Ocelot.AcceptanceTests
                 .And(x => _steps.GivenThereIsAConfiguration(configuration))
                 .And(x => _steps.GivenOcelotIsRunning())
                 .When(x => _steps.WhenIGetUrlOnTheApiGateway("/"))
-                .Then(x => _steps.ThenTheStatusCodeShouldBe(HttpStatusCode.NotFound))
+                .Then(x => _steps.ThenTheStatusCodeShouldBe(HttpStatusCode.InternalServerError))
                 .BDDfy();
         }
 
