57825155c3c4a8152c3501daced14d1b17488b5a
Tom Pallister <tom@threemammals.com>
2018-07-04 22:50:25 +0100

    #398 refactored MemoryHttpClientCache to stop it holding onto references

--- src/Ocelot/Requester/MemoryHttpClientCache.cs
+++ src/Ocelot/Requester/MemoryHttpClientCache.cs
@@ -5,36 +5,22 @@
 
     public class MemoryHttpClientCache : IHttpClientCache
     {
-        private readonly ConcurrentDictionary<string, ConcurrentQueue<IHttpClient>> _httpClientsCache;
+        private readonly ConcurrentDictionary<string, IHttpClient> _httpClientsCache;
 
         public MemoryHttpClientCache()
         {
-            _httpClientsCache = new ConcurrentDictionary<string, ConcurrentQueue<IHttpClient>>();
+            _httpClientsCache = new ConcurrentDictionary<string, IHttpClient>();
         }
 
-        public void Set(string id, IHttpClient client, TimeSpan expirationTime)
+        public void Set(string key, IHttpClient client, TimeSpan expirationTime)
         {
-            if (_httpClientsCache.TryGetValue(id, out var connectionQueue))
-            {
-                connectionQueue.Enqueue(client);
-            }
-            else
-            {
-                connectionQueue = new ConcurrentQueue<IHttpClient>();
-                connectionQueue.Enqueue(client);
-                _httpClientsCache.TryAdd(id, connectionQueue);
-            }
+            _httpClientsCache.AddOrUpdate(key, client, (k, oldValue) => client);
         }
 
-        public IHttpClient Get(string id)
+        public IHttpClient Get(string key)
         {
-            IHttpClient client= null;
-            if (_httpClientsCache.TryGetValue(id, out var connectionQueue))
-            {
-                connectionQueue.TryDequeue(out client);
-            }
-
-            return client;
+            //todo handle error?
+            return _httpClientsCache.TryGetValue(key, out var client) ? client : null;
         }      
     }
 }
