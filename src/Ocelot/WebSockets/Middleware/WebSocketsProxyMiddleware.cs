622de5efd020f0f23188357e625813d39de58572
Tom Pallister <TomPallister@users.noreply.github.com>
2018-09-30 10:17:51 +0100

    #639 added copywrite notice (#648)

--- src/Ocelot/WebSockets/Middleware/WebSocketsProxyMiddleware.cs
+++ src/Ocelot/WebSockets/Middleware/WebSocketsProxyMiddleware.cs
@@ -1,3 +1,7 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Modified https://github.com/aspnet/Proxy websockets class to use in Ocelot.
+
 using System;
 using System.Linq;
 using System.Net.WebSockets;
