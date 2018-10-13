68a8ee000be404a79eab9fafb5ee25e1c4b2db97
Tom Pallister <tom@threemammals.com>
2018-10-13 17:38:43 +0100

    #645 added a sample project using the administration api, tested locally and works as expected

---
+++ samples/AdministrationApi/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Administration;
+
+namespace AdministrationApi
+{
+  public class Program
+{
+    public static void Main(string[] args)
+    {
+         new WebHostBuilder()
+            .UseKestrel()
+            .UseUrls("http://localhost:5000")
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config
+                    .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                    .AddJsonFile("appsettings.json", true, true)
+                    .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                    .AddJsonFile("ocelot.json")
+                    .AddEnvironmentVariables();
+            })
+            .ConfigureServices(s => {
+                s.AddOcelot()
+                    .AddAdministration("/administration", "secret");
+            })
+            .ConfigureLogging((hostingContext, logging) =>
+            {
+                logging.AddConsole();
+            })
+            .UseIISIntegration()
+            .Configure(app =>
+            {
+                app.UseOcelot().Wait();
+            })
+            .Build()
+            .Run();
+    }
+}
+}
