b9ff7802bdc1b3b8aa026a01eb1a44e696217044
geffzhang <geffzhang@qq.com>
2019-01-20 13:19:39 +0800

    feat： remove port

--- samples/OelotKube/ApiGateway/Program.cs
+++ samples/OelotKube/ApiGateway/Program.cs
@@ -16,7 +16,6 @@ namespace ApiGateway
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://localhost:5000")
                 .ConfigureAppConfiguration((hostingContext, config) =>
                 {
                     config
