d147910e8e198aca4b1301e8fb1a252ead55141c
geffzhang <geffzhang@weyhd.com>
2019-05-10 11:31:48 +0800

    kubernetes use in cluster (#882)
    * refactor ：kubernetes use in cluster
    
    * feat：delete KubeClient

--- src/Ocelot.Provider.Kubernetes/OcelotBuilderExtensions.cs
+++ src/Ocelot.Provider.Kubernetes/OcelotBuilderExtensions.cs
@@ -1,14 +1,15 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using KubeClient;
+using Microsoft.Extensions.DependencyInjection;
 using Ocelot.DependencyInjection;
 
 namespace Ocelot.Provider.Kubernetes
 {
     public static class OcelotBuilderExtensions
     {
-        public static IOcelotBuilder AddKubernetes(this IOcelotBuilder builder)
+        public static IOcelotBuilder AddKubernetes(this IOcelotBuilder builder, bool usePodServiceAccount = true)
         {
             builder.Services.AddSingleton(KubernetesProviderFactory.Get);
-            builder.Services.AddSingleton<IKubeApiClientFactory, KubeApiClientFactory>();
+            builder.Services.AddKubeClient(usePodServiceAccount);
             return builder;
         }
     }
