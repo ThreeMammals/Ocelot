5ddd6b38dc634639a6e1762113c71478b684fd90
Jason Morton <jason.morton@arnoldclark.com>
2019-06-20 14:35:27 +0100

    Remove debug comments

--- src/Ocelot.Provider.Kubernetes/KubernetesProviderFactory.cs
+++ src/Ocelot.Provider.Kubernetes/KubernetesProviderFactory.cs
@@ -17,14 +17,14 @@ namespace Ocelot.Provider.Kubernetes
 
         private static ServiceDiscovery.Providers.IServiceDiscoveryProvider GetkubeProvider(IServiceProvider provider, Configuration.ServiceProviderConfiguration config, DownstreamReRoute reRoute, IOcelotLoggerFactory factory)
         {
-            //var kubeClient = provider.GetService<IKubeApiClient>();
+            var kubeClient = provider.GetService<IKubeApiClient>();
             var k8sRegistryConfiguration = new KubeRegistryConfiguration()
             {
                 KeyOfServiceInK8s = reRoute.ServiceName,
                 KubeNamespace = string.IsNullOrEmpty(reRoute.ServiceNamespace) ? config.Namespace : reRoute.ServiceNamespace
             };
 
-            var k8sServiceDiscoveryProvider = new Kube(k8sRegistryConfiguration, factory, null);
+            var k8sServiceDiscoveryProvider = new Kube(k8sRegistryConfiguration, factory, kubeClient);
             if (config.Type?.ToLower() == "pollkube")
             {
                 return new PollKube(config.PollingInterval, factory, k8sServiceDiscoveryProvider);
