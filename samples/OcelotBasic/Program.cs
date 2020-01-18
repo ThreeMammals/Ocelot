f0759ddf921b3ebed45e20f46c9862d881098c29
TomPallister <tom@threemammals.com>
2020-01-18 21:18:32 +0000

    +semver: major

--- samples/OcelotBasic/Program.cs
+++ samples/OcelotBasic/Program.cs
@@ -1,11 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 namespace OcelotBasic
 {
@@ -13,14 +11,32 @@ namespace OcelotBasic
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            new WebHostBuilder()
+               .UseKestrel()
+               .UseContentRoot(Directory.GetCurrentDirectory())
+               .ConfigureAppConfiguration((hostingContext, config) =>
+               {
+                   config
+                       .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                       .AddJsonFile("appsettings.json", true, true)
+                       .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                       .AddJsonFile("ocelot.json")
+                       .AddEnvironmentVariables();
+               })
+               .ConfigureServices(s => {
+                   s.AddOcelot();
+               })
+               .ConfigureLogging((hostingContext, logging) =>
+               {
+                   //add your logging
+               })
+               .UseIISIntegration()
+               .Configure(app =>
+               {
+                   app.UseOcelot().Wait();
+               })
+               .Build()
+               .Run();
         }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
     }
 }
