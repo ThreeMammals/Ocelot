62e745fea20c975cee1e8508c1945315e8ea1793
Mantas Audickas <mantas.audickas@outlook.com>
2020-04-14 08:51:47 +0300

    add downstream service name finder (#1188)

---
+++ test/Ocelot.UnitTests/DownstreamRouteFinder/DownstreamServiceFinderTests.cs
@@ -0,0 +1,36 @@
+﻿using Ocelot.DownstreamRouteFinder.Finder;
+using Xunit;
+
+namespace Ocelot.UnitTests.DownstreamRouteFinder
+{
+    public class DownstreamServiceFinderTests
+    {
+        private readonly DownstreamServiceFinder _serviceFinder;
+
+        public DownstreamServiceFinderTests()
+        {
+            _serviceFinder = new DownstreamServiceFinder();
+        }
+
+        [Fact]
+        public void should_return_empty_when_without_path()
+        {
+            var serviceName = _serviceFinder.GetServiceName("/", null, null, null, null);
+            Assert.Equal("", serviceName);
+        }
+
+        [Fact]
+        public void should_return_node_name_when_single_node()
+        {
+            var serviceName = _serviceFinder.GetServiceName("/service-name", null, null, null, null);
+            Assert.Equal("service-name", serviceName);
+        }
+
+        [Fact]
+        public void should_return_first_node_name_when_multiple_nodes()
+        {
+            var serviceName = _serviceFinder.GetServiceName("/service-name/some/longer/path", null, null, null, null);
+            Assert.Equal("service-name", serviceName);
+        }
+    }
+}
