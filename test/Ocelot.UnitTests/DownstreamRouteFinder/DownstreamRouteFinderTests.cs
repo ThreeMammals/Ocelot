e14ce8e4eaf8b426601938c55538fde1f3577602
jlukawska <jolanta.lukawska@gmail.com>
2020-07-28 13:45:00 +0200

    unit tests

--- test/Ocelot.UnitTests/DownstreamRouteFinder/DownstreamRouteFinderTests.cs
+++ test/Ocelot.UnitTests/DownstreamRouteFinder/DownstreamRouteFinderTests.cs
@@ -724,19 +724,21 @@ namespace Ocelot.UnitTests.DownstreamRouteFinder
             {
                 ["header1"] = "headerValue1",
                 ["header2"] = "headerValue2",
+                ["header3"] = "headerValue3",
             };
             var upstreamHeadersConfig = new Dictionary<string, UpstreamHeaderTemplate>()
             {
                 ["header1"] = new UpstreamHeaderTemplate("headerValue1", "headerValue1"),
                 ["header2"] = new UpstreamHeaderTemplate("headerValue2", "headerValue2"),
             };
+            var urlPlaceholders = new List<PlaceholderNameAndValue> { new PlaceholderNameAndValue("url", "urlValue") };
+            var headerPlaceholders = new List<PlaceholderNameAndValue> { new PlaceholderNameAndValue("header", "headerValue") };
 
             this.Given(x => x.GivenThereIsAnUpstreamUrlPath("matchInUrlMatcher/"))
                 .And(x => GivenTheUpstreamHeadersIs(upstreamHeaders))
                 .And(x => x.GivenTheTemplateVariableAndNameFinderReturns(
-                    new OkResponse<List<PlaceholderNameAndValue>>(
-                        new List<PlaceholderNameAndValue>())))
-                .And(x => x.GivenTheHeaderPlaceholderAndNameFinderReturns(new List<PlaceholderNameAndValue>()))
+                    new OkResponse<List<PlaceholderNameAndValue>>(urlPlaceholders)))
+                .And(x => x.GivenTheHeaderPlaceholderAndNameFinderReturns(headerPlaceholders))
                 .And(x => x.GivenTheConfigurationIs(new List<Route>
                     {
                         new RouteBuilder()
@@ -757,7 +759,7 @@ namespace Ocelot.UnitTests.DownstreamRouteFinder
                 .When(x => x.WhenICallTheFinder())
                 .Then(
                     x => x.ThenTheFollowingIsReturned(new DownstreamRouteHolder(
-                        new List<PlaceholderNameAndValue>(),
+                        urlPlaceholders.Union(headerPlaceholders).ToList(),
                         new RouteBuilder()
                             .WithDownstreamRoute(new DownstreamRouteBuilder()
                                 .WithDownstreamPathTemplate("someDownstreamPath")
