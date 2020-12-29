6e3432152d6ccdaed6765734f8c0e4796cb06a83
Eli Syrota <Eli.Syrota@simcorp.com>
2020-12-29 05:45:14 -0500

    Dependency Injection for IStickySessionStorage prodiver

--- src/Ocelot/DependencyInjection/OcelotBuilder.cs
+++ src/Ocelot/DependencyInjection/OcelotBuilder.cs
@@ -79,6 +79,7 @@ namespace Ocelot.DependencyInjection
             Services.TryAddSingleton<IFileConfigurationRepository, DiskFileConfigurationRepository>();
             Services.TryAddSingleton<IFileConfigurationSetter, FileAndInternalConfigurationSetter>();
             Services.TryAddSingleton<IServiceDiscoveryProviderFactory, ServiceDiscoveryProviderFactory>();
+            Services.TryAddSingleton<IStickySessionStorage, InMemoryStickySessionStorage>();
             Services.AddSingleton<ILoadBalancerCreator, NoLoadBalancerCreator>();
             Services.AddSingleton<ILoadBalancerCreator, RoundRobinCreator>();
             Services.AddSingleton<ILoadBalancerCreator, CookieStickySessionsCreator>();
--- src/Ocelot/LoadBalancer/LoadBalancers/CookieStickySessionsCreator.cs
+++ src/Ocelot/LoadBalancer/LoadBalancers/CookieStickySessionsCreator.cs
@@ -7,14 +7,20 @@ namespace Ocelot.LoadBalancer.LoadBalancers
 {
     public class CookieStickySessionsCreator : ILoadBalancerCreator
     {
+        private readonly IStickySessionStorage _sessionStorage;
+
+        public CookieStickySessionsCreator(IStickySessionStorage sessionStorage)
+        {
+            _sessionStorage = sessionStorage;
+        }
+
         public Response<ILoadBalancer> Create(DownstreamRoute route, IServiceDiscoveryProvider serviceProvider)
         {
             var options = route.LoadBalancerOptions;
             var loadBalancer = new RoundRobin(serviceProvider.GetAsync, route.LoadBalancerKey);
             var bus = new InMemoryBus<StickySession>();
-            var sessionStorage = new InMemoryStickySessionStorage();
             return new OkResponse<ILoadBalancer>(
-                new CookieStickySessions(loadBalancer, options.Key, options.ExpiryInMs, bus, sessionStorage));
+                new CookieStickySessions(loadBalancer, options.Key, options.ExpiryInMs, bus, _sessionStorage));
         }
 
         public string Type => nameof(CookieStickySessions);
