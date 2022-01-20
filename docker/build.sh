f1db0257cedd0e3f966f44db4c169fe15cef9555
TGP <thomasgardham-pallister@Thomass-MacBook-Pro-2.local>
2022-01-20 19:48:12 +0000

    install net5.0 in correct place in container

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,5 @@
 # this script build the ocelot docker file
-version=0.0.5
+version=0.0.6
 docker build --platform linux/amd64 -t mijitt0m/ocelot-build -f Dockerfile.base .
 echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
 docker tag mijitt0m/ocelot-build mijitt0m/ocelot-build:$version
