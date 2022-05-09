63a87090ffae9eb2ae21e2925a094dcf3abb5802
Hille <hille@onsive.net>
2022-05-09 14:52:24 +0200

    reopened pr #1030

--- src/Ocelot/WebSockets/Middleware/WebSocketsProxyMiddleware.cs
+++ src/Ocelot/WebSockets/Middleware/WebSocketsProxyMiddleware.cs
@@ -48,6 +48,10 @@ namespace Ocelot.WebSockets.Middleware
                 catch (OperationCanceledException)
                 {
                     await destination.CloseOutputAsync(WebSocketCloseStatus.EndpointUnavailable, null, cancellationToken);
+                    if (destination.State == WebSocketState.Open || destination.State == WebSocketState.CloseReceived)
+                    {
+                        await destination.CloseOutputAsync(WebSocketCloseStatus.EndpointUnavailable, null, cancellationToken);
+                    }
                     return;
                 }
                 catch (WebSocketException e)
@@ -55,6 +59,10 @@ namespace Ocelot.WebSockets.Middleware
                     if (e.WebSocketErrorCode == WebSocketError.ConnectionClosedPrematurely)
                     {
                         await destination.CloseOutputAsync(WebSocketCloseStatus.EndpointUnavailable, null, cancellationToken);
+                        if (destination.State == WebSocketState.Open || destination.State == WebSocketState.CloseReceived)
+                        {
+                            await destination.CloseOutputAsync(WebSocketCloseStatus.EndpointUnavailable, null, cancellationToken);
+                        }
                         return;
                     }
                     throw;
