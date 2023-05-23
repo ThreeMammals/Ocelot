16fdb7ebbe228ddf2d9fb8229f9e983482f38864
Fabrizio Mancin <fabrizio.mancin@esprinet.com>
2023-05-23 15:24:56 +0200

    SecurityOptions init moved to SecurityOptionsCreator

--- src/Ocelot/Configuration/Creator/SecurityOptionsCreator.cs
+++ src/Ocelot/Configuration/Creator/SecurityOptionsCreator.cs
@@ -1,4 +1,7 @@
 ﻿using Ocelot.Configuration.File;
+using System.Linq;
+using System.Collections.Generic;
+using IPAddressRange = NetTools;
 
 namespace Ocelot.Configuration.Creator
 {
@@ -6,7 +9,33 @@ namespace Ocelot.Configuration.Creator
     {
         public SecurityOptions Create(FileSecurityOptions securityOptions)
         {
-            return new SecurityOptions(securityOptions.IPAllowedList, securityOptions.IPBlockedList, securityOptions.ExcludeAllowedFromBlocked);
+            var IPAllowedList = new List<string>();
+            var IPBlockedList = new List<string>();
+
+            foreach (var allowed in securityOptions.IPAllowedList)
+            {
+                if (IPAddressRange.IPAddressRange.TryParse(allowed, out var allowedIpAddressRange))
+                {
+                    var allowedIps = allowedIpAddressRange.AsEnumerable().Select(x => x.ToString());
+                    IPAllowedList.AddRange(allowedIps);
+                }
+            }
+
+            foreach (var blocked in securityOptions.IPBlockedList)
+            {
+                if (IPAddressRange.IPAddressRange.TryParse(blocked, out var blockedIpAddressRange))
+                {
+                    var blockedIps = blockedIpAddressRange.AsEnumerable().Select(x => x.ToString());
+                    IPBlockedList.AddRange(blockedIps);
+                }
+            }
+
+            if (securityOptions.ExcludeAllowedFromBlocked)
+            {
+                IPBlockedList = IPBlockedList.Except(IPAllowedList).ToList();
+            }
+
+            return new SecurityOptions(IPAllowedList, IPBlockedList);
         }
     }
 }
--- src/Ocelot/Configuration/SecurityOptions.cs
+++ src/Ocelot/Configuration/SecurityOptions.cs
@@ -1,41 +1,18 @@
 ﻿using NetTools;
+using System.Collections.Generic;
 
 namespace Ocelot.Configuration
 {
     public class SecurityOptions
     {
-        public SecurityOptions(List<string> allowedList, List<string> blockedList, bool excludeAllowedFromBlocked = false)
+        public SecurityOptions(List<string> allowedList, List<string> blockedList)
         {
-            IPAllowedList = new List<string>();
-            IPBlockedList = new List<string>();
-            ExcludeAllowedFromBlocked = excludeAllowedFromBlocked;
-
-            foreach (var allowed in allowedList)
-            {
-                if (IPAddressRange.TryParse(allowed, out var allowedIpAddressRange))
-                {
-                    var allowedIps = allowedIpAddressRange.AsEnumerable().Select(x => x.ToString());
-                    IPAllowedList.AddRange(allowedIps);
-                }
-            }
-
-            foreach (var blocked in blockedList)
-            {
-                if (IPAddressRange.TryParse(blocked, out var blockedIpAddressRange))
-                {
-                    var blockedIps = blockedIpAddressRange.AsEnumerable().Select(x => x.ToString());
-                    IPBlockedList.AddRange(blockedIps);
-                }
-            }
-
-            if (ExcludeAllowedFromBlocked)
-            {
-                IPBlockedList = IPBlockedList.Except(IPAllowedList).ToList();
-            }
+            IPAllowedList = allowedList;
+            IPBlockedList = blockedList;
         }
 
         public List<string> IPAllowedList { get; }
+
         public List<string> IPBlockedList { get; }
-        public bool ExcludeAllowedFromBlocked { get; }
     }
 }
