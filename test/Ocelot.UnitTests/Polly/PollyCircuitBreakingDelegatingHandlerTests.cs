20290f2088c30760ab5476d5965f2a4ce666029e
Raman Maksimchuk <dotnet044@gmail.com>
2023-05-25 22:03:23 +0300

    Split using sections for Ocelot.* and System.* namespaces

--- test/Ocelot.UnitTests/Polly/PollyCircuitBreakingDelegatingHandlerTests.cs
+++ test/Ocelot.UnitTests/Polly/PollyCircuitBreakingDelegatingHandlerTests.cs
@@ -1,16 +1,20 @@
 ﻿using Moq;
+
 using Ocelot.Logging;
 using Ocelot.Provider.Polly;
 using Ocelot.Provider.Polly.Interfaces;
+
 using Polly;
 using Polly.Wrap;
 using Shouldly;
+
 using System;
 using System.Net;
 using System.Net.Http;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
+
 using Xunit;
 
 namespace Ocelot.UnitTests.Polly;
