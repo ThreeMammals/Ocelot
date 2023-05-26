15ba09bf0aa0abcf921f712845200b0208f5088d
Raman Maksimchuk <dotnet044@gmail.com>
2023-05-26 20:49:30 +0300

    SecurityOptionsCreator: File-scoped namespace declaration

--- src/Ocelot/Configuration/Creator/SecurityOptionsCreator.cs
+++ src/Ocelot/Configuration/Creator/SecurityOptionsCreator.cs
@@ -1,41 +1,42 @@
 ﻿using Ocelot.Configuration.File;
-using System.Linq;
+
 using System.Collections.Generic;
+using System.Linq;
+
 using IPAddressRange = NetTools;
 
-namespace Ocelot.Configuration.Creator
+namespace Ocelot.Configuration.Creator;
+
+public class SecurityOptionsCreator : ISecurityOptionsCreator
 {
-    public class SecurityOptionsCreator : ISecurityOptionsCreator
+    public SecurityOptions Create(FileSecurityOptions securityOptions)
     {
-        public SecurityOptions Create(FileSecurityOptions securityOptions)
+        var IPAllowedList = new List<string>();
+        var IPBlockedList = new List<string>();
+
+        foreach (var allowed in securityOptions.IPAllowedList)
         {
-            var IPAllowedList = new List<string>();
-            var IPBlockedList = new List<string>();
-
-            foreach (var allowed in securityOptions.IPAllowedList)
+            if (IPAddressRange.IPAddressRange.TryParse(allowed, out var allowedIpAddressRange))
             {
-                if (IPAddressRange.IPAddressRange.TryParse(allowed, out var allowedIpAddressRange))
-                {
-                    var allowedIps = allowedIpAddressRange.AsEnumerable().Select(x => x.ToString());
-                    IPAllowedList.AddRange(allowedIps);
-                }
+                var allowedIps = allowedIpAddressRange.AsEnumerable().Select(x => x.ToString());
+                IPAllowedList.AddRange(allowedIps);
             }
-
-            foreach (var blocked in securityOptions.IPBlockedList)
-            {
-                if (IPAddressRange.IPAddressRange.TryParse(blocked, out var blockedIpAddressRange))
-                {
-                    var blockedIps = blockedIpAddressRange.AsEnumerable().Select(x => x.ToString());
-                    IPBlockedList.AddRange(blockedIps);
-                }
-            }
-
-            if (securityOptions.ExcludeAllowedFromBlocked)
-            {
-                IPBlockedList = IPBlockedList.Except(IPAllowedList).ToList();
-            }
-
-            return new SecurityOptions(IPAllowedList, IPBlockedList);
         }
+
+        foreach (var blocked in securityOptions.IPBlockedList)
+        {
+            if (IPAddressRange.IPAddressRange.TryParse(blocked, out var blockedIpAddressRange))
+            {
+                var blockedIps = blockedIpAddressRange.AsEnumerable().Select(x => x.ToString());
+                IPBlockedList.AddRange(blockedIps);
+            }
+        }
+
+        if (securityOptions.ExcludeAllowedFromBlocked)
+        {
+            IPBlockedList = IPBlockedList.Except(IPAllowedList).ToList();
+        }
+
+        return new SecurityOptions(IPAllowedList, IPBlockedList);
     }
 }
