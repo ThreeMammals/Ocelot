cf1ae38f2ded32b50774bbc21a4dc3ea45c6fe87
Raman Maksimchuk <dotnet044@gmail.com>
2023-05-31 15:28:34 +0300

    Fix compiling error

--- test/Ocelot.UnitTests/DownstreamRouteFinder/DownstreamRouteProviderFactoryTests.cs
+++ test/Ocelot.UnitTests/DownstreamRouteFinder/DownstreamRouteProviderFactoryTests.cs
@@ -1,25 +1,20 @@
-using System;
-using System.Collections.Generic;
-
 using Microsoft.Extensions.DependencyInjection;
-
 using Moq;
-
 using Ocelot.Configuration;
 using Ocelot.Configuration.Builder;
 using Ocelot.Configuration.Creator;
-using Ocelot.DownstreamRouteFinder.Finder;
 using Ocelot.DownstreamRouteFinder.UrlMatcher;
 using Ocelot.Logging;
-
 using Shouldly;
-
+using System;
+using System.Collections.Generic;
 using TestStack.BDDfy;
-
 using Xunit;
 
 namespace Ocelot.UnitTests.DownstreamRouteFinder
 {
+    using Ocelot.DownstreamRouteFinder.Finder;
+
     public class DownstreamRouteProviderFactoryTests
     {
         private readonly DownstreamRouteProviderFactory _factory;
