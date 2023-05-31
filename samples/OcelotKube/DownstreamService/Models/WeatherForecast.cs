273cfb49ae42b035a2a7c72ee8283bcd68be65b8
Raman Maksimchuk <dotnet044@gmail.com>
2023-05-31 19:10:42 +0300

    Upgrade K8s sample to ASP.NET 7. Rename project to Ocelot.Samples.OcelotKube.DownstreamService

---
+++ samples/OcelotKube/DownstreamService/Models/WeatherForecast.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ocelot.Samples.OcelotKube.DownstreamService.Models;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string Summary { get; set; }
+}
