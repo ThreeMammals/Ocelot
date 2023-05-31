f14952dcfa1df0733ba85bba0b664f4ba5367dd7
Raman Maksimchuk <dotnet044@gmail.com>
2023-05-31 17:32:37 +0300

    Fix SA1600: Elements should be documented

--- src/Ocelot.Tracing.OpenTracing/OcelotBuilderExtensions.cs
+++ src/Ocelot.Tracing.OpenTracing/OcelotBuilderExtensions.cs
@@ -8,8 +8,16 @@ using Microsoft.Extensions.DependencyInjection.Extensions;
 using Ocelot.DependencyInjection;
 using Ocelot.Logging;
 
+/// <summary>
+/// Extension methods for the <see cref="IOcelotBuilder"/> interface.
+/// </summary>
 public static class OcelotBuilderExtensions
 {
+    /// <summary>
+    /// Adds OpenTracing services using builder.
+    /// </summary>
+    /// <param name="builder">The Ocelot builder with services.</param>
+    /// <returns>An <see cref="IOcelotBuilder"/> object.</returns>
     public static IOcelotBuilder AddOpenTracing(this IOcelotBuilder builder)
     {
         builder.Services.TryAddSingleton<ITracer, OpenTracingTracer>();
