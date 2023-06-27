410a2efe181e34e124920bff2866d7d20534a310
raman-m <dotnet044@gmail.com>
2023-06-27 17:50:53 +0300

    Improve code readability

--- src/Ocelot.Provider.Consul/ConsulProviderFactory.cs
+++ src/Ocelot.Provider.Consul/ConsulProviderFactory.cs
@@ -1,9 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Ocelot.Configuration;
 using Ocelot.Logging;
 using Ocelot.ServiceDiscovery;
+using Ocelot.ServiceDiscovery.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Ocelot.Provider.Consul;
 
@@ -12,35 +14,36 @@ public static class ConsulProviderFactory
     private static readonly List<PollConsul> ServiceDiscoveryProviders = new();
     private static readonly object LockObject = new();
 
-    public static ServiceDiscoveryFinderDelegate Get = (provider, config, route) =>
+    public static ServiceDiscoveryFinderDelegate Get { get; } = CreateProvider;
+
+    private static IServiceDiscoveryProvider CreateProvider(IServiceProvider provider, ServiceProviderConfiguration config, DownstreamRoute route)
     {
         var factory = provider.GetService<IOcelotLoggerFactory>();
         var consulFactory = provider.GetService<IConsulClientFactory>();
-        var consulRegistryConfiguration = new ConsulRegistryConfiguration(config.Scheme, config.Host, config.Port,
-            route.ServiceName, config.Token);
-        var consulServiceDiscoveryProvider = new Consul(consulRegistryConfiguration, factory, consulFactory);
 
-        if (string.Compare(config.Type, "PollConsul", StringComparison.OrdinalIgnoreCase) != 0)
+        var consulRegistryConfiguration = new ConsulRegistryConfiguration(
+            config.Scheme, config.Host, config.Port, route.ServiceName, config.Token);
+
+        var consulProvider = new Consul(consulRegistryConfiguration, factory, consulFactory);
+
+        if ("PollConsul".Equals(config.Type, StringComparison.OrdinalIgnoreCase))
         {
-            return consulServiceDiscoveryProvider;
-        }
-
-        lock (LockObject)
-        {
-            var discoveryProvider = ServiceDiscoveryProviders.FirstOrDefault(x => x.ServiceName == route.ServiceName);
-
-            if (discoveryProvider != null)
+            lock (LockObject)
             {
+                var discoveryProvider = ServiceDiscoveryProviders.FirstOrDefault(x => x.ServiceName == route.ServiceName);
+                if (discoveryProvider != null)
+                {
+                    return discoveryProvider;
+                }
+
+                discoveryProvider = new PollConsul(
+                    config.PollingInterval, route.ServiceName, factory, consulProvider);
+                ServiceDiscoveryProviders.Add(discoveryProvider);
+
                 return discoveryProvider;
             }
-
-            discoveryProvider = new PollConsul(
-                config.PollingInterval, route.ServiceName, factory,
-                consulServiceDiscoveryProvider);
-
-            ServiceDiscoveryProviders.Add(discoveryProvider);
-
-            return discoveryProvider;
         }
-    };
+
+        return consulProvider;
+    }
 }
--- src/Ocelot.Provider.Consul/PollConsul.cs
+++ src/Ocelot.Provider.Consul/PollConsul.cs
@@ -1,62 +1,62 @@
 ﻿using Ocelot.Logging;
 using Ocelot.ServiceDiscovery.Providers;
+using Ocelot.Values;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Ocelot.Values;
 
-namespace Ocelot.Provider.Consul
+namespace Ocelot.Provider.Consul;
+
+public sealed class PollConsul : IServiceDiscoveryProvider
 {
-    public sealed class PollConsul : IServiceDiscoveryProvider
+    private readonly IOcelotLogger _logger;
+    private readonly IServiceDiscoveryProvider _consulServiceDiscoveryProvider;
+
+    private readonly int _pollingInterval;
+    private readonly object _lockObject = new();
+
+    private DateTime _lastUpdateTime;
+    private List<Service> _services;
+
+    public PollConsul(int pollingInterval, string serviceName, IOcelotLoggerFactory factory, IServiceDiscoveryProvider consulServiceDiscoveryProvider)
     {
-        private readonly IOcelotLogger _logger;
-        private readonly IServiceDiscoveryProvider _consulServiceDiscoveryProvider;
+        _logger = factory.CreateLogger<PollConsul>();
+        _consulServiceDiscoveryProvider = consulServiceDiscoveryProvider;
+        _pollingInterval = pollingInterval;
 
-        private readonly int _pollingInterval;
-        private DateTime _lastUpdateTime;
-        private readonly object _lockObject = new();
+        // Initialize by DateTime.MinValue as lowest value.
+        // Polling will occur immediately during the first call
+        _lastUpdateTime = DateTime.MinValue;
 
-        private List<Service> _services;
+        _services = new List<Service>();
+        ServiceName = serviceName;
+    }
 
-        public PollConsul(int pollingInterval, string serviceName, IOcelotLoggerFactory factory, IServiceDiscoveryProvider consulServiceDiscoveryProvider)
+    public string ServiceName { get; }
+
+    /// <summary>
+    /// Get the services.
+    /// If the first call, retrieve the services and then start the timer.
+    /// </summary>
+    /// <returns>A <see cref="Task{T}" /> with a <see cref="List{Service}"/> result of <see cref="Service"/>.</returns>
+    public Task<List<Service>> Get()
+    {
+        lock (_lockObject)
         {
-            _logger = factory.CreateLogger<PollConsul>();
-            _consulServiceDiscoveryProvider = consulServiceDiscoveryProvider;
-            _pollingInterval = pollingInterval;
-            ServiceName = serviceName;
-            _services = new List<Service>();
+            var refreshTime = _lastUpdateTime.AddMilliseconds(_pollingInterval);
 
-            //initializing with update time = DateTime.MinValue
-            //polling will occur then.
-            _lastUpdateTime = DateTime.MinValue;
-        }
-
-        public string ServiceName { get; }
-
-        /// <summary>
-        /// Getting the services, but, if first call,
-        /// retrieving the services and then starting the timer.
-        /// </summary>
-        /// <returns>the service list.</returns>
-        public Task<List<Service>> Get()
-        {
-            lock (_lockObject)
+            // Check if any services available
+            if (refreshTime >= DateTime.UtcNow && _services.Any())
             {
-                var refreshTime = _lastUpdateTime.AddMilliseconds(_pollingInterval);
-
-                //checking if any services available
-                if (refreshTime >= DateTime.UtcNow && _services.Any())
-                {
-                    return Task.FromResult(_services);
-                }
-
-                _logger.LogInformation($"Retrieving new client information for service: {ServiceName}");
-                _services = _consulServiceDiscoveryProvider.Get().Result;
-                _lastUpdateTime = DateTime.UtcNow;
-
                 return Task.FromResult(_services);
             }
+
+            _logger.LogInformation($"Retrieving new client information for service: {ServiceName}.");
+            _services = _consulServiceDiscoveryProvider.Get().Result;
+            _lastUpdateTime = DateTime.UtcNow;
+
+            return Task.FromResult(_services);
         }
     }
 }
