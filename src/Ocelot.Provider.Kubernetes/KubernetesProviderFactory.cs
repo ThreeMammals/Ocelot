8028783b3db674b7b039550cc9478f9038917af9
Guillaume Gnaegi <58469901+ggnaegi@users.noreply.github.com>
2023-06-27 20:49:28 +0200

    Also refactoring the kubernetes provider factory (like consul and eureka)

--- src/Ocelot.Provider.Kubernetes/KubernetesProviderFactory.cs
+++ src/Ocelot.Provider.Kubernetes/KubernetesProviderFactory.cs
@@ -12,30 +12,28 @@ namespace Ocelot.Provider.Kubernetes
 {
     public static class KubernetesProviderFactory
     {
-        public static ServiceDiscoveryFinderDelegate Get = (provider, config, route) =>
+        public static ServiceDiscoveryFinderDelegate Get { get; } = CreateProvider;
+        private const string PollKube = "pollkube";
+
+        private static ServiceDiscovery.Providers.IServiceDiscoveryProvider CreateProvider(IServiceProvider provider, ServiceProviderConfiguration config, DownstreamRoute route)
         {
             var factory = provider.GetService<IOcelotLoggerFactory>();
-            return GetKubeProvider(provider, config, route, factory);
-        };
-
-        private static ServiceDiscovery.Providers.IServiceDiscoveryProvider GetKubeProvider(IServiceProvider provider, ServiceProviderConfiguration config, DownstreamRoute route, IOcelotLoggerFactory factory)
-        {
             var kubeClient = provider.GetService<IKubeApiClient>();
 
-            var k8sRegistryConfiguration = new KubeRegistryConfiguration
+            var k8SRegistryConfiguration = new KubeRegistryConfiguration
             {
                 KeyOfServiceInK8s = route.ServiceName,
                 KubeNamespace = string.IsNullOrEmpty(route.ServiceNamespace) ? config.Namespace : route.ServiceNamespace,
             };
 
-            var k8sServiceDiscoveryProvider = new KubernetesServiceDiscoveryProvider(k8sRegistryConfiguration, factory, kubeClient);
+            var k8SServiceDiscoveryProvider = new KubernetesServiceDiscoveryProvider(k8SRegistryConfiguration, factory, kubeClient);
 
-            if (config.Type?.ToLower() == "pollkube")
+            if (config.Type?.ToLower() == PollKube)
             {
-                return new PollKubernetes(config.PollingInterval, factory, k8sServiceDiscoveryProvider);
+                return new PollKubernetes(config.PollingInterval, factory, k8SServiceDiscoveryProvider);
             }
 
-            return k8sServiceDiscoveryProvider;
+            return k8SServiceDiscoveryProvider;
         }
     }
 }
