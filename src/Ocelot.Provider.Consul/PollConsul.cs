ebd0cc61b88108aecec2e53e1a80baba72a52a15
raman-m <dotnet044@gmail.com>
2023-07-10 18:54:03 +0300

    Fix empty the '_services' list during first access using PollConsul

--- src/Ocelot.Provider.Consul/PollConsul.cs
+++ src/Ocelot.Provider.Consul/PollConsul.cs
@@ -20,7 +20,9 @@ public sealed class PollConsul : IServiceDiscoveryProvider, IDisposable
     {
         _logger = factory.CreateLogger<PollConsul>();
         _consulServiceDiscoveryProvider = consulServiceDiscoveryProvider;
-        _services = new List<Service>();
+
+        // Solves the bug where the first access _services empty when using PollConsul
+        _services = _consulServiceDiscoveryProvider.Get().Result ?? new();
 
         _timer = new Timer(async x =>
         {
