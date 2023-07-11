37cc861c7611fb72db2e840fa39e21c986ded9bd
Raman Maksimchuk <dotnet044@gmail.com>
2023-07-11 20:22:47 +0300

    Update InMemoryStickySessionStorage.cs
    Use File-scoped namespace declaration

--- src/Ocelot/LoadBalancer/LoadBalancers/InMemoryStickySessionStorage.cs
+++ src/Ocelot/LoadBalancer/LoadBalancers/InMemoryStickySessionStorage.cs
@@ -1,39 +1,38 @@
 ﻿using System.Collections.Concurrent;
 
-namespace Ocelot.LoadBalancer.LoadBalancers
+namespace Ocelot.LoadBalancer.LoadBalancers;
+
+public class InMemoryStickySessionStorage : IStickySessionStorage
 {
-    public class InMemoryStickySessionStorage : IStickySessionStorage
+    private readonly ConcurrentDictionary<string, StickySession> _storage;
+
+    public InMemoryStickySessionStorage()
     {
-        private readonly ConcurrentDictionary<string, StickySession> _storage;
+        _storage = new ConcurrentDictionary<string, StickySession>();
+    }
 
-        public InMemoryStickySessionStorage()
-        {
-            _storage = new ConcurrentDictionary<string, StickySession>();
-        }
+    public bool TryGetSession(string key, out StickySession session)
+    {
+        return _storage.TryGetValue(key, out session);
+    }
 
-        public bool TryGetSession(string key, out StickySession session)
-        {
-            return _storage.TryGetValue(key, out session);
-        }
+    public StickySession GetSession(string key)
+    {
+        return _storage.TryGetValue(key, out var session) ? session : null;
+    }
 
-        public StickySession GetSession(string key)
-        {
-            return _storage.TryGetValue(key, out var session) ? session : null;
-        }
+    public void SetSession(string key, StickySession session)
+    {
+        _storage[key] = session;
+    }
 
-        public void SetSession(string key, StickySession session)
-        {
-            _storage[key] = session;
-        }
+    public bool TryRemove(string key, out StickySession session)
+    {
+        return _storage.TryRemove(key, out session);
+    }
 
-        public bool TryRemove(string key, out StickySession session)
-        {
-            return _storage.TryRemove(key, out session);
-        }
-
-        public bool Contains(string key)
-        {
-            return _storage.ContainsKey(key);
-        }
+    public bool Contains(string key)
+    {
+        return _storage.ContainsKey(key);
     }
 }
