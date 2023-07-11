57503cdf9af4abba9f54abe0024b71e3278e44e7
Raman Maksimchuk <dotnet044@gmail.com>
2023-07-11 20:56:09 +0300

    Update OcelotPipelineExtensions.cs
    Soft revert

--- src/Ocelot/Middleware/OcelotPipelineExtensions.cs
+++ src/Ocelot/Middleware/OcelotPipelineExtensions.cs
@@ -119,6 +119,9 @@ namespace Ocelot.Middleware
                 app.Use(pipelineConfiguration.AuthorizationMiddleware);
             }
 
+            // Now we can run the claims to headers transformation middleware	
+            app.UseClaimsToHeadersMiddleware();
+
             // Allow the user to implement their own query string manipulation logic
             app.UseIfNotNull(pipelineConfiguration.PreQueryStringBuilderMiddleware);
 
