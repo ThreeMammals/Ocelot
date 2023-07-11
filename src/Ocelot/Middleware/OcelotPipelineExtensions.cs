e008ea93aab42444ec665c6a80fbe1507f6affd7
Raman Maksimchuk <dotnet044@gmail.com>
2023-07-11 20:54:10 +0300

    Update OcelotPipelineExtensions.cs
    Soft revert to previous version

--- src/Ocelot/Middleware/OcelotPipelineExtensions.cs
+++ src/Ocelot/Middleware/OcelotPipelineExtensions.cs
@@ -119,18 +119,6 @@ namespace Ocelot.Middleware
                 app.Use(pipelineConfiguration.AuthorizationMiddleware);
             }
 
-            // Now we can run the claims to headers transformation middleware.
-            // We allow the ocelot middleware to be overriden by whatever the
-            // user wants
-            if (pipelineConfiguration.ClaimsToHeadersMiddleware == null)
-            {
-                app.UseClaimsToHeadersMiddleware();
-            }
-            else
-            {
-                app.Use(pipelineConfiguration.ClaimsToHeadersMiddleware);
-            }
-
             // Allow the user to implement their own query string manipulation logic
             app.UseIfNotNull(pipelineConfiguration.PreQueryStringBuilderMiddleware);
 
