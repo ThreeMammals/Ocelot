9e1ec616fc60b7c062a967c7038fa7726890964e
Raman Maksimchuk <dotnet044@gmail.com>
2023-07-11 20:12:57 +0300

    Update IStickySessionStorage.cs
    Use File-scoped namespace declaration

--- src/Ocelot/LoadBalancer/LoadBalancers/IStickySessionStorage.cs
+++ src/Ocelot/LoadBalancer/LoadBalancers/IStickySessionStorage.cs
@@ -1,11 +1,10 @@
-﻿namespace Ocelot.LoadBalancer.LoadBalancers
+﻿namespace Ocelot.LoadBalancer.LoadBalancers;
+
+public interface IStickySessionStorage
 {
-    public interface IStickySessionStorage
-    {
-        bool TryGetSession(string key, out StickySession session);
-        bool TryRemove(string key, out StickySession session);
-        StickySession GetSession(string key);
-        void SetSession(string key, StickySession session);
-        bool Contains(string key);
-    }
+    bool TryGetSession(string key, out StickySession session);
+    bool TryRemove(string key, out StickySession session);
+    StickySession GetSession(string key);
+    void SetSession(string key, StickySession session);
+    bool Contains(string key);
 }
