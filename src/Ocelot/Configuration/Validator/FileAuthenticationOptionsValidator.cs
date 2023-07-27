78a70dc8386b7351a00c3e0eec1de007422b5ec4
raman-m <dotnet044@gmail.com>
2023-07-27 21:35:54 +0300

    File-scoped namespace declaration

--- src/Ocelot/Configuration/Validator/FileAuthenticationOptionsValidator.cs
+++ src/Ocelot/Configuration/Validator/FileAuthenticationOptionsValidator.cs
@@ -5,33 +5,32 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Ocelot.Configuration.Validator
+namespace Ocelot.Configuration.Validator;
+
+public class FileAuthenticationOptionsValidator : AbstractValidator<FileAuthenticationOptions>
 {
-    public class FileAuthenticationOptionsValidator : AbstractValidator<FileAuthenticationOptions>
+    private readonly IAuthenticationSchemeProvider _authenticationSchemeProvider;
+
+    public FileAuthenticationOptionsValidator(IAuthenticationSchemeProvider authenticationSchemeProvider)
     {
-        private readonly IAuthenticationSchemeProvider _authenticationSchemeProvider;
+        _authenticationSchemeProvider = authenticationSchemeProvider;
 
-        public FileAuthenticationOptionsValidator(IAuthenticationSchemeProvider authenticationSchemeProvider)
+        RuleFor(authOptions => authOptions.AuthenticationProviderKey)
+            .MustAsync(IsSupportedAuthenticationProviders)
+            .WithMessage("{PropertyName}: {PropertyValue} is unsupported authentication provider");
+    }
+
+    private async Task<bool> IsSupportedAuthenticationProviders(string authenticationProviderKey, CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrEmpty(authenticationProviderKey))
         {
-            _authenticationSchemeProvider = authenticationSchemeProvider;
-
-            RuleFor(authOptions => authOptions.AuthenticationProviderKey)
-                .MustAsync(IsSupportedAuthenticationProviders)
-                .WithMessage("{PropertyName}: {PropertyValue} is unsupported authentication provider");
+            return true;
         }
 
-        private async Task<bool> IsSupportedAuthenticationProviders(string authenticationProviderKey, CancellationToken cancellationToken)
-        {
-            if (string.IsNullOrEmpty(authenticationProviderKey))
-            {
-                return true;
-            }
+        var schemes = await _authenticationSchemeProvider.GetAllSchemesAsync();
 
-            var schemes = await _authenticationSchemeProvider.GetAllSchemesAsync();
+        var supportedSchemes = schemes.Select(scheme => scheme.Name).ToList();
 
-            var supportedSchemes = schemes.Select(scheme => scheme.Name).ToList();
-
-            return supportedSchemes.Contains(authenticationProviderKey);
-        }
+        return supportedSchemes.Contains(authenticationProviderKey);
     }
 }
--- test/Ocelot.UnitTests/Configuration/Validation/FileAuthenticationOptionsValidatorTests.cs
+++ test/Ocelot.UnitTests/Configuration/Validation/FileAuthenticationOptionsValidatorTests.cs
@@ -11,113 +11,112 @@ using System.Threading.Tasks;
 using TestStack.BDDfy;
 using Xunit;
 
-namespace Ocelot.UnitTests.Configuration.Validation
+namespace Ocelot.UnitTests.Configuration.Validation;
+
+public class FileAuthenticationOptionsValidatorTests
 {
-    public class FileAuthenticationOptionsValidatorTests
+    private FileAuthenticationOptionsValidator _validator;
+    private readonly Mock<IAuthenticationSchemeProvider> _authProvider;
+    private FileAuthenticationOptions _authenticationOptions;
+    private ValidationResult _result;
+
+    public FileAuthenticationOptionsValidatorTests()
     {
-        private FileAuthenticationOptionsValidator _validator;
-        private readonly Mock<IAuthenticationSchemeProvider> _authProvider;
-        private FileAuthenticationOptions _authenticationOptions;
-        private ValidationResult _result;
+        _authProvider = new Mock<IAuthenticationSchemeProvider>();
+        _validator = new FileAuthenticationOptionsValidator(_authProvider.Object);
+    }
 
-        public FileAuthenticationOptionsValidatorTests()
+    [Fact]
+    public void should_be_valid_if_specified_authentication_provider_is_registered()
+    {
+        const string key = "JwtLads";
+
+        var authenticationOptions = new FileAuthenticationOptions
         {
-            _authProvider = new Mock<IAuthenticationSchemeProvider>();
-            _validator = new FileAuthenticationOptionsValidator(_authProvider.Object);
+            AuthenticationProviderKey = key,
+        };
+
+        this.Given(_ => GivenThe(authenticationOptions))
+            .And(_ => GivenAnAuthProvider(key))
+            .When(_ => WhenIValidateAsync())
+            .Then(_ => ThenTheResultIsValid())
+            .BDDfy();
+    }
+
+    [Fact]
+    public void should_not_be_valid_if_specified_authentication_provider_is_not_registered()
+    {
+        const string key = "JwtLads";
+
+        var authenticationOptions = new FileAuthenticationOptions
+        {
+            AuthenticationProviderKey = key,
+        };
+
+        this.Given(_ => GivenThe(authenticationOptions))
+            .When(_ => WhenIValidateAsync())
+            .Then(_ => ThenTheResultIsNotValid())
+            .And(_ => ThenTheErrorIs(key))
+            .BDDfy();
+    }
+
+    private void GivenAnAuthProvider(string key)
+    {
+        var schemes = new List<AuthenticationScheme>
+        {
+            new AuthenticationScheme(key, key, typeof(FakeAuthHandler)),
+        };
+
+        _authProvider
+            .Setup(x => x.GetAllSchemesAsync())
+            .ReturnsAsync(schemes);
+    }
+
+    private void ThenTheErrorIs(string providerKey)
+    {
+        _result.Errors[0].ErrorMessage.ShouldBe($"Authentication Provider Key: {providerKey} is unsupported authentication provider");
+    }
+
+    private void ThenTheResultIsValid()
+    {
+        _result.IsValid.ShouldBeTrue();
+    }
+
+    private void ThenTheResultIsNotValid()
+    {
+        _result.IsValid.ShouldBeFalse();
+    }        
+
+    private void GivenThe(FileAuthenticationOptions authenticationOptions)
+    {
+        _authenticationOptions = authenticationOptions;
+    }
+
+    private async Task WhenIValidateAsync()
+    {
+        _result = await _validator.ValidateAsync(_authenticationOptions);
+    }
+
+    private class FakeAuthHandler : IAuthenticationHandler
+    {
+        public Task<AuthenticateResult> AuthenticateAsync()
+        {
+            throw new NotImplementedException();
         }
 
-        [Fact]
-        public void should_be_valid_if_specified_authentication_provider_is_registered()
+        public Task ChallengeAsync(AuthenticationProperties properties)
         {
-            const string key = "JwtLads";
-
-            var authenticationOptions = new FileAuthenticationOptions
-            {
-                AuthenticationProviderKey = key,
-            };
-
-            this.Given(_ => GivenThe(authenticationOptions))
-                .And(_ => GivenAnAuthProvider(key))
-                .When(_ => WhenIValidateAsync())
-                .Then(_ => ThenTheResultIsValid())
-                .BDDfy();
+            throw new NotImplementedException();
         }
 
-        [Fact]
-        public void should_not_be_valid_if_specified_authentication_provider_is_not_registered()
+        public Task ForbidAsync(AuthenticationProperties properties)
         {
-            const string key = "JwtLads";
-
-            var authenticationOptions = new FileAuthenticationOptions
-            {
-                AuthenticationProviderKey = key,
-            };
-
-            this.Given(_ => GivenThe(authenticationOptions))
-                .When(_ => WhenIValidateAsync())
-                .Then(_ => ThenTheResultIsNotValid())
-                .And(_ => ThenTheErrorIs(key))
-                .BDDfy();
+            throw new NotImplementedException();
         }
 
-        private void GivenAnAuthProvider(string key)
+        public Task InitializeAsync(AuthenticationScheme scheme, HttpContext context)
         {
-            var schemes = new List<AuthenticationScheme>
-            {
-                new AuthenticationScheme(key, key, typeof(FakeAuthHandler)),
-            };
-
-            _authProvider
-                .Setup(x => x.GetAllSchemesAsync())
-                .ReturnsAsync(schemes);
-        }
-
-        private void ThenTheErrorIs(string providerKey)
-        {
-            _result.Errors[0].ErrorMessage.ShouldBe($"Authentication Provider Key: {providerKey} is unsupported authentication provider");
-        }
-
-        private void ThenTheResultIsValid()
-        {
-            _result.IsValid.ShouldBeTrue();
-        }
-
-        private void ThenTheResultIsNotValid()
-        {
-            _result.IsValid.ShouldBeFalse();
-        }        
-
-        private void GivenThe(FileAuthenticationOptions authenticationOptions)
-        {
-            _authenticationOptions = authenticationOptions;
-        }
-
-        private async Task WhenIValidateAsync()
-        {
-            _result = await _validator.ValidateAsync(_authenticationOptions);
-        }
-
-        private class FakeAuthHandler : IAuthenticationHandler
-        {
-            public Task<AuthenticateResult> AuthenticateAsync()
-            {
-                throw new NotImplementedException();
-            }
-
-            public Task ChallengeAsync(AuthenticationProperties properties)
-            {
-                throw new NotImplementedException();
-            }
-
-            public Task ForbidAsync(AuthenticationProperties properties)
-            {
-                throw new NotImplementedException();
-            }
-
-            public Task InitializeAsync(AuthenticationScheme scheme, HttpContext context)
-            {
-                throw new NotImplementedException();
-            }
+            throw new NotImplementedException();
         }
     }
 }
