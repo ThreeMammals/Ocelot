da7faa9c83c221393ed498db44fcc4ec32d130a2
Mehmet Yasin AKAR <mehyaa@gmail.com>
2023-09-15 09:59:23 +0300

    Tiny change

--- src/Ocelot/Authorization/ScopesAuthorizer.cs
+++ src/Ocelot/Authorization/ScopesAuthorizer.cs
@@ -7,6 +7,7 @@ namespace Ocelot.Authorization
     public class ScopesAuthorizer : IScopesAuthorizer
     {
         private const string ScopeClaimKey = "scope";
+
         private readonly IClaimsParser _claimsParser;
 
         public ScopesAuthorizer(IClaimsParser claimsParser)
