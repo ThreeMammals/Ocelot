5d9c214c67d51dbe9a1f598b63c50a76f2ef8cb0
raman-m <dotnet044@gmail.com>
2023-09-29 00:05:01 +0300

    Add unit tests

--- test/Ocelot.UnitTests/WebSockets/MockWebSocket.cs
+++ test/Ocelot.UnitTests/WebSockets/MockWebSocket.cs
@@ -161,7 +161,6 @@ internal class MockWebSocket : WebSocket
     //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
     //     Dispose(disposing: false);
     // }
-
     public override void Dispose()
     {
         // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
--- test/Ocelot.UnitTests/WebSockets/WebSocketsProxyMiddlewareTests.cs
+++ test/Ocelot.UnitTests/WebSockets/WebSocketsProxyMiddlewareTests.cs
@@ -19,6 +19,7 @@ public class WebSocketsProxyMiddlewareTests
 
     private readonly Mock<HttpContext> _context;
     private readonly Mock<IOcelotLogger> _logger;
+    private readonly Mock<IClientWebSocket> _client;
 
     public WebSocketsProxyMiddlewareTests()
     {
@@ -27,25 +28,31 @@ public class WebSocketsProxyMiddlewareTests
         _factory = new Mock<IWebSocketsFactory>();
 
         _context = new Mock<HttpContext>();
+        _context.SetupGet(x => x.WebSockets.IsWebSocketRequest).Returns(true);
+
         _logger = new Mock<IOcelotLogger>();
         _loggerFactory.Setup(x => x.CreateLogger<WebSocketsProxyMiddleware>())
             .Returns(_logger.Object);
 
         _middleware = new WebSocketsProxyMiddleware(_loggerFactory.Object, _next.Object, _factory.Object);
+
+        _client = new Mock<IClientWebSocket>();
+        _factory.Setup(x => x.CreateClient()).Returns(_client.Object);
     }
 
     [Fact]
-    public void ShouldIgnoreAllSslWarnings_WhenDangerousAcceptAnyServerCertificateValidatorIsTrue()
+    public void ShouldIgnoreAllSslWarningsWhenDangerousAcceptAnyServerCertificateValidatorIsTrue()
     {
-        this.Given(x => x.GivenPropertyDangerousAcceptAnyServerCertificateValidator(true))
+        List<object> actual = new();
+        this.Given(x => x.GivenPropertyDangerousAcceptAnyServerCertificateValidator(true, actual))
             .And(x => x.AndDoNotSetupProtocolsAndHeaders())
-            .And(x => x.AndDoNotConnectReally())
+            .And(x => x.AndDoNotConnectReally(null))
             .When(x => x.WhenInvokeWithHttpContext())
-            .Then(x => x.ThenIgnoredAllSslWarnings())
+            .Then(x => x.ThenIgnoredAllSslWarnings(actual))
             .BDDfy();
     }
 
-    private void GivenPropertyDangerousAcceptAnyServerCertificateValidator(bool enabled)
+    private void GivenPropertyDangerousAcceptAnyServerCertificateValidator(bool enabled, List<object> actual)
     {
         var request = new HttpRequestMessage(HttpMethod.Get, $"{Uri.UriSchemeWs}://localhost:12345");
         var downstream = new DownstreamRequest(request);
@@ -53,22 +60,16 @@ public class WebSocketsProxyMiddlewareTests
             .WithDangerousAcceptAnyServerCertificateValidator(enabled)
             .Build();
         _context.SetupGet(x => x.Items).Returns(new Dictionary<object, object>
-            {
-                { "DownstreamRequest", downstream },
-                { "DownstreamRoute", route },
-            });
-
-        _context.SetupGet(x => x.WebSockets.IsWebSocketRequest).Returns(true);
-
-        _client = new Mock<IClientWebSocket>();
-        _factory.Setup(x => x.CreateClient()).Returns(_client.Object);
+        {
+            { "DownstreamRequest", downstream },
+            { "DownstreamRoute", route },
+        });
 
         _client.SetupSet(x => x.Options.RemoteCertificateValidationCallback = It.IsAny<RemoteCertificateValidationCallback>())
-            .Callback<RemoteCertificateValidationCallback>(value => _callback = value);
+            .Callback<RemoteCertificateValidationCallback>(actual.Add);
 
-        _warning = string.Empty;
         _logger.Setup(x => x.LogWarning(It.IsAny<string>()))
-            .Callback<string>(message => _warning = message);
+            .Callback<string>(actual.Add);
     }
 
     private void AndDoNotSetupProtocolsAndHeaders()
@@ -77,9 +78,11 @@ public class WebSocketsProxyMiddlewareTests
         _context.SetupGet(x => x.Request.Headers).Returns(new HeaderDictionary());
     }
 
-    private void AndDoNotConnectReally()
+    private void AndDoNotConnectReally(Action<Uri, CancellationToken> callbackConnectAsync)
     {
-        _client.Setup(x => x.ConnectAsync(It.IsAny<Uri>(), It.IsAny<CancellationToken>())).Verifiable();
+        Action<Uri, CancellationToken> doNothing = (u, t) => { };
+        _client.Setup(x => x.ConnectAsync(It.IsAny<Uri>(), It.IsAny<CancellationToken>()))
+            .Callback(callbackConnectAsync ?? doNothing);
         var clientSocket = new Mock<WebSocket>();
         var serverSocket = new Mock<WebSocket>();
         _client.Setup(x => x.ToWebSocket()).Returns(clientSocket.Object);
@@ -97,28 +100,77 @@ public class WebSocketsProxyMiddlewareTests
         serverSocket.SetupGet(x => x.CloseStatus).Returns(WebSocketCloseStatus.Empty);
     }
 
-    private Mock<IClientWebSocket> _client;
-    private RemoteCertificateValidationCallback _callback;
-    private string _warning;
-
     private async Task WhenInvokeWithHttpContext()
     {
         await _middleware.Invoke(_context.Object);
     }
 
-    private void ThenIgnoredAllSslWarnings()
+    private void ThenIgnoredAllSslWarnings(List<object> actual)
     {
-        _context.Object.Items.DownstreamRoute().DangerousAcceptAnyServerCertificateValidator
-            .ShouldBeTrue();
+        var route = _context.Object.Items.DownstreamRoute();
+        var request = _context.Object.Items.DownstreamRequest();
+        route.DangerousAcceptAnyServerCertificateValidator.ShouldBeTrue();
 
         _logger.Verify(x => x.LogWarning(It.IsAny<string>()), Times.Once());
-        _warning.ShouldNotBeNullOrEmpty();
+        var warning = actual.Last() as string;
+        warning.ShouldNotBeNullOrEmpty();
+        var expectedWarning = string.Format(WebSocketsProxyMiddleware.IgnoredSslWarningFormat, route.UpstreamPathTemplate, route.DownstreamPathTemplate);
+        warning.ShouldBe(expectedWarning);
 
         _client.VerifySet(x => x.Options.RemoteCertificateValidationCallback = It.IsAny<RemoteCertificateValidationCallback>(),
             Times.Once());
 
-        _callback.ShouldNotBeNull();
-        var validation = _callback.Invoke(null, null, null, SslPolicyErrors.None);
+        var callback = actual.First() as RemoteCertificateValidationCallback;
+        callback.ShouldNotBeNull();
+        var validation = callback.Invoke(null, null, null, SslPolicyErrors.None);
         validation.ShouldBeTrue();
+    }
+
+    [Theory]
+    [InlineData("http", "ws")]
+    [InlineData("https", "wss")]
+    [InlineData("ftp", "ftp")]
+    public void ShouldReplaceNonWsSchemes(string scheme, string expectedScheme)
+    {
+        List<object> actual = new();
+        this.Given(x => x.GivenNonWebsocketScheme(scheme, actual))
+            .And(x => x.AndDoNotSetupProtocolsAndHeaders())
+            .And(x => x.AndDoNotConnectReally((uri, token) => actual.Add(uri)))
+            .When(x => x.WhenInvokeWithHttpContext())
+            .Then(x => x.ThenNonWsSchemesAreReplaced(scheme, expectedScheme, actual))
+            .BDDfy();
+    }
+
+    private void GivenNonWebsocketScheme(string scheme, List<object> actual)
+    {
+        var requestMessage = new HttpRequestMessage(HttpMethod.Get, $"{scheme}://localhost:12345");
+        var request = new DownstreamRequest(requestMessage);
+        var route = new DownstreamRouteBuilder().Build();
+        var items = new Dictionary<object, object>
+        {
+            { "DownstreamRequest", request },
+            { "DownstreamRoute", route },
+        };
+        _context.SetupGet(x => x.Items).Returns(items);
+
+        _logger.Setup(x => x.LogWarning(It.IsAny<string>()))
+            .Callback<string>(actual.Add);
+    }
+
+    private void ThenNonWsSchemesAreReplaced(string scheme, string expectedScheme, List<object> actual)
+    {
+        var route = _context.Object.Items.DownstreamRoute();
+        var request = _context.Object.Items.DownstreamRequest();
+        route.DangerousAcceptAnyServerCertificateValidator.ShouldBeFalse();
+
+        _logger.Verify(x => x.LogWarning(It.IsAny<string>()), Times.Once());
+        var warning = actual.First() as string;
+        warning.ShouldNotBeNullOrEmpty();
+        warning.ShouldContain($"'{scheme}'");
+        var expectedWarning = string.Format(WebSocketsProxyMiddleware.InvalidSchemeWarningFormat, scheme, request.ToUri().Replace(expectedScheme, scheme));
+        warning.ShouldBe(expectedWarning);
+
+        request.Scheme.ShouldBe(expectedScheme);
+        ((Uri)actual.Last()).Scheme.ShouldBe(expectedScheme);
     }
 }
