679c4fff46d90139b97422f8c1db03df5c71d40a
raman-m <dotnet044@gmail.com>
2023-10-02 10:51:04 +0300

    Add default port

--- src/Ocelot.Provider.Consul/ConsulRegistryConfiguration.cs
+++ src/Ocelot.Provider.Consul/ConsulRegistryConfiguration.cs
@@ -2,10 +2,18 @@
 
 public class ConsulRegistryConfiguration
 {
+    /// <summary>
+    /// Consul HTTP client default port.
+    /// <para>
+    /// HashiCorp Developer docs: <see href="https://developer.hashicorp.com/consul">Consul</see> | <see href="https://developer.hashicorp.com/consul/docs/install/ports">Required Ports</see>.
+    /// </para>
+    /// </summary>
+    public const int DefaultHttpPort = 8500;
+
     public ConsulRegistryConfiguration(string scheme, string host, int port, string keyOfServiceInConsul, string token)
     {
         Host = string.IsNullOrEmpty(host) ? "localhost" : host;
-        Port = port > 0 ? port : 8500; // Is 8500 default port of Consul?
+        Port = port > 0 ? port : DefaultHttpPort;
         Scheme = string.IsNullOrEmpty(scheme) ? Uri.UriSchemeHttp : scheme;
         KeyOfServiceInConsul = keyOfServiceInConsul;
         Token = token;
