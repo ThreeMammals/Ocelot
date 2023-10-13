3b776a753cfecd17b70c91c8f3ea7e46ccb7de84
Raynald Messié <redbird_project@yahoo.fr>
2023-10-13 18:40:23 +0200

    #1712 Bump to Polly 8.0 (#1714)
    * #1712 Migrate to Polly 8.0
    
    * code review post merge
    
    * post PR
    
    * #1712 Migrate to Polly 8.0
    
    * code review post merge
    
    * Update src/Ocelot.Provider.Polly/PollyQoSProvider.cs
    
    Co-authored-by: Raman Maksimchuk <dotnet044@gmail.com>
    
    * namespaces
    
    * Refactor QoS provider
    
    * Refactor AddPolly extension
    
    * Remove single quote because semicolon ends sentence
    
    ---------
    
    Co-authored-by: Ray <rmessie@traceparts.com>
    Co-authored-by: Raman Maksimchuk <dotnet044@gmail.com>

--- src/Ocelot.Provider.Polly/CircuitBreaker.cs
+++ src/Ocelot.Provider.Polly/CircuitBreaker.cs
@@ -1,19 +1,12 @@
-using Polly;
-
 namespace Ocelot.Provider.Polly
 {
     public class CircuitBreaker
     {
-        private readonly List<IAsyncPolicy> _policies = new();
-
         public CircuitBreaker(params IAsyncPolicy[] policies)
         {
-            foreach (var policy in policies.Where(p => p != null))
-            {
-                _policies.Add(policy);
-            }
+            Policies = policies.Where(p => p != null).ToArray();
         }
 
-        public IAsyncPolicy[] Policies => _policies.ToArray();
+        public IAsyncPolicy[] Policies { get; }
     }
 }
