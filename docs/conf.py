0153fd3cdfa8a448db0f3d2d3376aec7d6e44d40
Raman Maksimchuk <dotnet044@gmail.com>
2023-10-15 22:03:02 +0300

    Update conf.py: Update release, author and copyright

--- docs/conf.py
+++ docs/conf.py
@@ -7,9 +7,9 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Ocelot'
-copyright = '2023, ThreeMammals.org'
-author = 'ThreeMammals.org'
-release = '20.0.0'
+copyright = ' 2023, ThreeMammals Ocelot team'
+author = 'Tom Pallister, Ocelot Core team at ThreeMammals and Ocelot GitHub community'
+release = '20.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
