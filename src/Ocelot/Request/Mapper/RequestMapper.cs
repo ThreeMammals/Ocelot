ccc164b8ff2410eaa535e4c3e8d170ac08ed176d
Tanmay Seth <kushal.seth2010@gmail.com>
2023-10-19 19:04:16 +0530

    changing accessiblity

--- src/Ocelot/Request/Mapper/RequestMapper.cs
+++ src/Ocelot/Request/Mapper/RequestMapper.cs
@@ -83,7 +83,7 @@ namespace Ocelot.Request.Mapper
         /// Check if this process is running on Windows in an in process instance in IIS.
         /// </summary>
         /// <returns>True if Windows and in an in process instance on IIS, false otherwise.</returns>
-        public static bool IsRunningInProcessIIS()
+        private static bool IsRunningInProcessIIS()
         {
             if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
