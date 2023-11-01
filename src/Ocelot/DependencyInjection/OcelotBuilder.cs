ae43f32fe32b322487623e4b1a22299c3d91bb73
Stjepan <majdak.stjepan@gmail.com>
2023-11-01 12:41:13 +0100

    #952 #1174 Merge query strings without duplicate values (#1182)
    * Fix issue  #952 and #1174
    
    * Fix compiling errors
    
    * Fix warnings
    
    * Fix errors
    
    * Remove and Sort Usings
    
    * CA1845 Use span-based 'string.Concat' and 'AsSpan' instead of 'Substring'.
    Use 'AsSpan' with 'string.Concat'
    
    * IDE1006 Naming rule violation: These words must begin with upper case characters: {should_*}.
    Fix name violation
    
    * Add namespace
    
    * Fix build errors
    
    * Test class should match the name of tested class
    
    * Simplify too long class names, and they should match
    
    * Move to the parent folder which was empty
    
    * Fix warnings
    
    * Process dictionaries using LINQ to Objects approach
    
    * Fix code review issues from @RaynaldM
    
    * Remove tiny private helper with one reference
    
    * Fix warning & messages
    
    * Define theory instead of 2 facts
    
    * Add unit test for issue #952
    
    * Add additional unit test for #952 to keep param
    
    * Add tests for issue #1174
    
    * Remove unnecessary parameter
    
    * Copy routing.rst from released version
    
    * Refactor the middleware body for query params
    
    * Update routing.rst: Describe query string user scenarios
    
    ---------
    
    Co-authored-by: Stjepan Majdak <stjepan.majdak@a1.hr>
    Co-authored-by: raman-m <dotnet044@gmail.com>

--- src/Ocelot/DependencyInjection/OcelotBuilder.cs
+++ src/Ocelot/DependencyInjection/OcelotBuilder.cs
@@ -16,7 +16,7 @@ using Ocelot.Configuration.Setter;
 using Ocelot.Configuration.Validator;
 using Ocelot.DownstreamRouteFinder.Finder;
 using Ocelot.DownstreamRouteFinder.UrlMatcher;
-using Ocelot.DownstreamUrlCreator.UrlTemplateReplacer;
+using Ocelot.DownstreamUrlCreator;
 using Ocelot.Headers;
 using Ocelot.Infrastructure;
 using Ocelot.Infrastructure.Claims.Parser;
@@ -103,7 +103,7 @@ namespace Ocelot.DependencyInjection
             Services.TryAddSingleton<IClaimsParser, ClaimsParser>();
             Services.TryAddSingleton<IUrlPathToUrlTemplateMatcher, RegExUrlMatcher>();
             Services.TryAddSingleton<IPlaceholderNameAndValueFinder, UrlPathPlaceholderNameAndValueFinder>();
-            Services.TryAddSingleton<IDownstreamPathPlaceholderReplacer, DownstreamTemplatePathPlaceholderReplacer>();
+            Services.TryAddSingleton<IDownstreamPathPlaceholderReplacer, DownstreamPathPlaceholderReplacer>();
             Services.AddSingleton<IDownstreamRouteProvider, DownstreamRouteFinder.Finder.DownstreamRouteFinder>();
             Services.AddSingleton<IDownstreamRouteProvider, DownstreamRouteCreator>();
             Services.TryAddSingleton<IDownstreamRouteProviderFactory, DownstreamRouteProviderFactory>();
