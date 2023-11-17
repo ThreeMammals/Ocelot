9f24c42c8b8fc184438080b47a713ed5254e8ba9
Guillaume Gnaegi <58469901+ggnaegi@users.noreply.github.com>
2023-11-17 20:49:46 +0100

    Upgrade solution's projects to .net 8

--- test/Ocelot.UnitTests/Consul/ConsulServiceDiscoveryProviderTests.cs
+++ test/Ocelot.UnitTests/Consul/ConsulServiceDiscoveryProviderTests.cs
@@ -205,7 +205,7 @@ namespace Ocelot.UnitTests.Consul
                             }
 
                             var json = JsonConvert.SerializeObject(_serviceEntries);
-                            context.Response.Headers.Add("Content-Type", "application/json");
+                            context.Response.Headers.Append("Content-Type", "application/json");
                             await context.Response.WriteAsync(json);
                         }
                     });
