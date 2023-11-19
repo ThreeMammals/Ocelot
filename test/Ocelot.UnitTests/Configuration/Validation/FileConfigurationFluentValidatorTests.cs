72e4389fae7e71a06a4bf75744bbb149bd668368
Guillaume Gnaegi <58469901+ggnaegi@users.noreply.github.com>
2023-11-19 16:19:14 +0100

    adding indication about ISystemClock in .NET8

--- test/Ocelot.UnitTests/Configuration/Validation/FileConfigurationFluentValidatorTests.cs
+++ test/Ocelot.UnitTests/Configuration/Validation/FileConfigurationFluentValidatorTests.cs
@@ -1565,6 +1565,9 @@ namespace Ocelot.UnitTests.Configuration.Validation
 
         private class TestHandler : AuthenticationHandler<TestOptions>
         {
+            // https://learn.microsoft.com/en-us/dotnet/core/compatibility/aspnet-core/8.0/isystemclock-obsolete
+            // .NET 8.0: TimeProvider is now a settable property on the Options classes for the authentication and identity components.
+            // It can be set directly or by registering a provider in the dependency injection container.
 #if NET8_0_OR_GREATER
             public TestHandler(IOptionsMonitor<TestOptions> options, ILoggerFactory logger, UrlEncoder encoder) : base(options, logger, encoder)
             {
