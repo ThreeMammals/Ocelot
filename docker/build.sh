315283f3b9900cdffccb335d7f8d15440c1026c4
Guillaume Gnaegi <58469901+ggnaegi@users.noreply.github.com>
2023-11-19 03:00:23 +0100

    downstream service only available for .net 7

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,7 @@
 # this script build the ocelot docker file
 version=0.0.10
-docker build --platform linux/amd64 -t mijitt0m/ocelot-build -f Dockerfile.base .
+docker build --platform linux/amd64 -t ggnaegi/ocelot-build -f Dockerfile.base .
 echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
-docker tag mijitt0m/ocelot-build mijitt0m/ocelot-build:$version
-docker push mijitt0m/ocelot-build:latest
-docker push mijitt0m/ocelot-build:$version
+docker tag ggnaegi/ocelot-build ggnaegi/ocelot-build:$version
+docker push ggnaegi/ocelot-build:latest
+docker push ggnaegi/ocelot-build:$version
