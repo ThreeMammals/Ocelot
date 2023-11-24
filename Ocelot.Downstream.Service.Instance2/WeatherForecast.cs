67fadab2967f84466a6bc74f03419aec89aab8c4
Tanmay Seth <kushal.seth2010@gmail.com>
2023-11-24 12:53:10 +0530

    Sample LoadBalancer application using consul

---
+++ Ocelot.Downstream.Service.Instance2/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Ocelot.Downstream.Service.Instance2
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
---
+++ Ocelot.Sample.ApiGateway/Program.cs
@@ -0,0 +1,29 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Provider.Consul;
+using Ocelot.Middleware;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Configuration.AddJsonFile("Ocelot.json");
+builder.Services.AddOcelot(builder.Configuration).AddConsul();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseOcelot().Wait();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
