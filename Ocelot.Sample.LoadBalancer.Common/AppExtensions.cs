f997eb54f167da3afb297670faca6d45fd3d0f3f
Tanmay Seth <kushal.seth2010@gmail.com>
2023-11-24 12:53:10 +0530

    Sample LoadBalancer application using consul

---
+++ Ocelot.Sample.LoadBalancer.Common/AppExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Consul;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ocelot.Sample.LoadBalancer.Common
+{
+    public static class AppExtensions
+    {
+        public static IServiceCollection AddConsulConfig(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddSingleton<IConsulClient>(sp => new ConsulClient(config =>
+            {
+                config.Address = new Uri("http://localhost:8500"); // Consul address
+            }));
+            return services;
+        }
+
+        public static IApplicationBuilder UseConsul(this IApplicationBuilder app, IConfiguration configuration)
+        {
+            var consulClient = app.ApplicationServices.GetRequiredService<IConsulClient>();
+            var lifetime = app.ApplicationServices.GetRequiredService<IApplicationLifetime>();
+
+            var registration = new AgentServiceRegistration()
+            {
+                ID = configuration["Consul:ServiceId"],
+                Name = configuration["Consul:ServiceName"],
+                Address = configuration["Consul:ServiceHost"], // Your API address
+                Port = int.Parse(configuration["Consul:ServicePort"]), // Your API port
+            };
+
+            consulClient.Agent.ServiceDeregister(registration.ID).ConfigureAwait(true);
+            consulClient.Agent.ServiceRegister(registration).ConfigureAwait(true);
+
+            return app;
+        }
+    }
+}
