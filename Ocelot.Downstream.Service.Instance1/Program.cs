6b9ff3b39b147848124683f78ec96a724dc95963
Tanmay Seth <kushal.seth2010@gmail.com>
2023-12-06 18:44:42 +0530

    Code related to appsettings.json

--- Ocelot.Downstream.Service.Instance1/Program.cs
+++ Ocelot.Downstream.Service.Instance1/Program.cs
@@ -7,7 +7,24 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Getting Error here, how to resolve?
+//builder.Host.ConfigureWebHostDefaults(a =>
+//{
+//    a.ConfigureAppConfiguration((hostingContext, config) =>
+//    {
+//        // Load settings from appsettings.json
+//        config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+//        // Load settings from appsettings.instance1.json
+//        config.AddJsonFile("appsettings.instance1.json", optional: true, reloadOnChange: true);
+
+//        // Load settings from appsettings.instance2.json
+//        config.AddJsonFile("appsettings.instance2.json", optional: true, reloadOnChange: true);
+//    });
+//});
 builder.Services.AddConsulConfig(builder.Configuration);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
