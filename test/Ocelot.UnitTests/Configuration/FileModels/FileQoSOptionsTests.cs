ab79b01b41477274e5a20f4716ff3331138953b7
Alvin Huang <huanguolin@outlook.com>
2023-12-08 03:59:46 +0800

    #1833 Default timeout(90s) of downstream requests is broken (#1834)
    * fix #1833, Default timeout(90s) of downstream requests is broken
    
    * Fix test Should_log_warning_if_downstream_service_returns_internal_server_error
    
    * Add unit tests
    
    * Recover mixed line endings. Add docs
    
    * Unit tests for HttpClientBuilder
    
    * Add issue info
    
    * Acceptance test
    
    * Just formatting
    
    ---------
    
    Co-authored-by: alvin huang <alvin.huang@grapecity.com>
    Co-authored-by: Raman Maksimchuk <dotnet044@gmail.com>

---
+++ test/Ocelot.UnitTests/Configuration/FileModels/FileQoSOptionsTests.cs
@@ -0,0 +1,36 @@
+﻿using Ocelot.Configuration.File;
+
+namespace Ocelot.UnitTests.Configuration.FileModels;
+
+public class FileQoSOptionsTests
+{
+    [Fact(DisplayName = "1833: Default constructor must assign zero to the TimeoutValue property")]
+    public void Cstor_Default_AssignedZeroToTimeoutValue()
+    {
+        // Arrange, Act
+        var actual = new FileQoSOptions();
+
+        // Assert
+        Assert.Equal(0, actual.TimeoutValue);
+    }
+
+    [Fact]
+    public void Cstor_Default_AssignedZeroToExceptionsAllowedBeforeBreaking()
+    {
+        // Arrange, Act
+        var actual = new FileQoSOptions();
+
+        // Assert
+        Assert.Equal(0, actual.ExceptionsAllowedBeforeBreaking);
+    }
+
+    [Fact]
+    public void Cstor_Default_AssignedOneToDurationOfBreak()
+    {
+        // Arrange, Act
+        var actual = new FileQoSOptions();
+
+        // Assert
+        Assert.Equal(1, actual.DurationOfBreak);
+    }
+}
