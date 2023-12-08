c3662d5be4f191f5566b3f91ac44d3506bc3a4e9
Tanmay Seth <kushal.seth2010@gmail.com>
2023-12-08 15:41:53 +0530

    Add json in program.cs

--- Ocelot.Downstream.Service.Instance1/Program.cs
+++ Ocelot.Downstream.Service.Instance1/Program.cs
@@ -7,22 +7,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Load settings from appsettings.instance1.json
+builder.Configuration.AddJsonFile("appsettings.instance1.json", optional: true, reloadOnChange: true);
 
-//Getting Error here, how to resolve?
-//builder.Host.ConfigureWebHostDefaults(a =>
-//{
-//    a.ConfigureAppConfiguration((hostingContext, config) =>
-//    {
-//        // Load settings from appsettings.json
-//        config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-
-//        // Load settings from appsettings.instance1.json
-//        config.AddJsonFile("appsettings.instance1.json", optional: true, reloadOnChange: true);
-
-//        // Load settings from appsettings.instance2.json
-//        config.AddJsonFile("appsettings.instance2.json", optional: true, reloadOnChange: true);
-//    });
-//});
+// Load settings from appsettings.instance2.json
+builder.Configuration.AddJsonFile("appsettings.instance2.json", optional: true, reloadOnChange: true);
 builder.Services.AddConsulConfig(builder.Configuration);
 
 var app = builder.Build();
