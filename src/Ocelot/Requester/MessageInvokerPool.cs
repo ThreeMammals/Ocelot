6a75d9995d8e6275fa44bcb5005502ea00190ae6
Guillaume Gnaegi <58469901+ggnaegi@users.noreply.github.com>
2023-12-19 01:17:57 +0100

    some minor changes in MessageInvokerPool.

--- src/Ocelot/Requester/MessageInvokerPool.cs
+++ src/Ocelot/Requester/MessageInvokerPool.cs
@@ -9,14 +9,14 @@ public class MessageInvokerPool : IMessageInvokerPool
     //todo: this should be configurable and available as global config parameter in ocelot.json
     public const int DefaultRequestTimeoutSeconds = 90;
 
-    private readonly ConcurrentDictionary<MessageInvokerCacheKey, HttpMessageInvoker> _handlersPool;
+    private readonly ConcurrentDictionary<MessageInvokerCacheKey, Lazy<HttpMessageInvoker>> _handlersPool;
     private readonly IDelegatingHandlerHandlerFactory _handlerFactory;
     private readonly IOcelotLogger _logger;
 
     public MessageInvokerPool(IDelegatingHandlerHandlerFactory handlerFactory, IOcelotLoggerFactory loggerFactory)
     {
         _handlerFactory = handlerFactory ?? throw new ArgumentNullException(nameof(handlerFactory));
-        _handlersPool = new ConcurrentDictionary<MessageInvokerCacheKey, HttpMessageInvoker>();
+        _handlersPool = new ConcurrentDictionary<MessageInvokerCacheKey, Lazy<HttpMessageInvoker>>();
 
         ArgumentNullException.ThrowIfNull(loggerFactory);
 
@@ -25,38 +25,34 @@ public class MessageInvokerPool : IMessageInvokerPool
 
     public HttpMessageInvoker Get(DownstreamRoute downstreamRoute)
     {
-        var timeout = downstreamRoute.QosOptions.TimeoutValue == 0
-            ? TimeSpan.FromSeconds(DefaultRequestTimeoutSeconds)
-            : TimeSpan.FromMilliseconds(downstreamRoute.QosOptions.TimeoutValue);
-
         // Since the comparison is based on the downstream route object reference,
         // and the QoS Options properties can't be changed after the route is created,
         // we don't need to use the timeout value as part of the cache key.
-        return _handlersPool.GetOrAdd(new MessageInvokerCacheKey(downstreamRoute), _ => CreateMessageInvoker(downstreamRoute, timeout));
+        return _handlersPool.GetOrAdd(new MessageInvokerCacheKey(downstreamRoute), cacheKey =>
+            new Lazy<HttpMessageInvoker>(() => CreateMessageInvoker(cacheKey.DownstreamRoute))
+        ).Value;
     }
 
     public void Clear() => _handlersPool.Clear();
 
-    private HttpMessageInvoker CreateMessageInvoker(DownstreamRoute downstreamRoute, TimeSpan timeout)
+    private HttpMessageInvoker CreateMessageInvoker(DownstreamRoute downstreamRoute)
     {
         var baseHandler = CreateHandler(downstreamRoute);
         var handlers = _handlerFactory.Get(downstreamRoute).Data;
+        handlers.Reverse();
 
-        handlers
-            .Select(handler => handler)
-            .Reverse()
-            .ToList()
-            .ForEach(handler =>
-            {
-                var delegatingHandler = handler();
-                delegatingHandler.InnerHandler = baseHandler;
-                baseHandler = delegatingHandler;
-            });
+        foreach (var delegatingHandler in handlers.Select(handler => handler()))
+        {
+            delegatingHandler.InnerHandler = baseHandler;
+            baseHandler = delegatingHandler;
+        }
 
         // Adding timeout handler to the top of the chain
         // it's standard behavior to throw TimeoutException
         // after the defined timeout (90 seconds by default)
-        var timeoutHandler = new TimeoutDelegatingHandler(timeout)
+        var timeoutHandler = new TimeoutDelegatingHandler(downstreamRoute.QosOptions.TimeoutValue == 0
+            ? TimeSpan.FromSeconds(DefaultRequestTimeoutSeconds)
+            : TimeSpan.FromMilliseconds(downstreamRoute.QosOptions.TimeoutValue))
         {
             InnerHandler = baseHandler,
         };
@@ -98,7 +94,7 @@ public class MessageInvokerPool : IMessageInvokerPool
 
     private readonly struct MessageInvokerCacheKey : IEquatable<MessageInvokerCacheKey>
     {
-        private DownstreamRoute DownstreamRoute { get; }
+        public DownstreamRoute DownstreamRoute { get; }
 
         public MessageInvokerCacheKey(DownstreamRoute downstreamRoute)
         {
