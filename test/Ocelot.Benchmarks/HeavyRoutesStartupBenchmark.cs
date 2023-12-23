dc3a0c141ea0f8aafd7190800583c143e9a2d6fa
Tanmay Seth <kushal.seth2010@gmail.com>
2023-12-23 17:18:12 +0530

    Updating benchmarking for multiple configs.

--- test/Ocelot.Benchmarks/HeavyRoutesStartupBenchmark.cs
+++ test/Ocelot.Benchmarks/HeavyRoutesStartupBenchmark.cs
@@ -5,6 +5,11 @@ using Newtonsoft.Json;
 using Ocelot.Configuration.File;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using System.Net.Sockets;
+using System.Net;
+using Iced.Intel;
+using Microsoft.AspNetCore.Routing;
+using System.Runtime.Intrinsics.X86;
 namespace Ocelot.Benchmarks
 {
     [Config(typeof(HeavyRoutesStartupBenchmark))]
@@ -22,10 +27,68 @@ namespace Ocelot.Benchmarks
         [GlobalSetup]
         public void SetUp()
         {
+            //Setting up for more than 600 routes in different files
+            CreateOcelotConfigFile(0,100,"ocelot.json");
+            CreateOcelotConfigFile(101, 500, "ocelot.second.json");
+        }
+
+/* * Summary *
+        BenchmarkDotNet v0.13.11, Windows 11 (10.0.22621.2861/22H2/2022Update/SunValley2)
+  Intel Core i7-8650U CPU 1.90GHz(Kaby Lake R), 1 CPU, 8 logical and 4 physical cores
+  .NET SDK 8.0.100
+  [Host]     : .NET 6.0.25 (6.0.2523.51912), X64 RyuJIT AVX2[AttachedDebugger]
+  DefaultJob : .NET 6.0.25 (6.0.2523.51912), X64 RyuJIT AVX2
+| Method                             | Mean    | Error    | StdDev   | StdErr   | Min     | Q1      | Median  | Q3      | Max     | Op/s   | Gen0       | Gen1      | Allocated |
+|----------------------------------- |--------:|---------:|---------:|---------:|--------:|--------:|--------:|--------:|--------:|-------:|-----------:|----------:|----------:|
+| StartOcelotWithLargeConfigurations | 5.350 s | 0.0519 s | 0.0460 s | 0.0123 s | 5.214 s | 5.339 s | 5.351 s | 5.376 s | 5.408 s | 0.1869 | 42000.0000 | 3000.0000 | 185.05 MB |//        BenchmarkDotNet v0.13.11, Windows 11 (10.0.22621.2861/22H2/2022Update/SunValley2)
+  Intel Core i7-8650U CPU 1.90GHz(Kaby Lake R), 1 CPU, 8 logical and 4 physical cores
+  .NET SDK 8.0.100
+  [Host]     : .NET 6.0.25 (6.0.2523.51912), X64 RyuJIT AVX2[AttachedDebugger]
+  DefaultJob : .NET 6.0.25 (6.0.2523.51912), X64 RyuJIT AVX2
+| Method                             | Mean    | Error    | StdDev   | StdErr   | Min     | Q1      | Median  | Q3      | Max     | Op/s   | Gen0       | Gen1      | Allocated |
+|----------------------------------- |--------:|---------:|---------:|---------:|--------:|--------:|--------:|--------:|--------:|-------:|-----------:|----------:|----------:|
+| StartOcelotWithLargeConfigurations | 5.350 s | 0.0519 s | 0.0460 s | 0.0123 s | 5.214 s | 5.339 s | 5.351 s | 5.376 s | 5.408 s | 0.1869 | 42000.0000 | 3000.0000 | 185.05 MB |
+        */
+
+        [Benchmark]
+        public void StartOcelotWithLargeConfigurations()
+        {
+            OcelotStartup($"http://localhost:{FindAvailablePort()}");
+        }
+
+        [GlobalCleanup]
+        public void Cleanup()
+        {
+            // Stop the web host after benchmarking
+            _ocelot.StopAsync().GetAwaiter().GetResult();
+        }
+
+        private int FindAvailablePort()
+        {
+            TcpListener listener = null;
+            try
+            {
+                listener = new TcpListener(IPAddress.Loopback, 0);
+                listener.Start();
+                return ((IPEndPoint)listener.LocalEndpoint).Port;
+            }
+            finally
+            {
+                listener?.Stop();
+            }
+        }
+
+        /// <summary>
+        /// To replicate large number of routes.
+        /// </summary>
+        /// <param name="start">Starting digit to create downstream/upstream files.</param>
+        /// <param name="end">Ending digit to create downstream/upstream files.</param>
+        /// <param name="fileName">Ocelot files names.</param>
+        private static void CreateOcelotConfigFile(int start, int end, string fileName)
+        {
             var routes = new List<FileRoute>();
 
-            const int MaximumNumberOfRoutes = 100;
-            for (int i = 0; i < MaximumNumberOfRoutes; i++)
+            for (int i = start; i < end; i++)
             {
                 routes.Add(new()
                 {
@@ -45,20 +108,7 @@ namespace Ocelot.Benchmarks
             }
 
             var configuration = new FileConfiguration { Routes = routes };
-            GivenThereIsAConfiguration(configuration);
-        }
-
-        [Benchmark]
-        public void StartOcelotWithLargeConfigurations()
-        {
-            OcelotStartup("http://localhost:5000");
-        }
-
-        [GlobalCleanup]
-        public void Cleanup()
-        {
-            // Stop the web host after benchmarking
-            _ocelot.StopAsync().GetAwaiter().GetResult();
+            GivenThereIsAConfiguration(configuration, fileName);
         }
 
         private void OcelotStartup(string url)
@@ -74,6 +124,7 @@ namespace Ocelot.Benchmarks
                         .AddJsonFile("appsettings.json", true, true)
                         .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
                         .AddJsonFile("ocelot.json", false, false)
+                        .AddJsonFile("ocelot.second.json", false, false)
                         .AddEnvironmentVariables();
                 })
                 .ConfigureServices(s =>
@@ -94,9 +145,9 @@ namespace Ocelot.Benchmarks
             _ocelot.Start();
         }
 
-        public static void GivenThereIsAConfiguration(FileConfiguration fileConfiguration)
+        public static void GivenThereIsAConfiguration(FileConfiguration fileConfiguration, string fileName)
         {
-            var configurationPath = Path.Combine(AppContext.BaseDirectory, "ocelot.json");
+            var configurationPath = Path.Combine(AppContext.BaseDirectory, fileName);
 
             var jsonConfiguration = JsonConvert.SerializeObject(fileConfiguration);
 
