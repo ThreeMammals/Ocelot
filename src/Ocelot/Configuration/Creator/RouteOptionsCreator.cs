214b66b82a2567039e3aa23df3439f0f5e0a6c8a
Raman Maksimchuk <dotnet044@gmail.com>
2024-01-01 16:48:06 +0300

    Remove private helpers with one reference

--- src/Ocelot/Configuration/Creator/RouteOptionsCreator.cs
+++ src/Ocelot/Configuration/Creator/RouteOptionsCreator.cs
@@ -7,35 +7,22 @@ namespace Ocelot.Configuration.Creator
     {
         public RouteOptions Create(FileRoute fileRoute)
         {
-            var isAuthenticated = IsAuthenticated(fileRoute);
-            var isAuthorized = IsAuthorized(fileRoute);
-            var isCached = IsCached(fileRoute);
-            var enableRateLimiting = IsEnableRateLimiting(fileRoute);
+            var authOpts = fileRoute?.AuthenticationOptions;
+            var isAuthenticated = authOpts != null
+                && (!string.IsNullOrEmpty(authOpts.AuthenticationProviderKey)
+                    || authOpts.AuthenticationProviderKeys?.Any(k => !string.IsNullOrWhiteSpace(k)) == true);
+            var isAuthorized = fileRoute.RouteClaimsRequirement?.Any() == true;
+            var isCached = fileRoute.FileCacheOptions.TtlSeconds > 0;
+            var enableRateLimiting = fileRoute.RateLimitOptions?.EnableRateLimiting == true;
             var useServiceDiscovery = !string.IsNullOrEmpty(fileRoute.ServiceName);
 
-            var options = new RouteOptionsBuilder()
+            return new RouteOptionsBuilder()
                 .WithIsAuthenticated(isAuthenticated)
                 .WithIsAuthorized(isAuthorized)
                 .WithIsCached(isCached)
                 .WithRateLimiting(enableRateLimiting)
                 .WithUseServiceDiscovery(useServiceDiscovery)
                 .Build();
-
-            return options;
         }
-
-        private static bool IsEnableRateLimiting(FileRoute fileRoute) => fileRoute.RateLimitOptions?.EnableRateLimiting == true;
-
-        private bool IsAuthenticated(FileRoute fileRoute)
-        {
-            var options = fileRoute?.AuthenticationOptions;
-            return options != null &&
-                (!string.IsNullOrEmpty(options.AuthenticationProviderKey)
-                || options.AuthenticationProviderKeys?.Any(apk => !string.IsNullOrWhiteSpace(apk)) == true);
-        }
-
-        private static bool IsAuthorized(FileRoute fileRoute) => fileRoute.RouteClaimsRequirement?.Count > 0;
-
-        private static bool IsCached(FileRoute fileRoute) => fileRoute.FileCacheOptions.TtlSeconds > 0;
     }
 }
