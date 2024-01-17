60f0becca4d4e7660d414328c938fb35a3900ffa
Guillaume Gnaegi <58469901+ggnaegi@users.noreply.github.com>
2024-01-17 11:51:04 +0100

    adding a tolerance margin

--- test/Ocelot.UnitTests/Requester/MessageInvokerPoolTests.cs
+++ test/Ocelot.UnitTests/Requester/MessageInvokerPoolTests.cs
@@ -287,8 +287,8 @@ public class MessageInvokerPoolTests
             // Compare the elapsed time with the given timeout
             // You can use elapsed.CompareTo(timeout) or simply check if elapsed > timeout, based on your requirement
             Assert.IsType<TimeoutException>(e);
-            Assert.True(elapsed >= timeout, $"Elapsed time {elapsed} is bigger than expected timeout {timeout}");
-            Assert.True(elapsed < timeout.Add(TimeSpan.FromMilliseconds(200)), $"Elapsed time {elapsed} is smaller than expected timeout {timeout} + 200 ms");
+            Assert.True(elapsed >= timeout.Subtract(TimeSpan.FromMilliseconds(200)), $"Elapsed time {elapsed} is smaller than expected timeout {timeout} - 200 ms");
+            Assert.True(elapsed < timeout.Add(TimeSpan.FromMilliseconds(200)), $"Elapsed time {elapsed} is bigger than expected timeout {timeout} + 200 ms");
         }
     }
 
