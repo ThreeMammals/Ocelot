4bca217e9349b29d8d470f02e8d9ed43406a5640
Steven Liekens <steven.liekens@gmail.com>
2024-01-27 06:27:09 +0100

    Fix the route key format used for load balancing
    The old key format did not contain enough information to disambiguate routes based on an UpstreamHost. This was especially problematic when a ServiceName was used in conjuction with Service Discovery, instead of DownstreamHostAndPorts configuration.
    
    Resolves #1496

--- src/Ocelot/Configuration/Creator/RouteKeyCreator.cs
+++ src/Ocelot/Configuration/Creator/RouteKeyCreator.cs
@@ -5,13 +5,54 @@ namespace Ocelot.Configuration.Creator
 {
     public class RouteKeyCreator : IRouteKeyCreator
     {
-        public string Create(FileRoute fileRoute) => IsStickySession(fileRoute)
-            ? $"{nameof(CookieStickySessions)}:{fileRoute.LoadBalancerOptions.Key}"
-            : $"{fileRoute.UpstreamPathTemplate}|{string.Join(',', fileRoute.UpstreamHttpMethod)}|{string.Join(',', fileRoute.DownstreamHostAndPorts.Select(x => $"{x.Host}:{x.Port}"))}";
+        public string Create(FileRoute fileRoute)
+        {
+            var isStickySession = fileRoute.LoadBalancerOptions is
+            {
+                Type: nameof(CookieStickySessions),
+                Key.Length: > 0
+            };
 
-        private static bool IsStickySession(FileRoute fileRoute) =>
-            !string.IsNullOrEmpty(fileRoute.LoadBalancerOptions.Type)
-            && !string.IsNullOrEmpty(fileRoute.LoadBalancerOptions.Key)
-            && fileRoute.LoadBalancerOptions.Type == nameof(CookieStickySessions);
+            if (isStickySession)
+            {
+                return $"{nameof(CookieStickySessions)}:{fileRoute.LoadBalancerOptions.Key}";
+            }
+
+            var keyBuilder = new StringBuilder();
+            foreach (var upstreamHttpMethod in fileRoute.UpstreamHttpMethod)
+            {
+                Append(upstreamHttpMethod, ',');
+            }
+
+            Append(fileRoute.UpstreamHost);
+            Append(fileRoute.UpstreamPathTemplate);
+
+            if (!string.IsNullOrEmpty(fileRoute.ServiceName))
+            {
+                Append(fileRoute.ServiceNamespace);
+                Append(fileRoute.ServiceName);
+            }
+            else
+            {
+                foreach (var downstreamHostAndPorts in fileRoute.DownstreamHostAndPorts)
+                {
+                    Append(downstreamHostAndPorts.Host);
+                    Append(downstreamHostAndPorts.Port.ToString(), ':');
+                }
+            }
+
+            return keyBuilder.ToString();
+
+            // Helper method for appending a part to the key, automatically inserts a separator as needed
+            void Append(string next, char separator = '|')
+            {
+                if (keyBuilder.Length > 0)
+                {
+                    keyBuilder.Append(separator);
+                }
+
+                keyBuilder.Append(next);
+            }
+        }
     }
 }
