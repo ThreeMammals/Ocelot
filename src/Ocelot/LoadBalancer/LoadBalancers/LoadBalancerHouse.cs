e1063025e957ad5da803a6378232dd4a822969b2
Steven Liekens <steven.liekens@gmail.com>
2024-01-28 04:01:21 +0100

    Remove redundant load balancer type check

--- src/Ocelot/LoadBalancer/LoadBalancers/LoadBalancerHouse.cs
+++ src/Ocelot/LoadBalancer/LoadBalancers/LoadBalancerHouse.cs
@@ -16,29 +16,14 @@ namespace Ocelot.LoadBalancer.LoadBalancers
 
         public Response<ILoadBalancer> Get(DownstreamRoute route, ServiceProviderConfiguration config)
         {
+            if (_loadBalancers.TryGetValue(route.LoadBalancerKey, out var loadBalancer))
+            {
+                return new OkResponse<ILoadBalancer>(loadBalancer);
+            }
+
             try
             {
-                Response<ILoadBalancer> result;
-
-                if (_loadBalancers.TryGetValue(route.LoadBalancerKey, out var loadBalancer))
-                {
-                    if (route.LoadBalancerOptions.Type != loadBalancer.GetType().Name)
-                    {
-                        result = _factory.Get(route, config);
-                        if (result.IsError)
-                        {
-                            return new ErrorResponse<ILoadBalancer>(result.Errors);
-                        }
-
-                        loadBalancer = result.Data;
-                        AddLoadBalancer(route.LoadBalancerKey, loadBalancer);
-                    }
-
-                    return new OkResponse<ILoadBalancer>(loadBalancer);
-                }
-
-                result = _factory.Get(route, config);
-
+                var result = _factory.Get(route, config);
                 if (result.IsError)
                 {
                     return new ErrorResponse<ILoadBalancer>(result.Errors);
