9868b039356834ec77ef8b6241b29d2838d8567d
Raman Maksimchuk <dotnet044@gmail.com>
2024-02-05 21:17:03 +0300

    Convert back to block scoped namespace

--- src/Ocelot/Authentication/Middleware/AuthenticationMiddleware.cs
+++ src/Ocelot/Authentication/Middleware/AuthenticationMiddleware.cs
@@ -4,94 +4,95 @@ using Ocelot.Configuration;
 using Ocelot.Logging;
 using Ocelot.Middleware;
 
-namespace Ocelot.Authentication.Middleware;
-
-public sealed class AuthenticationMiddleware : OcelotMiddleware
+namespace Ocelot.Authentication.Middleware
 {
-    private readonly RequestDelegate _next;
-
-    public AuthenticationMiddleware(RequestDelegate next, IOcelotLoggerFactory loggerFactory)
-        : base(loggerFactory.CreateLogger<AuthenticationMiddleware>())
+    public sealed class AuthenticationMiddleware : OcelotMiddleware
     {
-        _next = next;
-    }
+        private readonly RequestDelegate _next;
 
-    public async Task Invoke(HttpContext httpContext)
-    {
-        var request = httpContext.Request;
-        var path = httpContext.Request.Path;
-        var downstreamRoute = httpContext.Items.DownstreamRoute();
-
-        // reducing nesting, returning early when no authentication is needed.
-        if (request.Method.Equals("OPTIONS", StringComparison.OrdinalIgnoreCase) || !downstreamRoute.IsAuthenticated)
+        public AuthenticationMiddleware(RequestDelegate next, IOcelotLoggerFactory loggerFactory)
+            : base(loggerFactory.CreateLogger<AuthenticationMiddleware>())
         {
-            Logger.LogInformation($"No authentication needed for path '{path}'.");
-            await _next(httpContext);
-            return;
+            _next = next;
         }
 
-        Logger.LogInformation(() => $"The path '{path}' is an authenticated route! {MiddlewareName} checking if client is authenticated...");
-
-        var result = await AuthenticateAsync(httpContext, downstreamRoute);
-
-        if (result.Principal?.Identity == null)
+        public async Task Invoke(HttpContext httpContext)
         {
-            SetUnauthenticatedError(httpContext, path, null);
-            return;
-        }
+            var request = httpContext.Request;
+            var path = httpContext.Request.Path;
+            var downstreamRoute = httpContext.Items.DownstreamRoute();
 
-        httpContext.User = result.Principal;
-
-        if (httpContext.User.Identity.IsAuthenticated)
-        {
-            Logger.LogInformation(() => $"Client has been authenticated for path '{path}' by '{httpContext.User.Identity.AuthenticationType}' scheme.");
-            await _next.Invoke(httpContext);
-            return;
-        }
-
-        SetUnauthenticatedError(httpContext, path, httpContext.User.Identity.Name);
-    }
-
-    private void SetUnauthenticatedError(HttpContext httpContext, string path, string userName)
-    {
-        var error = new UnauthenticatedError($"Request for authenticated route '{path}' {(string.IsNullOrEmpty(userName) ? "was unauthenticated" : $"by '{userName}' was unauthenticated!")}");
-        Logger.LogWarning(() => $"Client has NOT been authenticated for path '{path}' and pipeline error set. {error};");
-        httpContext.Items.SetError(error);
-    }
-
-    private async Task<AuthenticateResult> AuthenticateAsync(HttpContext context, DownstreamRoute route)
-    {
-        var options = route.AuthenticationOptions;
-        if (!string.IsNullOrWhiteSpace(options.AuthenticationProviderKey))
-        {
-            return await context.AuthenticateAsync(options.AuthenticationProviderKey);
-        }
-
-        var providerKeys = options.AuthenticationProviderKeys;
-        if (providerKeys.Length == 0 || providerKeys.All(string.IsNullOrWhiteSpace))
-        {
-            Logger.LogWarning(() => $"Impossible to authenticate client for path '{route.DownstreamPathTemplate}': both {nameof(options.AuthenticationProviderKey)} and {nameof(options.AuthenticationProviderKeys)} are empty but the {nameof(Configuration.AuthenticationOptions)} have defined.");
-            return AuthenticateResult.NoResult();
-        }
-
-        AuthenticateResult result = null;
-        foreach (var scheme in providerKeys.Where(apk => !string.IsNullOrWhiteSpace(apk)))
-        {
-            try
+            // reducing nesting, returning early when no authentication is needed.
+            if (request.Method.Equals("OPTIONS", StringComparison.OrdinalIgnoreCase) || !downstreamRoute.IsAuthenticated)
             {
-                result = await context.AuthenticateAsync(scheme);
-                if (result?.Succeeded == true)
+                Logger.LogInformation($"No authentication needed for path '{path}'.");
+                await _next(httpContext);
+                return;
+            }
+
+            Logger.LogInformation(() => $"The path '{path}' is an authenticated route! {MiddlewareName} checking if client is authenticated...");
+
+            var result = await AuthenticateAsync(httpContext, downstreamRoute);
+
+            if (result.Principal?.Identity == null)
+            {
+                SetUnauthenticatedError(httpContext, path, null);
+                return;
+            }
+
+            httpContext.User = result.Principal;
+
+            if (httpContext.User.Identity.IsAuthenticated)
+            {
+                Logger.LogInformation(() => $"Client has been authenticated for path '{path}' by '{httpContext.User.Identity.AuthenticationType}' scheme.");
+                await _next.Invoke(httpContext);
+                return;
+            }
+
+            SetUnauthenticatedError(httpContext, path, httpContext.User.Identity.Name);
+        }
+
+        private void SetUnauthenticatedError(HttpContext httpContext, string path, string userName)
+        {
+            var error = new UnauthenticatedError($"Request for authenticated route '{path}' {(string.IsNullOrEmpty(userName) ? "was unauthenticated" : $"by '{userName}' was unauthenticated!")}");
+            Logger.LogWarning(() => $"Client has NOT been authenticated for path '{path}' and pipeline error set. {error};");
+            httpContext.Items.SetError(error);
+        }
+
+        private async Task<AuthenticateResult> AuthenticateAsync(HttpContext context, DownstreamRoute route)
+        {
+            var options = route.AuthenticationOptions;
+            if (!string.IsNullOrWhiteSpace(options.AuthenticationProviderKey))
+            {
+                return await context.AuthenticateAsync(options.AuthenticationProviderKey);
+            }
+
+            var providerKeys = options.AuthenticationProviderKeys;
+            if (providerKeys.Length == 0 || providerKeys.All(string.IsNullOrWhiteSpace))
+            {
+                Logger.LogWarning(() => $"Impossible to authenticate client for path '{route.DownstreamPathTemplate}': both {nameof(options.AuthenticationProviderKey)} and {nameof(options.AuthenticationProviderKeys)} are empty but the {nameof(Configuration.AuthenticationOptions)} have defined.");
+                return AuthenticateResult.NoResult();
+            }
+
+            AuthenticateResult result = null;
+            foreach (var scheme in providerKeys.Where(apk => !string.IsNullOrWhiteSpace(apk)))
+            {
+                try
                 {
-                    return result;
+                    result = await context.AuthenticateAsync(scheme);
+                    if (result?.Succeeded == true)
+                    {
+                        return result;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Logger.LogWarning(() =>
+                        $"Impossible to authenticate client for path '{route.DownstreamPathTemplate}' and {nameof(options.AuthenticationProviderKey)}:{scheme}. Error: {e.Message}.");
                 }
             }
-            catch (Exception e)
-            {
-                Logger.LogWarning(() =>
-                    $"Impossible to authenticate client for path '{route.DownstreamPathTemplate}' and {nameof(options.AuthenticationProviderKey)}:{scheme}. Error: {e.Message}.");
-            }
-        }
 
-        return result ?? AuthenticateResult.NoResult();
+            return result ?? AuthenticateResult.NoResult();
+        }
     }
 }
--- src/Ocelot/Configuration/File/FileAuthenticationOptions.cs
+++ src/Ocelot/Configuration/File/FileAuthenticationOptions.cs
@@ -1,30 +1,31 @@
-﻿namespace Ocelot.Configuration.File;
-
-public sealed class FileAuthenticationOptions
+﻿namespace Ocelot.Configuration.File
 {
-    public FileAuthenticationOptions()
+    public sealed class FileAuthenticationOptions
     {
-        AllowedScopes = [];
-        AuthenticationProviderKeys = [];
+        public FileAuthenticationOptions()
+        {
+            AllowedScopes = [];
+            AuthenticationProviderKeys = [];
+        }
+
+        public FileAuthenticationOptions(FileAuthenticationOptions from)
+        {
+            AllowedScopes = [..from.AllowedScopes];
+            AuthenticationProviderKey = from.AuthenticationProviderKey;
+            AuthenticationProviderKeys = from.AuthenticationProviderKeys;
+        }
+
+        public List<string> AllowedScopes { get; set; }
+
+        [Obsolete("Use the " + nameof(AuthenticationProviderKeys) + " property!")]
+        public string AuthenticationProviderKey { get; set; }
+
+        public string[] AuthenticationProviderKeys { get; set; }
+
+        public override string ToString() => new StringBuilder()
+            .Append($"{nameof(AuthenticationProviderKey)}:'{AuthenticationProviderKey}',")
+            .Append($"{nameof(AuthenticationProviderKeys)}:[{string.Join(',', AuthenticationProviderKeys.Select(x => $"'{x}'"))}],")
+            .Append($"{nameof(AllowedScopes)}:[{string.Join(',', AllowedScopes.Select(x => $"'{x}'"))}]")
+            .ToString();
     }
-
-    public FileAuthenticationOptions(FileAuthenticationOptions from)
-    {
-        AllowedScopes = [..from.AllowedScopes];
-        AuthenticationProviderKey = from.AuthenticationProviderKey;
-        AuthenticationProviderKeys = from.AuthenticationProviderKeys;
-    }
-
-    public List<string> AllowedScopes { get; set; }
-
-    [Obsolete("Use the " + nameof(AuthenticationProviderKeys) + " property!")]
-    public string AuthenticationProviderKey { get; set; }
-
-    public string[] AuthenticationProviderKeys { get; set; }
-
-    public override string ToString() => new StringBuilder()
-        .Append($"{nameof(AuthenticationProviderKey)}:'{AuthenticationProviderKey}',")
-        .Append($"{nameof(AuthenticationProviderKeys)}:[{string.Join(',', AuthenticationProviderKeys.Select(x => $"'{x}'"))}],")
-        .Append($"{nameof(AllowedScopes)}:[{string.Join(',', AllowedScopes.Select(x => $"'{x}'"))}]")
-        .ToString();
 }
