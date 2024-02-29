53e49d1ca701e8185e5f26a460f3892406470064
Raman Maksimchuk <dotnet044@gmail.com>
2024-02-29 16:05:22 +0300

    Convert to file-scoped namespace

--- test/Ocelot.AcceptanceTests/MapRequestTests.cs
+++ test/Ocelot.AcceptanceTests/MapRequestTests.cs
@@ -1,245 +1,240 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Ocelot.Configuration.File;
+using System.Text;
 
-namespace Ocelot.AcceptanceTests
+namespace Ocelot.AcceptanceTests;
+
+public class MapRequestTests : IDisposable
 {
-    public class MapRequestTests : IDisposable
+    private readonly ServiceHandler _serviceHandler;
+    private readonly Steps _steps;
+
+    private class ChunkedContent : HttpContent
     {
-        private readonly ServiceHandler _serviceHandler;
-        private readonly Steps _steps;
+        private readonly string[] _chunks;
 
-        private class ChunkedContent : HttpContent
+        public ChunkedContent(params string[] chunks)
         {
-            private readonly string[] _chunks;
+            _chunks = chunks;
+        }
 
-            public ChunkedContent(params string[] chunks)
+        protected override async Task SerializeToStreamAsync(Stream stream, TransportContext context)
+        {
+            foreach (var chunk in _chunks)
             {
-                _chunks = chunks;
-            }
-
-            protected override async Task SerializeToStreamAsync(Stream stream, TransportContext context)
-            {
-                foreach (var chunk in _chunks)
-                {
-                    var bytes = Encoding.Default.GetBytes(chunk);
-                    await stream.WriteAsync(bytes, 0, bytes.Length);
-                }
-            }
-
-            protected override bool TryComputeLength(out long length)
-            {
-                length = -1;
-                return false;
+                var bytes = Encoding.Default.GetBytes(chunk);
+                await stream.WriteAsync(bytes, 0, bytes.Length);
             }
         }
 
-        public MapRequestTests()
+        protected override bool TryComputeLength(out long length)
         {
-            _serviceHandler = new ServiceHandler();
-            _steps = new Steps();
+            length = -1;
+            return false;
         }
-        
-        [Fact]
-        public void should_map_request_without_content()
-        {
-            var port = PortFinder.GetRandomPort();
+    }
 
-            var configuration = new FileConfiguration
+    public MapRequestTests()
+    {
+        _serviceHandler = new ServiceHandler();
+        _steps = new Steps();
+    }
+    
+    [Fact]
+    public void should_map_request_without_content()
+    {
+        var port = PortFinder.GetRandomPort();
+
+        var configuration = new FileConfiguration
+        {
+            Routes = new List<FileRoute>
             {
-                Routes = new List<FileRoute>
+                new()
                 {
-                    new()
+                    DownstreamPathTemplate = "/",
+                    DownstreamScheme = "http",
+                    DownstreamHostAndPorts = new List<FileHostAndPort>
                     {
-                        DownstreamPathTemplate = "/",
-                        DownstreamScheme = "http",
-                        DownstreamHostAndPorts = new List<FileHostAndPort>
+                        new()
                         {
-                            new()
-                            {
-                                Host = "localhost",
-                                Port = port,
-                            },
+                            Host = "localhost",
+                            Port = port,
                         },
-                        UpstreamPathTemplate = "/",
-                        UpstreamHttpMethod = new List<string> { "Get" },
                     },
+                    UpstreamPathTemplate = "/",
+                    UpstreamHttpMethod = new List<string> { "Get" },
                 },
-            };
+            },
+        };
 
-            this.Given(x => x.GivenThereIsAServiceRunningOn($"http://localhost:{port}", "/", 200))
-                .And(x => _steps.GivenThereIsAConfiguration(configuration))
-                .And(x => _steps.GivenOcelotIsRunning())
-                .When(x => _steps.WhenIGetUrlOnTheApiGateway("/"))
-                .Then(x => _steps.ThenTheStatusCodeShouldBe(HttpStatusCode.OK))
-                .And(x => _steps.ThenTheResponseBodyShouldBe(";;"))
-                .BDDfy();
-        }
+        this.Given(x => x.GivenThereIsAServiceRunningOn($"http://localhost:{port}", "/", 200))
+            .And(x => _steps.GivenThereIsAConfiguration(configuration))
+            .And(x => _steps.GivenOcelotIsRunning())
+            .When(x => _steps.WhenIGetUrlOnTheApiGateway("/"))
+            .Then(x => _steps.ThenTheStatusCodeShouldBe(HttpStatusCode.OK))
+            .And(x => _steps.ThenTheResponseBodyShouldBe(";;"))
+            .BDDfy();
+    }
 
-        [Fact]
-        public void should_map_request_with_content_length()
+    [Fact]
+    public void should_map_request_with_content_length()
+    {
+        var port = PortFinder.GetRandomPort();
+
+        var configuration = new FileConfiguration
         {
-            var port = PortFinder.GetRandomPort();
-
-            var configuration = new FileConfiguration
+            Routes = new List<FileRoute>
             {
-                Routes = new List<FileRoute>
+                new()
                 {
-                    new()
+                    DownstreamPathTemplate = "/",
+                    DownstreamScheme = "http",
+                    DownstreamHostAndPorts = new List<FileHostAndPort>
                     {
-                        DownstreamPathTemplate = "/",
-                        DownstreamScheme = "http",
-                        DownstreamHostAndPorts = new List<FileHostAndPort>
+                        new()
                         {
-                            new()
-                            {
-                                Host = "localhost",
-                                Port = port,
-                            },
+                            Host = "localhost",
+                            Port = port,
                         },
-                        UpstreamPathTemplate = "/",
-                        UpstreamHttpMethod = new List<string> { "Post" },
                     },
+                    UpstreamPathTemplate = "/",
+                    UpstreamHttpMethod = new List<string> { "Post" },
                 },
-            };
+            },
+        };
 
-            this.Given(x => x.GivenThereIsAServiceRunningOn($"http://localhost:{port}", "/", 200))
-                .And(x => _steps.GivenThereIsAConfiguration(configuration))
-                .And(x => _steps.GivenOcelotIsRunning())
-                .When(x => _steps.WhenIPostUrlOnTheApiGateway("/", new StringContent("This is some content")))
-                .Then(x => _steps.ThenTheStatusCodeShouldBe(HttpStatusCode.OK))
-                .And(x => _steps.ThenTheResponseBodyShouldBe("20;;This is some content"))
-                .BDDfy();
-        }
+        this.Given(x => x.GivenThereIsAServiceRunningOn($"http://localhost:{port}", "/", 200))
+            .And(x => _steps.GivenThereIsAConfiguration(configuration))
+            .And(x => _steps.GivenOcelotIsRunning())
+            .When(x => _steps.WhenIPostUrlOnTheApiGateway("/", new StringContent("This is some content")))
+            .Then(x => _steps.ThenTheStatusCodeShouldBe(HttpStatusCode.OK))
+            .And(x => _steps.ThenTheResponseBodyShouldBe("20;;This is some content"))
+            .BDDfy();
+    }
 
-        [Fact]
-        public void should_map_request_with_empty_content()
+    [Fact]
+    public void should_map_request_with_empty_content()
+    {
+        var port = PortFinder.GetRandomPort();
+
+        var configuration = new FileConfiguration
         {
-            var port = PortFinder.GetRandomPort();
-
-            var configuration = new FileConfiguration
+            Routes = new List<FileRoute>
             {
-                Routes = new List<FileRoute>
+                new()
                 {
-                    new()
+                    DownstreamPathTemplate = "/",
+                    DownstreamScheme = "http",
+                    DownstreamHostAndPorts = new List<FileHostAndPort>
                     {
-                        DownstreamPathTemplate = "/",
-                        DownstreamScheme = "http",
-                        DownstreamHostAndPorts = new List<FileHostAndPort>
+                        new()
                         {
-                            new()
-                            {
-                                Host = "localhost",
-                                Port = port,
-                            },
+                            Host = "localhost",
+                            Port = port,
                         },
-                        UpstreamPathTemplate = "/",
-                        UpstreamHttpMethod = new List<string> { "Post" },
                     },
+                    UpstreamPathTemplate = "/",
+                    UpstreamHttpMethod = new List<string> { "Post" },
                 },
-            };
+            },
+        };
 
-            this.Given(x => x.GivenThereIsAServiceRunningOn($"http://localhost:{port}", "/", 200))
-                .And(x => _steps.GivenThereIsAConfiguration(configuration))
-                .And(x => _steps.GivenOcelotIsRunning())
-                .When(x => _steps.WhenIPostUrlOnTheApiGateway("/", new StringContent("")))
-                .Then(x => _steps.ThenTheStatusCodeShouldBe(HttpStatusCode.OK))
-                .And(x => _steps.ThenTheResponseBodyShouldBe("0;;"))
-                .BDDfy();
-        }
+        this.Given(x => x.GivenThereIsAServiceRunningOn($"http://localhost:{port}", "/", 200))
+            .And(x => _steps.GivenThereIsAConfiguration(configuration))
+            .And(x => _steps.GivenOcelotIsRunning())
+            .When(x => _steps.WhenIPostUrlOnTheApiGateway("/", new StringContent("")))
+            .Then(x => _steps.ThenTheStatusCodeShouldBe(HttpStatusCode.OK))
+            .And(x => _steps.ThenTheResponseBodyShouldBe("0;;"))
+            .BDDfy();
+    }
 
-        [Fact]
-        public void should_map_request_with_chunked_content()
+    [Fact]
+    public void should_map_request_with_chunked_content()
+    {
+        var port = PortFinder.GetRandomPort();
+
+        var configuration = new FileConfiguration
         {
-            var port = PortFinder.GetRandomPort();
-
-            var configuration = new FileConfiguration
+            Routes = new List<FileRoute>
             {
-                Routes = new List<FileRoute>
+                new()
                 {
-                    new()
+                    DownstreamPathTemplate = "/",
+                    DownstreamScheme = "http",
+                    DownstreamHostAndPorts = new List<FileHostAndPort>
                     {
-                        DownstreamPathTemplate = "/",
-                        DownstreamScheme = "http",
-                        DownstreamHostAndPorts = new List<FileHostAndPort>
+                        new()
                         {
-                            new()
-                            {
-                                Host = "localhost",
-                                Port = port,
-                            },
+                            Host = "localhost",
+                            Port = port,
                         },
-                        UpstreamPathTemplate = "/",
-                        UpstreamHttpMethod = new List<string> { "Post" },
                     },
+                    UpstreamPathTemplate = "/",
+                    UpstreamHttpMethod = new List<string> { "Post" },
                 },
-            };
+            },
+        };
 
-            this.Given(x => x.GivenThereIsAServiceRunningOn($"http://localhost:{port}", "/", 200))
-                .And(x => _steps.GivenThereIsAConfiguration(configuration))
-                .And(x => _steps.GivenOcelotIsRunning())
-                .When(x => _steps.WhenIPostUrlOnTheApiGateway("/", new ChunkedContent("This ", "is some content")))
-                .Then(x => _steps.ThenTheStatusCodeShouldBe(HttpStatusCode.OK))
-                .And(x => _steps.ThenTheResponseBodyShouldBe(";chunked;This is some content"))
-                .BDDfy();
-        }
+        this.Given(x => x.GivenThereIsAServiceRunningOn($"http://localhost:{port}", "/", 200))
+            .And(x => _steps.GivenThereIsAConfiguration(configuration))
+            .And(x => _steps.GivenOcelotIsRunning())
+            .When(x => _steps.WhenIPostUrlOnTheApiGateway("/", new ChunkedContent("This ", "is some content")))
+            .Then(x => _steps.ThenTheStatusCodeShouldBe(HttpStatusCode.OK))
+            .And(x => _steps.ThenTheResponseBodyShouldBe(";chunked;This is some content"))
+            .BDDfy();
+    }
 
-        [Fact]
-        public void should_map_request_with_empty_chunked_content()
+    [Fact]
+    public void should_map_request_with_empty_chunked_content()
+    {
+        var port = PortFinder.GetRandomPort();
+
+        var configuration = new FileConfiguration
         {
-            var port = PortFinder.GetRandomPort();
-
-            var configuration = new FileConfiguration
+            Routes = new List<FileRoute>
             {
-                Routes = new List<FileRoute>
+                new()
                 {
-                    new()
+                    DownstreamPathTemplate = "/",
+                    DownstreamScheme = "http",
+                    DownstreamHostAndPorts = new List<FileHostAndPort>
                     {
-                        DownstreamPathTemplate = "/",
-                        DownstreamScheme = "http",
-                        DownstreamHostAndPorts = new List<FileHostAndPort>
+                        new()
                         {
-                            new()
-                            {
-                                Host = "localhost",
-                                Port = port,
-                            },
+                            Host = "localhost",
+                            Port = port,
                         },
-                        UpstreamPathTemplate = "/",
-                        UpstreamHttpMethod = new List<string> { "Post" },
                     },
+                    UpstreamPathTemplate = "/",
+                    UpstreamHttpMethod = new List<string> { "Post" },
                 },
-            };
+            },
+        };
 
-            this.Given(x => x.GivenThereIsAServiceRunningOn($"http://localhost:{port}", "/", 200))
-                .And(x => _steps.GivenThereIsAConfiguration(configuration))
-                .And(x => _steps.GivenOcelotIsRunning())
-                .When(x => _steps.WhenIPostUrlOnTheApiGateway("/", new ChunkedContent()))
-                .Then(x => _steps.ThenTheStatusCodeShouldBe(HttpStatusCode.OK))
-                .And(x => _steps.ThenTheResponseBodyShouldBe(";chunked;"))
-                .BDDfy();
-        }
+        this.Given(x => x.GivenThereIsAServiceRunningOn($"http://localhost:{port}", "/", 200))
+            .And(x => _steps.GivenThereIsAConfiguration(configuration))
+            .And(x => _steps.GivenOcelotIsRunning())
+            .When(x => _steps.WhenIPostUrlOnTheApiGateway("/", new ChunkedContent()))
+            .Then(x => _steps.ThenTheStatusCodeShouldBe(HttpStatusCode.OK))
+            .And(x => _steps.ThenTheResponseBodyShouldBe(";chunked;"))
+            .BDDfy();
+    }
 
-        private void GivenThereIsAServiceRunningOn(string baseUrl, string basePath, int statusCode)
+    private void GivenThereIsAServiceRunningOn(string baseUrl, string basePath, int statusCode)
+    {
+        _serviceHandler.GivenThereIsAServiceRunningOn(baseUrl, basePath, async context =>
         {
-            _serviceHandler.GivenThereIsAServiceRunningOn(baseUrl, basePath, async context =>
-            {
-                var request = context.Request;
-                var response = context.Response;
+            var request = context.Request;
+            var response = context.Response;
 
-                await response.WriteAsync(request.ContentLength + ";" + request.Headers.TransferEncoding + ";");
-                await request.Body.CopyToAsync(response.Body);
-            });
-        }
+            await response.WriteAsync(request.ContentLength + ";" + request.Headers.TransferEncoding + ";");
+            await request.Body.CopyToAsync(response.Body);
+        });
+    }
 
-        public void Dispose()
-        {
-            _serviceHandler?.Dispose();
-            _steps?.Dispose();
-        }
+    public void Dispose()
+    {
+        _serviceHandler?.Dispose();
+        _steps?.Dispose();
     }
 }
