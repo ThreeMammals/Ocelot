692039df1f40430d606992ac2c3f6c1419616d08
Guillaume Gnaegi <58469901+ggnaegi@users.noreply.github.com>
2024-04-03 22:11:33 +0200

    a bit of code cleanup, removing some usings

--- src/Ocelot/Configuration/Creator/IVersionPolicyCreator.cs
+++ src/Ocelot/Configuration/Creator/IVersionPolicyCreator.cs
@@ -1,14 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Ocelot.Configuration.Creator;
 
-namespace Ocelot.Configuration.Creator
+public interface IVersionPolicyCreator
 {
-    public interface IVersionPolicyCreator
-    {
-        HttpVersionPolicy Create(string downstreamVersionPolicy);
-    }
+    HttpVersionPolicy Create(string downstreamVersionPolicy);
 }
--- src/Ocelot/Configuration/Creator/VersionPolicyCreator.cs
+++ src/Ocelot/Configuration/Creator/VersionPolicyCreator.cs
@@ -1,18 +1,12 @@
-﻿using System.Net.Http;
+﻿namespace Ocelot.Configuration.Creator;
 
-namespace Ocelot.Configuration.Creator
+public class VersionPolicyCreator : IVersionPolicyCreator
 {
-    public class VersionPolicyCreator : IVersionPolicyCreator
+    public HttpVersionPolicy Create(string downstreamVersionPolicy) => downstreamVersionPolicy switch
     {
-        public HttpVersionPolicy Create(string downstreamVersionPolicy)
-        {
-            return downstreamVersionPolicy switch
-            {
-                VersionPolicies.RequestVersionExact => HttpVersionPolicy.RequestVersionExact,
-                VersionPolicies.RequestVersionOrHigher => HttpVersionPolicy.RequestVersionOrHigher,
-                VersionPolicies.RequestVersionOrLower => HttpVersionPolicy.RequestVersionOrLower,
-                _ => HttpVersionPolicy.RequestVersionOrLower,
-            };
-        }
-    }
+        VersionPolicies.RequestVersionExact => HttpVersionPolicy.RequestVersionExact,
+        VersionPolicies.RequestVersionOrHigher => HttpVersionPolicy.RequestVersionOrHigher,
+        VersionPolicies.RequestVersionOrLower => HttpVersionPolicy.RequestVersionOrLower,
+        _ => HttpVersionPolicy.RequestVersionOrLower,
+    };
 }
