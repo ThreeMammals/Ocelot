98fe814395a86b65cdf31534f618d1d7d69225d1
Raman Maksimchuk <dotnet044@gmail.com>
2024-04-06 17:25:53 +0300

    UpstreamHeaderTemplate

--- src/Ocelot/Values/UpstreamHeaderTemplate.cs
+++ src/Ocelot/Values/UpstreamHeaderTemplate.cs
@@ -1,21 +1,19 @@
-﻿using System.Text.RegularExpressions;
+﻿namespace Ocelot.Values;
 
-namespace Ocelot.Values;
-
+/// <summary>
+/// Upstream template properties of headers and their regular expression.
+/// </summary>
+/// <remarks>Ocelot feature: <see href="https://github.com/ThreeMammals/Ocelot/blob/develop/docs/features/routing.rst#upstream-headers">Routing based on request header</see>.</remarks>
 public class UpstreamHeaderTemplate
 {
     public string Template { get; }
-
     public string OriginalValue { get; }
-
     public Regex Pattern { get; }
 
     public UpstreamHeaderTemplate(string template, string originalValue)
     {
         Template = template;
         OriginalValue = originalValue;
-        Pattern = template == null ?
-            new Regex("$^", RegexOptions.Compiled | RegexOptions.Singleline) :
-            new Regex(template, RegexOptions.Compiled | RegexOptions.Singleline);
+        Pattern = new Regex(template ?? "$^", RegexOptions.Compiled | RegexOptions.Singleline);
     }
 }
