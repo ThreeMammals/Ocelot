7e9882466b51511099b126117c71ef8e8fe65963
Raman Maksimchuk <dotnet044@gmail.com>
2024-04-15 18:42:46 +0300

    Convert facts to one `Theory`

--- test/Ocelot.UnitTests/Configuration/UpstreamHeaderTemplatePatternCreatorTests.cs
+++ test/Ocelot.UnitTests/Configuration/UpstreamHeaderTemplatePatternCreatorTests.cs
@@ -15,140 +15,31 @@ public class UpstreamHeaderTemplatePatternCreatorTests
         _creator = new();
     }
 
-    [Fact]
-    public void Should_create_pattern_without_placeholders()
+    [Theory(DisplayName = "Should create pattern")]
+    [InlineData("country", "a text without placeholders", "^(?i)a text without placeholders$", " without placeholders")]
+    [InlineData("country", "a text without placeholders", "^a text without placeholders$", " Route is case sensitive", true)]
+    [InlineData("country", "{header:start}rest of the text", "^(?i)(?<start>.+)rest of the text$", " with placeholder in the beginning")]
+    [InlineData("country", "rest of the text{header:end}", "^(?i)rest of the text(?<end>.+)$", " with placeholder at the end")]
+    [InlineData("country", "{header:countrycode}", "^(?i)(?<countrycode>.+)$", " with placeholder only")]
+    [InlineData("country", "any text {header:cc} and other {header:version} and {header:bob} the end", "^(?i)any text (?<cc>.+) and other (?<version>.+) and (?<bob>.+) the end$", " with more placeholders")]
+    public void Create_WithUpstreamHeaderTemplates_ShouldCreatePattern(string key, string template, string expected, string withMessage, bool? isCaseSensitive = null)
     {
         // Arrange
         var fileRoute = new FileRoute
         {
+            RouteIsCaseSensitive = isCaseSensitive ?? false,
             UpstreamHeaderTemplates = new Dictionary<string, string>
             {
-                ["country"] = "a text without placeholders",
-            },
-        };
-        GivenTheFollowingFileRoute(fileRoute);
-
-        // Act
-        WhenICreateTheTemplatePattern();
-
-        // Assert
-        ThenTheFollowingIsReturned("country", "^(?i)a text without placeholders$");
-    }
-
-    [Fact]
-    public void Should_create_pattern_case_sensitive()
-    {
-        // Arrange
-        var fileRoute = new FileRoute
-        {
-            RouteIsCaseSensitive = true,
-            UpstreamHeaderTemplates = new Dictionary<string, string>
-            {
-                ["country"] = "a text without placeholders",
-            },
-        };
-        GivenTheFollowingFileRoute(fileRoute);
-
-        // Act
-        WhenICreateTheTemplatePattern();
-
-        // Assert
-        ThenTheFollowingIsReturned("country", "^a text without placeholders$");
-    }
-
-    [Fact]
-    public void Should_create_pattern_with_placeholder_in_the_beginning()
-    {
-        // Arrange
-        var fileRoute = new FileRoute
-        {
-            UpstreamHeaderTemplates = new Dictionary<string, string>
-            {
-                ["country"] = "{header:start}rest of the text",
-            },
-        };
-        GivenTheFollowingFileRoute(fileRoute);
-
-        // Act
-        WhenICreateTheTemplatePattern();
-
-        // Assert
-        ThenTheFollowingIsReturned("country", "^(?i)(?<start>.+)rest of the text$");
-    }
-
-    [Fact]
-    public void Should_create_pattern_with_placeholder_at_the_end()
-    {
-        // Arrange
-        var fileRoute = new FileRoute
-        {
-            UpstreamHeaderTemplates = new Dictionary<string, string>
-            {
-                ["country"] = "rest of the text{header:end}",
-            },
-        };
-        GivenTheFollowingFileRoute(fileRoute);
-
-        // Act
-        WhenICreateTheTemplatePattern();
-
-        // Assert
-        ThenTheFollowingIsReturned("country", "^(?i)rest of the text(?<end>.+)$");
-    }
-
-    [Fact]
-    public void Should_create_pattern_with_placeholder_only()
-    {
-        // Arrange
-        var fileRoute = new FileRoute
-        {
-            UpstreamHeaderTemplates = new Dictionary<string, string>
-            {
-                ["country"] = "{header:countrycode}",
+                [key] = template,
             },
         };
 
-        GivenTheFollowingFileRoute(fileRoute);
-
         // Act
-        WhenICreateTheTemplatePattern();
+        var actual = _creator.Create(fileRoute);
 
         // Assert
-        ThenTheFollowingIsReturned("country", "^(?i)(?<countrycode>.+)$");
-    }
-
-    [Fact]
-    public void Should_crate_pattern_with_more_placeholders()
-    {
-        // Arrange
-        var fileRoute = new FileRoute
-        {
-            UpstreamHeaderTemplates = new Dictionary<string, string>
-            {
-                ["country"] = "any text {header:cc} and other {header:version} and {header:bob} the end",
-            },
-        };
-        GivenTheFollowingFileRoute(fileRoute);
-
-        // Act
-        WhenICreateTheTemplatePattern();
-
-        // Assert
-        ThenTheFollowingIsReturned("country", "^(?i)any text (?<cc>.+) and other (?<version>.+) and (?<bob>.+) the end$");
-    }
-
-    private void GivenTheFollowingFileRoute(FileRoute fileRoute)
-    {
-        _fileRoute = fileRoute;
-    }
-
-    private void WhenICreateTheTemplatePattern()
-    {
-        _result = _creator.Create(_fileRoute);
-    }
-
-    private void ThenTheFollowingIsReturned(string headerKey, string expected)
-    {
-        _result[headerKey].Template.ShouldBe(expected);
+        var message = nameof(Create_WithUpstreamHeaderTemplates_ShouldCreatePattern).Replace('_', ' ') + withMessage;
+        actual[key].ShouldNotBeNull()
+            .Template.ShouldBe(expected, message);
     }
 }
