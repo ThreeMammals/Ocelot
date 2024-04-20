e1b5dc3f3fb2f964f247ee17d58ae32294541125
Raman Maksimchuk <dotnet044@gmail.com>
2024-04-20 16:34:51 +0300

    Initial refactoring

--- src/Ocelot.Provider.Kubernetes/Kube.cs
+++ src/Ocelot.Provider.Kubernetes/Kube.cs
@@ -12,34 +12,36 @@ public class Kube : IServiceDiscoveryProvider
     private readonly KubeRegistryConfiguration _kubeRegistryConfiguration;
     private readonly IOcelotLogger _logger;
     private readonly IKubeApiClient _kubeApi;
+    private readonly List<Service> _services;
 
     public Kube(KubeRegistryConfiguration kubeRegistryConfiguration, IOcelotLoggerFactory factory, IKubeApiClient kubeApi)
     {
         _kubeRegistryConfiguration = kubeRegistryConfiguration;
         _logger = factory.CreateLogger<Kube>();
         _kubeApi = kubeApi;
+        _services = new();
     }
 
-    public async Task<List<Service>> GetAsync()
+    public virtual async Task<List<Service>> GetAsync()
     {
         var endpoint = await _kubeApi
             .ResourceClient(client => new EndPointClientV1(client))
             .GetAsync(_kubeRegistryConfiguration.KeyOfServiceInK8s, _kubeRegistryConfiguration.KubeNamespace);
 
-        var services = new List<Service>();
-        if (endpoint != null && endpoint.Subsets.Any())
+        _services.Clear();
+        if (endpoint?.Subsets.Count != 0)
         {
-            services.AddRange(BuildServices(endpoint));
+            _services.AddRange(BuildServices(endpoint));
         }
         else
         {
-            _logger.LogWarning(() => $"namespace:{_kubeRegistryConfiguration.KubeNamespace}service:{_kubeRegistryConfiguration.KeyOfServiceInK8s} Unable to use ,it is invalid. Address must contain host only e.g. localhost and port must be greater than 0");
+            _logger.LogWarning(() => $"K8s Namespace:{_kubeRegistryConfiguration.KubeNamespace}, Service:{_kubeRegistryConfiguration.KeyOfServiceInK8s}; Unable to use: it is invalid. Address must contain host only e.g. localhost and port must be greater than 0!");
         }
 
-        return services;
+        return _services;
     }
 
-    private static List<Service> BuildServices(EndpointsV1 endpoint)
+    protected virtual List<Service> BuildServices(EndpointsV1 endpoint)
     {
         var services = new List<Service>();
 
