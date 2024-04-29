aae05450bee51a6d4242bd4c031f7738b55e3c99
Raman Maksimchuk <dotnet044@gmail.com>
2024-04-29 16:06:35 +0300

    Rate Limiting feature name should match folder name

---
+++ src/Ocelot/RateLimiting/IRateLimitCounterHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace Ocelot.RateLimit
+{
+    public interface IRateLimitCounterHandler
+    {
+        bool Exists(string id);
+
+        RateLimitCounter? Get(string id);
+
+        void Remove(string id);
+
+        void Set(string id, RateLimitCounter counter, TimeSpan expirationTime);
+    }
+}
