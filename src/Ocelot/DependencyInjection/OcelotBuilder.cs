668d7d12c4d3330ac75e17a6032d787feea3f510
Raman Maksimchuk <dotnet044@gmail.com>
2024-04-30 13:01:09 +0300

    Wrap services as a feature

--- src/Ocelot/DependencyInjection/OcelotBuilder.cs
+++ src/Ocelot/DependencyInjection/OcelotBuilder.cs
@@ -109,11 +109,7 @@ namespace Ocelot.DependencyInjection
             Services.TryAddSingleton<IDownstreamRouteProviderFactory, DownstreamRouteProviderFactory>();
             Services.TryAddSingleton<IHttpResponder, HttpContextResponder>();
             Services.TryAddSingleton<IErrorsToHttpStatusCodeMapper, ErrorsToHttpStatusCodeMapper>();
-
-            // Feature: Rate Limiting
-            Services.TryAddSingleton<IRateLimitCore, RateLimitCore>();
-            Services.TryAddSingleton<IRateLimitStorage, MemoryCacheRateLimitStorage>();
-
+            Services.AddRateLimiting(); // Feature: Rate Limiting
             Services.TryAddSingleton<IRequestMapper, RequestMapper>();
             Services.TryAddSingleton<IHttpHandlerOptionsCreator, HttpHandlerOptionsCreator>();
             Services.TryAddSingleton<IDownstreamAddressesCreator, DownstreamAddressesCreator>();
