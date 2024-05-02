7f2f575c14d48439eb35b83cd31200618562bc91
Guillaume Gnaegi <58469901+ggnaegi@users.noreply.github.com>
2024-05-02 23:53:49 +0200

    The tests are now passing again...

--- src/Ocelot/Configuration/Creator/MetadataCreator.cs
+++ src/Ocelot/Configuration/Creator/MetadataCreator.cs
@@ -7,9 +7,11 @@ public class MetadataCreator : IMetadataCreator
 {
     public MetadataOptions Create(IDictionary<string, string> metadata, FileGlobalConfiguration fileGlobalConfiguration)
     {
-        var mergedMetadata = fileGlobalConfiguration.MetadataOptions.Metadata.Any()
-            ? new Dictionary<string, string>(fileGlobalConfiguration.MetadataOptions.Metadata)
-            : new Dictionary<string, string>();
+        // metadata from the route could be null when no metadata is defined
+        metadata ??= new Dictionary<string, string>();
+
+        // metadata from the global configuration is never null
+        var mergedMetadata = new Dictionary<string, string>(fileGlobalConfiguration.MetadataOptions.Metadata);
 
         foreach (var (key, value) in metadata)
         {
