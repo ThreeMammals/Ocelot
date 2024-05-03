a3a2725beb5f10edeb7c053343633777cc03b944
Guillaume Gnaegi <58469901+ggnaegi@users.noreply.github.com>
2024-05-03 13:11:11 +0200

    adding latest test cases. That should be enough (includes global configuration changes too)

--- test/Ocelot.AcceptanceTests/DownstreamMetadataTests.cs
+++ test/Ocelot.AcceptanceTests/DownstreamMetadataTests.cs
@@ -2,6 +2,7 @@
 using Ocelot.Configuration;
 using Ocelot.Configuration.File;
 using Ocelot.Middleware;
+using System.Globalization;
 
 namespace Ocelot.AcceptanceTests;
 
@@ -9,6 +10,22 @@ public class DownstreamMetadataTests : IDisposable
 {
     private readonly Steps _steps;
     private readonly ServiceHandler _serviceHandler;
+
+    public enum StringArrayConfig
+    {
+        Default = 1,
+        AlternateSeparators,
+        AlternateTrimChars,
+        AlternateStringSplitOptions,
+        Mix,
+    }
+
+    public enum NumberConfig
+    {
+        Default = 1,
+        AlternateNumberStyle,
+        AlternateCulture,
+    }
 
     public DownstreamMetadataTests()
     {
@@ -59,13 +76,134 @@ public class DownstreamMetadataTests : IDisposable
             .BDDfy();
     }
 
+    /// <summary>
+    /// Testing the string array type with different configurations.
+    /// </summary>
+    /// <param name="separators">The possible separators.</param>
+    /// <param name="trimChars">The trimmed characters.</param>
+    /// <param name="stringSplitOption">If the empty entries should be removed.</param>
+    /// <param name="currentConfig">The current test configuration.</param>
+    [Theory]
+    [InlineData(new[] { "," }, new[] { ' ' }, nameof(StringSplitOptions.None), StringArrayConfig.Default)]
+    [InlineData(
+        new[] { ";", ".", "," },
+        new[] { ' ' },
+        nameof(StringSplitOptions.None),
+        StringArrayConfig.AlternateSeparators)]
+    [InlineData(
+        new[] { "," },
+        new[] { ' ', ';', ':' },
+        nameof(StringSplitOptions.None),
+        StringArrayConfig.AlternateTrimChars)]
+    [InlineData(
+        new[] { "," },
+        new[] { ' ' },
+        nameof(StringSplitOptions.RemoveEmptyEntries),
+        StringArrayConfig.AlternateStringSplitOptions)]
+    [InlineData(
+        new[] { ";", ".", "," },
+        new[] { ' ', '_', ':' },
+        nameof(StringSplitOptions.RemoveEmptyEntries),
+        StringArrayConfig.Mix)]
+    public void ShouldMatchTargetStringArrayAccordingToConfiguration(
+        string[] separators,
+        char[] trimChars,
+        string stringSplitOption,
+        StringArrayConfig currentConfig)
+    {
+        (Dictionary<string, string> sourceDictionary, Dictionary<string, string[]> _) =
+            GetSourceAndTargetDictionariesForStringArrayType(currentConfig);
+
+        sourceDictionary.Add(nameof(StringArrayConfig), currentConfig.ToString());
+
+        var port = PortFinder.GetRandomPort();
+        var configuration = new FileConfiguration
+        {
+            Routes = new List<FileRoute>
+            {
+                new()
+                {
+                    DownstreamPathTemplate = "/",
+                    DownstreamHostAndPorts = new List<FileHostAndPort> { new() { Host = "localhost", Port = port, }, },
+                    DownstreamScheme = "http",
+                    UpstreamPathTemplate = "/",
+                    UpstreamHttpMethod = new List<string> { "Get" },
+                    Metadata = sourceDictionary,
+                    DelegatingHandlers = new List<string> { nameof(StringArrayDownStreamMetadataHandler) },
+                },
+            },
+            GlobalConfiguration = new FileGlobalConfiguration
+            {
+                MetadataOptions = new FileMetadataOptions
+                {
+                    Separators = separators, TrimChars = trimChars, StringSplitOption = stringSplitOption,
+                },
+            },
+        };
+
+        this.Given(x => x.GivenThereIsAServiceRunningOn($"http://localhost:{port}"))
+            .And(x => _steps.GivenThereIsAConfiguration(configuration))
+            .And(x => x.GivenOcelotIsRunningWithSpecificHandlerForType(typeof(StringArrayDownStreamMetadataHandler)))
+            .When(x => _steps.WhenIGetUrlOnTheApiGateway($"/"))
+            .Then(x => _steps.ThenTheStatusCodeShouldBe(HttpStatusCode.OK))
+            .BDDfy();
+    }
+
+    [Theory]
+    [InlineData(NumberStyles.Any, "de-CH", NumberConfig.Default)]
+    [InlineData(NumberStyles.AllowParentheses | NumberStyles.AllowLeadingWhite | NumberStyles.AllowTrailingWhite | NumberStyles.AllowLeadingSign, "de-CH", NumberConfig.AlternateNumberStyle)]
+    public void ShouldMatchTargetNumberAccordingToConfiguration(
+        NumberStyles numberStyles,
+        string cultureName,
+        NumberConfig currentConfig)
+    {
+        (Dictionary<string, string> sourceDictionary, Dictionary<string, int> _) =
+            GetSourceAndTargetDictionariesForNumberType();
+
+        sourceDictionary.Add(nameof(NumberConfig), currentConfig.ToString());
+
+        var port = PortFinder.GetRandomPort();
+        var configuration = new FileConfiguration
+        {
+            Routes = new List<FileRoute>
+            {
+                new()
+                {
+                    DownstreamPathTemplate = "/",
+                    DownstreamHostAndPorts = new List<FileHostAndPort> { new() { Host = "localhost", Port = port, }, },
+                    DownstreamScheme = "http",
+                    UpstreamPathTemplate = "/",
+                    UpstreamHttpMethod = new List<string> { "Get" },
+                    Metadata = sourceDictionary,
+                    DelegatingHandlers = new List<string> { nameof(IntDownStreamMetadataHandler) },
+                },
+            },
+            GlobalConfiguration = new FileGlobalConfiguration
+            {
+                MetadataOptions = new FileMetadataOptions
+                {
+                    NumberStyle = numberStyles.ToString(), CurrentCulture = cultureName,
+                },
+            },
+        };
+
+        this.Given(x => x.GivenThereIsAServiceRunningOn($"http://localhost:{port}"))
+            .And(x => _steps.GivenThereIsAConfiguration(configuration))
+            .And(x => x.GivenOcelotIsRunningWithSpecificHandlerForType(typeof(IntDownStreamMetadataHandler)))
+            .When(x => _steps.WhenIGetUrlOnTheApiGateway($"/"))
+            .Then(x => _steps.ThenTheStatusCodeShouldBe(HttpStatusCode.OK))
+            .BDDfy();
+    }
+
     private void GivenThereIsAServiceRunningOn(string url)
     {
-        _serviceHandler.GivenThereIsAServiceRunningOn(url, context =>
-        {
-            context.Response.StatusCode = 200;
-            return Task.CompletedTask;
-        });
+        _serviceHandler.GivenThereIsAServiceRunningOn(
+            url,
+            context =>
+            {
+                context.Response.StatusCode = 200;
+                return Task.CompletedTask;
+            });
     }
 
     /// <summary>
@@ -75,29 +213,30 @@ public class DownstreamMetadataTests : IDisposable
     /// <exception cref="NotImplementedException">Throws if delegating handler type doesn't match.</exception>
     private void GivenOcelotIsRunningWithSpecificHandlerForType(Type currentType)
     {
-        if (currentType == typeof(StringDownStreamMetadataHandler))
+        switch (currentType)
         {
-            _steps.GivenOcelotIsRunningWithSpecificHandlerRegisteredInDi<StringDownStreamMetadataHandler>();
-        }
-        else if (currentType == typeof(StringArrayDownStreamMetadataHandler))
-        {
-            _steps.GivenOcelotIsRunningWithSpecificHandlerRegisteredInDi<StringArrayDownStreamMetadataHandler>();
-        }
-        else if (currentType == typeof(BoolDownStreamMetadataHandler))
-        {
-            _steps.GivenOcelotIsRunningWithSpecificHandlerRegisteredInDi<BoolDownStreamMetadataHandler>();
-        }
-        else if (currentType == typeof(DoubleDownStreamMetadataHandler))
-        {
-            _steps.GivenOcelotIsRunningWithSpecificHandlerRegisteredInDi<DoubleDownStreamMetadataHandler>();
-        }
-        else if (currentType == typeof(SuperDataContainerDownStreamMetadataHandler))
-        {
-            _steps.GivenOcelotIsRunningWithSpecificHandlerRegisteredInDi<SuperDataContainerDownStreamMetadataHandler>();
-        }
-        else
-        {
-            throw new NotImplementedException();
+            case { } t when t == typeof(StringDownStreamMetadataHandler):
+                _steps.GivenOcelotIsRunningWithSpecificHandlerRegisteredInDi<StringDownStreamMetadataHandler>();
+                break;
+            case { } t when t == typeof(StringArrayDownStreamMetadataHandler):
+                _steps.GivenOcelotIsRunningWithSpecificHandlerRegisteredInDi<StringArrayDownStreamMetadataHandler>();
+                break;
+            case { } t when t == typeof(BoolDownStreamMetadataHandler):
+                _steps.GivenOcelotIsRunningWithSpecificHandlerRegisteredInDi<BoolDownStreamMetadataHandler>();
+                break;
+            case { } t when t == typeof(DoubleDownStreamMetadataHandler):
+                _steps.GivenOcelotIsRunningWithSpecificHandlerRegisteredInDi<DoubleDownStreamMetadataHandler>();
+                break;
+            case { } t when t == typeof(SuperDataContainerDownStreamMetadataHandler):
+                _steps
+                    .GivenOcelotIsRunningWithSpecificHandlerRegisteredInDi<
+                        SuperDataContainerDownStreamMetadataHandler>();
+                break;
+            case { } t when t == typeof(IntDownStreamMetadataHandler):
+                _steps.GivenOcelotIsRunningWithSpecificHandlerRegisteredInDi<IntDownStreamMetadataHandler>();
+                break;
+            default:
+                throw new NotImplementedException();
         }
     }
 
@@ -132,6 +271,13 @@ public class DownstreamMetadataTests : IDisposable
         }
     }
 
+    private class IntDownStreamMetadataHandler : DownstreamMetadataHandler<int>
+    {
+        public IntDownStreamMetadataHandler(IHttpContextAccessor httpContextAccessor) : base(httpContextAccessor)
+        {
+        }
+    }
+
     private class SuperDataContainerDownStreamMetadataHandler : DownstreamMetadataHandler<SuperDataContainer>
     {
         public SuperDataContainerDownStreamMetadataHandler(IHttpContextAccessor httpContextAccessor) : base(
@@ -154,21 +300,166 @@ public class DownstreamMetadataTests : IDisposable
             _httpContextAccessor = httpContextAccessor;
         }
 
-        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+        protected override Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
             CancellationToken cancellationToken)
         {
             var downstreamRoute = _httpContextAccessor.HttpContext?.Items.DownstreamRoute();
 
-            (Dictionary<string, string> _, Dictionary<string, object> targetDictionary) =
-                GetSourceAndTargetDictionary(typeof(T));
-
-            foreach (var key in targetDictionary.Keys)
+            if (downstreamRoute.MetadataOptions.Metadata.ContainsKey(nameof(StringArrayConfig)))
             {
-                Assert.Equal(targetDictionary[key], downstreamRoute.GetMetadata<T>(key));
+                var currentConfig =
+                    Enum.Parse<StringArrayConfig>(downstreamRoute.MetadataOptions.Metadata[nameof(StringArrayConfig)]);
+                downstreamRoute.MetadataOptions.Metadata.Remove(nameof(StringArrayConfig));
+
+                (Dictionary<string, string> _, Dictionary<string, string[]> targetDictionary) =
+                    GetSourceAndTargetDictionariesForStringArrayType(currentConfig);
+
+                foreach (var key in targetDictionary.Keys)
+                {
+                    Assert.Equal(targetDictionary[key], downstreamRoute.GetMetadata<string[]>(key));
+                }
+            }
+            else if (downstreamRoute.MetadataOptions.Metadata.ContainsKey(nameof(NumberConfig)))
+            {
+                downstreamRoute.MetadataOptions.Metadata.Remove(nameof(NumberConfig));
+
+                (Dictionary<string, string> _, Dictionary<string, int> targetDictionary) =
+                    GetSourceAndTargetDictionariesForNumberType();
+
+                foreach (var key in targetDictionary.Keys)
+                {
+                    Assert.Equal(targetDictionary[key], downstreamRoute.GetMetadata<double>(key));
+                }
+            }
+            else
+            {
+                (Dictionary<string, string> _, Dictionary<string, object> targetDictionary) =
+                    GetSourceAndTargetDictionary(typeof(T));
+
+                foreach (var key in targetDictionary.Keys)
+                {
+                    Assert.Equal(targetDictionary[key], downstreamRoute.GetMetadata<T>(key));
+                }
             }
 
             return base.SendAsync(request, cancellationToken);
         }
+    }
+
+    public static (Dictionary<string, string> SourceDictionary, Dictionary<string, string[]> TargetDictionary)
+        GetSourceAndTargetDictionariesForStringArrayType(StringArrayConfig currentConfig)
+    {
+        Dictionary<string, string> sourceDictionary;
+        Dictionary<string, string[]> targetDictionary;
+
+        if (currentConfig == StringArrayConfig.Default)
+        {
+            sourceDictionary = new Dictionary<string, string>
+            {
+                { "Key1", "Value1, Value2, Value3" },
+                { "Key2", "Value2, Value3, Value4" },
+                { "Key3", "Value3, ,Value4, Value5" },
+            };
+
+            targetDictionary = new Dictionary<string, string[]>
+            {
+                { "Key1", new[] { "Value1", "Value2", "Value3" } },
+                { "Key2", new[] { "Value2", "Value3", "Value4" } },
+                { "Key3", new[] { "Value3", "Value4", "Value5" } },
+            };
+
+            return (sourceDictionary, targetDictionary);
+        }
+
+        if (currentConfig == StringArrayConfig.AlternateSeparators)
+        {
+            sourceDictionary = new Dictionary<string, string>
+            {
+                { "Key1", "Value1; Value2. Value3" },
+                { "Key2", "Value2. Value3, Value4" },
+                { "Key3", "Value3, ,Value4; Value5" },
+            };
+
+            targetDictionary = new Dictionary<string, string[]>
+            {
+                { "Key1", new[] { "Value1", "Value2", "Value3" } },
+                { "Key2", new[] { "Value2", "Value3", "Value4" } },
+                { "Key3", new[] { "Value3", "Value4", "Value5" } },
+            };
+
+            return (sourceDictionary, targetDictionary);
+        }
+
+        if (currentConfig == StringArrayConfig.AlternateTrimChars)
+        {
+            sourceDictionary = new Dictionary<string, string>
+            {
+                { "Key1", "Value1; :, Value2 :, Value3 " },
+                { "Key2", " Value2, Value3; , Value4" },
+                { "Key3", "Value3 , ,Value4, Value5 " },
+            };
+
+            targetDictionary = new Dictionary<string, string[]>
+            {
+                { "Key1", new[] { "Value1", "Value2", "Value3" } },
+                { "Key2", new[] { "Value2", "Value3", "Value4" } },
+                { "Key3", new[] { "Value3", "Value4", "Value5" } },
+            };
+
+            return (sourceDictionary, targetDictionary);
+        }
+
+        if (currentConfig == StringArrayConfig.AlternateStringSplitOptions)
+        {
+            sourceDictionary = new Dictionary<string, string>
+            {
+                { "Key1", "Value1, ,Value2, Value3, " },
+                { "Key2", "Value2, , ,Value3, Value4, , ," },
+                { "Key3", "Value3, ,Value4, , ,Value5" },
+            };
+
+            targetDictionary = new Dictionary<string, string[]>
+            {
+                { "Key1", new[] { "Value1", "Value2", "Value3" } },
+                { "Key2", new[] { "Value2", "Value3", "Value4" } },
+                { "Key3", new[] { "Value3", "Value4", "Value5" } },
+            };
+
+            return (sourceDictionary, targetDictionary);
+        }
+
+        if (currentConfig == StringArrayConfig.Mix)
+        {
+            sourceDictionary = new Dictionary<string, string>
+            {
+                { "Key1", "Value1; :, Value2. :, Value3 " },
+                { "Key2", " Value2_, , , Value3; , Value4" },
+                { "Key3", "Value3:; , ,Value4, Value5 " },
+            };
+
+            targetDictionary = new Dictionary<string, string[]>
+            {
+                { "Key1", new[] { "Value1", "Value2", "Value3" } },
+                { "Key2", new[] { "Value2", "Value3", "Value4" } },
+                { "Key3", new[] { "Value3", "Value4", "Value5" } },
+            };
+
+            return (sourceDictionary, targetDictionary);
+        }
+
+        throw new NotImplementedException();
+    }
+
+    public static (Dictionary<string, string> SourceDictionary, Dictionary<string, int> TargetDictionary)
+        GetSourceAndTargetDictionariesForNumberType()
+    {
+        return (
+            new Dictionary<string, string>
+            {
+                { "Key1", "-2" }, { "Key2", " (1000000) " }, { "Key3", "-1000000000  " },
+            },
+            new Dictionary<string, int> { { "Key1", -2 }, { "Key2", -1000000 }, { "Key3", -1000000000 } });
     }
 
     /// <summary>
@@ -255,7 +546,8 @@ public class DownstreamMetadataTests : IDisposable
             return (sourceDictionary, targetDictionary);
         }
 
-        if (currentType == typeof(SuperDataContainerDownStreamMetadataHandler) || currentType == typeof(SuperDataContainer))
+        if (currentType == typeof(SuperDataContainerDownStreamMetadataHandler) ||
+            currentType == typeof(SuperDataContainer))
         {
             sourceDictionary = new Dictionary<string, string>
             {
@@ -265,13 +557,9 @@ public class DownstreamMetadataTests : IDisposable
             targetDictionary = new Dictionary<string, object>
             {
                 {
-                    "Key1",
-                    new SuperDataContainer
+                    "Key1", new SuperDataContainer
                     {
-                        Key1 = "Bonjour",
-                        Key2 = "Hello",
-                        Key3 = 0.00001,
-                        Key4 = true,
+                        Key1 = "Bonjour", Key2 = "Hello", Key3 = 0.00001, Key4 = true,
                     }
                 },
             };
@@ -285,8 +573,11 @@ public class DownstreamMetadataTests : IDisposable
     public class SuperDataContainer
     {
         public string Key1 { get; set; }
+
         public string Key2 { get; set; }
+
         public double Key3 { get; set; }
+
         public bool? Key4 { get; set; }
 
         public override bool Equals(object obj)
