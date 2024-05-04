8b72797f5aa6f679637093faa1e6a42bd477b11a
Jolanta Łukawska <jolanta.lukawska@outlook.com>
2024-05-04 21:09:16 +0200

    refactor of ClientRateLimitTests

--- test/Ocelot.AcceptanceTests/Steps.cs
+++ test/Ocelot.AcceptanceTests/Steps.cs
@@ -8,7 +8,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Net.Http.Headers;
 using Newtonsoft.Json;
 using Ocelot.AcceptanceTests.Caching;
 using Ocelot.Cache.CacheManager;
