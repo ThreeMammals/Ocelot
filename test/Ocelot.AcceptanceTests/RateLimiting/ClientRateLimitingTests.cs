035fa13afd3006790789732b17affe8ed27a7847
Raman Maksimchuk <dotnet044@gmail.com>
2024-05-06 21:45:50 +0300

    Acceptance test for #1590 user scenario

--- test/Ocelot.AcceptanceTests/RateLimiting/ClientRateLimitingTests.cs
+++ test/Ocelot.AcceptanceTests/RateLimiting/ClientRateLimitingTests.cs
@@ -27,7 +27,7 @@ public sealed class ClientRateLimitingTests : Steps, IDisposable
     public void Should_call_with_rate_limiting()
     {
         var port = PortFinder.GetRandomPort();
-        var route = GivenRoute(port, null, null, new(), 3, "1s", 1000);
+        var route = GivenRoute(port, null, null, new(), 3, "1s", 1); // periods are equal
         var configuration = GivenConfigurationWithRateLimitOptions(route);
         this.Given(x => x.GivenThereIsAServiceRunningOn(DownstreamUrl(port), "/api/ClientRateLimit"))
             .And(x => GivenThereIsAConfiguration(configuration))
@@ -52,17 +52,17 @@ public sealed class ClientRateLimitingTests : Steps, IDisposable
         this.Given(x => x.GivenThereIsAServiceRunningOn(DownstreamUrl(port), "/api/ClientRateLimit"))
             .And(x => GivenThereIsAConfiguration(configuration))
             .And(x => GivenOcelotIsRunning())
-            .When(x => x.WhenIGetUrlOnTheApiGatewayMultipleTimesForRateLimit(() => $"/ClientRateLimit/?{Count()}", 1))
+            .When(x => x.WhenIGetUrlOnTheApiGatewayMultipleTimesForRateLimit(Url, 1))
             .Then(x => ThenTheStatusCodeShouldBe(OK))
-            .When(x => x.WhenIGetUrlOnTheApiGatewayMultipleTimesForRateLimit(() => $"/ClientRateLimit/?{Count()}", 2))
+            .When(x => x.WhenIGetUrlOnTheApiGatewayMultipleTimesForRateLimit(Url, 2))
             .Then(x => ThenTheStatusCodeShouldBe(OK))
-            .When(x => x.WhenIGetUrlOnTheApiGatewayMultipleTimesForRateLimit(() => $"/ClientRateLimit/?{Count()}", 1))
+            .When(x => x.WhenIGetUrlOnTheApiGatewayMultipleTimesForRateLimit(Url, 1))
             .Then(x => ThenTheStatusCodeShouldBe(TooManyRequests))
             .And(x => GivenIWait(1000))
-            .When(x => x.WhenIGetUrlOnTheApiGatewayMultipleTimesForRateLimit(() => $"/ClientRateLimit/?{Count()}", 1))
+            .When(x => x.WhenIGetUrlOnTheApiGatewayMultipleTimesForRateLimit(Url, 1))
             .Then(x => ThenTheStatusCodeShouldBe(TooManyRequests))
             .And(x => GivenIWait(1000))
-            .When(x => x.WhenIGetUrlOnTheApiGatewayMultipleTimesForRateLimit(() => $"/ClientRateLimit/?{Count()}", 1))
+            .When(x => x.WhenIGetUrlOnTheApiGatewayMultipleTimesForRateLimit(Url, 1))
             .Then(x => ThenTheStatusCodeShouldBe(OK))
             .And(x => ThenTheResponseBodyShouldBe("4")) // total 4 OK responses
             .BDDfy();
@@ -70,9 +70,11 @@ public sealed class ClientRateLimitingTests : Steps, IDisposable
 
     private int _count = 0;
     private int Count() => ++_count;
-    private void WhenIGetUrlOnTheApiGatewayMultipleTimesForRateLimit(Func<string> urlDelegate, int times)
+    private string Url() => $"/ClientRateLimit/?{Count()}";
+
+    private void WhenIGetUrlOnTheApiGatewayMultipleTimesForRateLimit(Func<string> urlDelegate, long times)
     {
-        for (int i = 0; i < times; i++)
+        for (long i = 0; i < times; i++)
         {
             var url = urlDelegate.Invoke();
             WhenIGetUrlOnTheApiGatewayMultipleTimesForRateLimit(url, 1);
@@ -84,13 +86,47 @@ public sealed class ClientRateLimitingTests : Steps, IDisposable
     public void Should_call_middleware_with_white_list_client()
     {
         var port = PortFinder.GetRandomPort();
-        var route = GivenRoute(port, null, null, whitelist: new() { "ocelotclient1" }, 3, "1s", 100);
+        var route = GivenRoute(port, null, null, whitelist: new() { "ocelotclient1" }, 3, "3s", 2); // main period is greater than ban one
         var configuration = GivenConfigurationWithRateLimitOptions(route);
         this.Given(x => x.GivenThereIsAServiceRunningOn(DownstreamUrl(port), "/api/ClientRateLimit"))
             .And(x => GivenThereIsAConfiguration(configuration))
             .And(x => GivenOcelotIsRunning())
             .When(x => WhenIGetUrlOnTheApiGatewayMultipleTimesForRateLimit("/api/ClientRateLimit", 4))
             .Then(x => ThenTheStatusCodeShouldBe(OK))
+            .BDDfy();
+    }
+
+    [Fact]
+    [Trait("Bug", "1590")]
+    public void StatusShouldNotBeEqualTo429_PeriodTimespanValueIsGreaterThanPeriod()
+    {
+        _counterOne = 0;
+
+        // Bug scenario
+        const string period = "1s";
+        const double periodTimespan = /*30*/3; // but decrease 30 to 3 secs, "no wasting time" life hack
+        const long limit = 100L;
+
+        var port = PortFinder.GetRandomPort();
+        var route = GivenRoute(port, "/api/ClientRateLimit?count={count}", "/ClientRateLimit/?{count}", new(),
+            limit, period, periodTimespan); // bug scenario, adapted
+        var configuration = GivenConfigurationWithRateLimitOptions(route);
+        this.Given(x => x.GivenThereIsAServiceRunningOn(DownstreamUrl(port), "/api/ClientRateLimit"))
+            .And(x => GivenThereIsAConfiguration(configuration))
+            .And(x => GivenOcelotIsRunning())
+
+            // main scenario
+            .When(x => x.WhenIGetUrlOnTheApiGatewayMultipleTimesForRateLimit(Url, route.RateLimitOptions.Limit)) // 100 times to reach the limit
+            .Then(x => ThenTheStatusCodeShouldBe(OK))
+            .And(x => ThenTheResponseBodyShouldBe(route.RateLimitOptions.Limit.ToString())) // total 100 OK responses
+
+            // extra scenario
+            .When(x => x.WhenIGetUrlOnTheApiGatewayMultipleTimesForRateLimit(Url, 1)) // 101st request should fail
+            .Then(x => ThenTheStatusCodeShouldBe(TooManyRequests))
+            .And(x => GivenIWait((int)TimeSpan.FromSeconds(route.RateLimitOptions.PeriodTimespan).TotalMilliseconds)) // in 3 secs PeriodTimespan will elapse
+            .When(x => x.WhenIGetUrlOnTheApiGatewayMultipleTimesForRateLimit(Url, 1))
+            .Then(x => ThenTheStatusCodeShouldBe(OK))
+            .And(x => ThenTheResponseBodyShouldBe("101")) // total 101 OK responses
             .BDDfy();
     }
 
