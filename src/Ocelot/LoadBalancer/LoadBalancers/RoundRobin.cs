fbd0d8d2a124ea2b149e0543353872e4012aa6ee
Raman Maksimchuk <dotnet044@gmail.com>
2024-07-11 14:32:53 +0300

    Code review by @raman-m

--- src/Ocelot/LoadBalancer/LoadBalancers/RoundRobin.cs
+++ src/Ocelot/LoadBalancer/LoadBalancers/RoundRobin.cs
@@ -19,37 +19,38 @@ namespace Ocelot.LoadBalancer.LoadBalancers
         public async Task<Response<ServiceHostAndPort>> Lease(HttpContext httpContext)
         {
             var services = await _servicesDelegate?.Invoke() ?? new List<Service>();
-
-            if (services?.Count != 0)
+            if (services.Count == 0)
             {
-                lock (_lock)
-                {
-                    if (_last >= services.Count)
-                    {
-                        _last = 0;
-                    }
-
-                    var next = services[_last++];
-
-                    if (next == null)
-                    {
-                        return new ErrorResponse<ServiceHostAndPort>(new ServicesAreNullError($"The service with index {_last} was null in {nameof(RoundRobin)} during the {nameof(Lease)} operation. Total services count: {services.Count}."));
-                    }
-
-                    if (next.HostAndPort == null)
-                    {
-                        return new ErrorResponse<ServiceHostAndPort>(new ServicesAreNullError($"The {nameof(next.HostAndPort)} was null in the service with index {_last} in {nameof(RoundRobin)} during the {nameof(Lease)} operation. Total services count: {services.Count}."));
-                    }
-
-                    return new OkResponse<ServiceHostAndPort>(next.HostAndPort);
-                }
+                return new ErrorResponse<ServiceHostAndPort>(new ServicesAreEmptyError($"There were no services in {nameof(RoundRobin)} during {nameof(Lease)} operation."));
             }
 
-            return new ErrorResponse<ServiceHostAndPort>(new ServicesAreEmptyError($"There were no services in {nameof(RoundRobin)} during {nameof(Lease)} operation."));
+            lock (_lock)
+            {
+                int count = services.Count; // capture the count value because another thread might modify the list
+                if (_last >= count)
+                {
+                    _last = 0;
+                }
+
+                var next = services[_last];
+                if (next == null)
+                {
+                    return new ErrorResponse<ServiceHostAndPort>(new ServicesAreNullError($"The service at index {_last} was null in {nameof(RoundRobin)} during the {nameof(Lease)} operation. Total services count: {count}."));
+                }
+                else if (next.HostAndPort == null)
+                {
+                    return new ErrorResponse<ServiceHostAndPort>(new ServicesAreNullError($"The {nameof(next.HostAndPort)} was null for the service at index {_last} in {nameof(RoundRobin)} during the {nameof(Lease)} operation. Total services count: {count}."));
+                }
+
+                // Happy path: Lease now
+                _last++; // the Lease has been successful; advance the index.
+                return new OkResponse<ServiceHostAndPort>(next.HostAndPort);
+            }
         }
 
         public void Release(ServiceHostAndPort hostAndPort)
         {
+            // TODO Add lease map
         }
     }
 }
--- test/Ocelot.UnitTests/LoadBalancer/RoundRobinTests.cs
+++ test/Ocelot.UnitTests/LoadBalancer/RoundRobinTests.cs
@@ -4,97 +4,80 @@ using Ocelot.Responses;
 using Ocelot.Values;
 using System.Diagnostics;
 
-namespace Ocelot.UnitTests.LoadBalancer
+namespace Ocelot.UnitTests.LoadBalancer;
+
+public class RoundRobinTests : UnitTest
 {
-    public class RoundRobinTests : UnitTest
+    private readonly HttpContext _httpContext;
+
+    public RoundRobinTests()
     {
-        private readonly RoundRobin _roundRobin;
-        private List<Service> _services;
-        private Response<ServiceHostAndPort> _hostAndPort;
-        private readonly HttpContext _httpContext;
+        _httpContext = new DefaultHttpContext();
+    }
 
-        public RoundRobinTests()
+    [Fact]
+    public void Should_get_next_address()
+    {
+        var services = GivenServices();
+        var roundRobin = GivenLoadBalancer(services);
+        WhenIGetTheNextAddress(roundRobin).Data.ShouldNotBeNull().ShouldBe(services[0].HostAndPort);
+        WhenIGetTheNextAddress(roundRobin).Data.ShouldNotBeNull().ShouldBe(services[1].HostAndPort);
+        WhenIGetTheNextAddress(roundRobin).Data.ShouldNotBeNull().ShouldBe(services[2].HostAndPort);
+    }
+
+    [Fact]
+    [Trait("Feat", "336")]
+    public void Should_go_back_to_first_address_after_finished_last()
+    {
+        var services = GivenServices();
+        var roundRobin = GivenLoadBalancer(services);
+        var stopWatch = Stopwatch.StartNew();
+        while (stopWatch.ElapsedMilliseconds < 1000)
         {
-            _httpContext = new DefaultHttpContext();
-            _services = new List<Service>
-            {
-                new("product", new ServiceHostAndPort("127.0.0.1", 5000), string.Empty, string.Empty, Array.Empty<string>()),
-                new("product", new ServiceHostAndPort("127.0.0.1", 5001), string.Empty, string.Empty, Array.Empty<string>()),
-                new("product", new ServiceHostAndPort("127.0.0.1", 5001), string.Empty, string.Empty, Array.Empty<string>()),
-            };
-
-            _roundRobin = new RoundRobin(() => Task.FromResult(_services));
+            WhenIGetTheNextAddress(roundRobin).Data.ShouldNotBeNull().ShouldBe(services[0].HostAndPort);
+            WhenIGetTheNextAddress(roundRobin).Data.ShouldNotBeNull().ShouldBe(services[1].HostAndPort);
+            WhenIGetTheNextAddress(roundRobin).Data.ShouldNotBeNull().ShouldBe(services[2].HostAndPort);
         }
+    }
 
-        [Fact]
-        public void should_get_next_address()
-        {
-            this.Given(x => x.GivenIGetTheNextAddress())
-                .Then(x => x.ThenTheNextAddressIndexIs(0))
-                .Given(x => x.GivenIGetTheNextAddress())
-                .Then(x => x.ThenTheNextAddressIndexIs(1))
-                .Given(x => x.GivenIGetTheNextAddress())
-                .Then(x => x.ThenTheNextAddressIndexIs(2))
-                .BDDfy();
-        }
+    [Fact]
+    [Trait("Bug", "2110")]
+    public void Should_return_error_if_selected_service_is_null()
+    {
+        var invalidServices = new List<Service> { null };
+        var roundRobin = GivenLoadBalancer(invalidServices);
+        var response = WhenIGetTheNextAddress(roundRobin);
+        ThenServicesAreNullErrorIsReturned(response);
+    }
 
-        [Fact]
-        public async Task should_go_back_to_first_address_after_finished_last()
-        {
-            var stopWatch = Stopwatch.StartNew();
+    [Fact]
+    [Trait("Bug", "2110")]
+    public void Should_return_error_if_host_and_port_is_null_in_the_selected_service()
+    {
+        var invalidService = new Service(string.Empty, null, string.Empty, string.Empty, new List<string>());
+        var services = new List<Service> { invalidService };
+        var roundRobin = GivenLoadBalancer(services);
+        var response = WhenIGetTheNextAddress(roundRobin);
+        ThenServicesAreNullErrorIsReturned(response);
+    }
 
-            while (stopWatch.ElapsedMilliseconds < 1000)
-            {
-                var address = await _roundRobin.Lease(_httpContext);
-                address.Data.ShouldBe(_services[0].HostAndPort);
-                address = await _roundRobin.Lease(_httpContext);
-                address.Data.ShouldBe(_services[1].HostAndPort);
-                address = await _roundRobin.Lease(_httpContext);
-                address.Data.ShouldBe(_services[2].HostAndPort);
-            }
-        }
+    private static List<Service> GivenServices() => new()
+    {
+        new("product", new ServiceHostAndPort("127.0.0.1", 5000), string.Empty, string.Empty, Array.Empty<string>()),
+        new("product", new ServiceHostAndPort("127.0.0.1", 5001), string.Empty, string.Empty, Array.Empty<string>()),
+        new("product", new ServiceHostAndPort("127.0.0.1", 5002), string.Empty, string.Empty, Array.Empty<string>()),
+    };
 
-        [Fact]
-        public void should_return_error_if_selected_service_is_null()
-        {
-            Service invalidService = null;
+    private static RoundRobin GivenLoadBalancer(List<Service> services = null)
+        => new(() => Task.FromResult(services));
 
-            this.Given(x => x.GivenServices(new List<Service> { invalidService }))
-               .And(x => x.GivenIGetTheNextAddress())
-               .Then(x => x.ThenServiceAreNullErrorIsReturned())
-               .BDDfy();
-        }
+    private Response<ServiceHostAndPort> WhenIGetTheNextAddress(RoundRobin roundRobin)
+        => roundRobin.Lease(_httpContext).Result;
 
-        [Fact]
-        public void should_return_error_if_host_and_port_is_null_in_the_selected_service()
-        {
-            var invalidService = new Service(string.Empty, null, string.Empty, string.Empty, new List<string>());
-
-            this.Given(x => x.GivenServices(new List<Service> { invalidService }))
-               .And(x => x.GivenIGetTheNextAddress())
-               .Then(x => x.ThenServiceAreNullErrorIsReturned())
-               .BDDfy();
-        }
-
-        private void GivenIGetTheNextAddress()
-        {
-            _hostAndPort = _roundRobin.Lease(_httpContext).Result;
-        }
-
-        private void GivenServices(List<Service> services)
-        {
-            _services = services;
-        }
-
-        private void ThenTheNextAddressIndexIs(int index)
-        {
-            _hostAndPort.Data.ShouldBe(_services[index].HostAndPort);
-        }
-
-        private void ThenServiceAreNullErrorIsReturned()
-        {
-            _hostAndPort.IsError.ShouldBeTrue();
-            _hostAndPort.Errors[0].ShouldBeOfType<ServicesAreNullError>();
-        }
+    private static void ThenServicesAreNullErrorIsReturned(Response<ServiceHostAndPort> response)
+    {
+        response.ShouldNotBeNull().Data.ShouldBeNull();
+        response.IsError.ShouldBeTrue();
+        response.Errors[0].ShouldBeOfType<ServicesAreNullError>();
     }
 }
