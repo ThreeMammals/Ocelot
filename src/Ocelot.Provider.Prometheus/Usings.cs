4ca1413289062f53f3681089879304e412d74f7f
Mohsen Rajabi <m.kabir8895@gmail.com>
2024-07-15 10:20:21 +0330

    feat: add prometheus

---
+++ src/Ocelot.Provider.Prometheus/Usings.cs
@@ -0,0 +1,6 @@
+﻿// Default Microsoft.NET.Sdk namespaces
+// Project extra global namespaces
+global using System;
+global using System.Net.Http;
+global using System.Threading;
+global using System.Threading.Tasks;
