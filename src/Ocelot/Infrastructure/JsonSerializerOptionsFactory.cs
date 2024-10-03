0fda6afd26c6935bcc597738b2ce648eaf6721fc
Mohsen Rajabi <m.kabir8895@gmail.com>
2024-10-04 00:18:26 +0330

    fix: rm JsonPath.Net

--- src/Ocelot/Infrastructure/JsonSerializerOptionsFactory.cs
+++ src/Ocelot/Infrastructure/JsonSerializerOptionsFactory.cs
@@ -24,4 +24,77 @@ public static class JsonSerializerOptionsFactory
         ReferenceHandler = ReferenceHandler.IgnoreCycles,
         WriteIndented = true
     };
+
+    public static List<string> ExtractValuesFromJsonPath(this JsonDocument document, string jsonPath)
+    {
+        var root = document.RootElement;
+
+        var pathParts = jsonPath.Trim('$', '.').Replace("[*]", "").Trim().Split('.');
+
+        var elements = new List<string>();
+
+        TraverseJsonPath(root, pathParts, 0, elements);
+
+        return elements;
+    }
+
+    private static void TraverseJsonPath(JsonElement currentElement, string[] pathParts, int index, List<string> elements)
+    {
+        if (index >= pathParts.Length) return;
+
+        var part = pathParts[index];
+
+        if (currentElement.ValueKind == JsonValueKind.Array)
+        {
+            foreach (var element in currentElement.EnumerateArray())
+            {
+                if (element.ValueKind == JsonValueKind.Object)
+                {
+                    if (element.TryGetProperty(part, out JsonElement nextElement))
+                    {
+                        if (nextElement.ValueKind == JsonValueKind.Array)
+                        {
+                            TraverseJsonPath(nextElement, pathParts, 0, elements);
+                        }
+                        else
+                        {
+                            var item = nextElement.ToString();
+                            if (!string.IsNullOrWhiteSpace(item))
+                                elements.Add(item);
+                        }
+                    }
+                    else
+                    {
+                        TraverseJsonPath(element, pathParts, index + 1, elements);
+                    }
+                }
+                else
+                {
+                    TraverseJsonPath(element, pathParts, index + 1, elements);
+                }
+            }
+        }
+        else if (currentElement.ValueKind == JsonValueKind.Object)
+        {
+            if (currentElement.TryGetProperty(part, out JsonElement nextElement))
+            {
+                if (nextElement.ValueKind == JsonValueKind.Array)
+                {
+                    TraverseJsonPath(nextElement, pathParts, index + 1, elements);
+                }
+                else
+                {
+                    var item = nextElement.ToString();
+                    if (!string.IsNullOrWhiteSpace(item))
+                        elements.Add(item);
+                }
+            }
+        }
+        else
+        {
+            var item = currentElement.ToString();
+            if (!string.IsNullOrWhiteSpace(item))
+                elements.Add(item);
+        }
+    }
 }
