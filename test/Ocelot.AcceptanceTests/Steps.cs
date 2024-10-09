10610e48486d1a5b62615cf180a1d96c34a6679e
Henrique Holtz <henrique_holtz@hotmail.com>
2024-10-09 20:13:54 -0300

    test: Undo moving the function "Steps.ThenTheStatusCodeShouldBe(HttpStatusCode expected)"

--- test/Ocelot.AcceptanceTests/Steps.cs
+++ test/Ocelot.AcceptanceTests/Steps.cs
@@ -988,15 +988,14 @@ public class Steps : IDisposable
         _response.Content.Headers.ContentLength.ShouldBe(expected);
     }
 
+    public void ThenTheStatusCodeShouldBe(HttpStatusCode expected)
+        => _response.StatusCode.ShouldBe(expected);
+
     public void ThenTheStatusCodeShouldBe(int expectedHttpStatusCode)
     {
         var responseStatusCode = (int)_response.StatusCode;
         responseStatusCode.ShouldBe(expectedHttpStatusCode);
     }
-
-    public void ThenTheStatusCodeShouldBe(HttpStatusCode expected)
-        => _response.StatusCode.ShouldBe(expected);
-
 
     public void ThenTheRequestIdIsReturned()
     {
