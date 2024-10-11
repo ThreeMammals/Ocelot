9bd1214818d54b2b5353cd11b193d74b44a2cfd3
Henrique Holtz <henrique_holtz@hotmail.com>
2024-10-11 08:01:57 -0300

    test: Bringing back the slash I removed with no reason

--- test/Ocelot.AcceptanceTests/Configuration/ConfigurationInConsulTests.cs
+++ test/Ocelot.AcceptanceTests/Configuration/ConfigurationInConsulTests.cs
@@ -67,7 +67,7 @@ namespace Ocelot.AcceptanceTests.Configuration
                 .And(x => x.GivenThereIsAServiceRunningOn($"http://localhost:{servicePort}", string.Empty, 200, "Hello from Laura"))
                 .And(x => _steps.GivenThereIsAConfigurationAsync(configuration))
                 .And(x => _steps.GivenOcelotIsRunningUsingConsulToStoreConfigAndJsonSerializedCache())
-                .When(x => _steps.WhenIGetUrlOnTheApiGateway(""))
+                .When(x => _steps.WhenIGetUrlOnTheApiGateway("/"))
                 .Then(x => _steps.ThenTheStatusCodeShouldBe(HttpStatusCode.OK))
                 .And(x => _steps.ThenTheResponseBodyShouldBe("Hello from Laura"))
                 .BDDfy();
