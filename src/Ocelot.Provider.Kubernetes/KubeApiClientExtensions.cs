59e55fc5f4674153abd1dd35b438c93ff9f5fe09
VIMPELCOM_MAIN\NKuksov <nkuksov@beeline.ru>
2024-10-13 15:12:32 +0300

    Added 'WatchKube' discovery provider

---
+++ src/Ocelot.Provider.Kubernetes/KubeApiClientExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Ocelot.Provider.Kubernetes.Interfaces;
+
+namespace Ocelot.Provider.Kubernetes;
+
+public static class KubeApiClientExtensions
+{
+    public static IEndPointClient EndpointsV1(this IKubeApiClient client)
+        => client.ResourceClient(x => new EndPointClientV1(client));
+}
