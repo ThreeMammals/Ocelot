6ff1011e67fa6f363ea69cc662576a15da010a29
Raman Maksimchuk <dotnet044@gmail.com>
2024-10-16 18:34:08 +0300

    Sync call of the async method inside of sync context. Add comment with ToDo

--- src/Ocelot/Configuration/Repository/ConsulFileConfigurationPollerOption.cs
+++ src/Ocelot/Configuration/Repository/ConsulFileConfigurationPollerOption.cs
@@ -18,7 +18,7 @@
         {
             var delay = 1000;
 
-            var fileConfig = _fileConfigurationRepository.Get().GetAwaiter().GetResult();
+            var fileConfig = _fileConfigurationRepository.Get().GetAwaiter().GetResult(); // sync call, so TODO extend IFileConfigurationPollerOptions interface with 2nd async method
             if (fileConfig?.Data?.GlobalConfiguration?.ServiceDiscoveryProvider != null &&
                     !fileConfig.IsError &&
                     fileConfig.Data.GlobalConfiguration.ServiceDiscoveryProvider.PollingInterval > 0)
