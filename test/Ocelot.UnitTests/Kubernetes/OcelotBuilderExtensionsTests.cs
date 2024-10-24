1bb499681c194558bae556ed9183aa3203f654db
Raman Maksimchuk <dotnet044@gmail.com>
2024-10-24 20:18:24 +0300

    Cover by unit tests

--- test/Ocelot.UnitTests/Kubernetes/OcelotBuilderExtensionsTests.cs
+++ test/Ocelot.UnitTests/Kubernetes/OcelotBuilderExtensionsTests.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using KubeClient;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Ocelot.DependencyInjection;
 using Ocelot.Provider.Kubernetes;
+using Ocelot.Provider.Kubernetes.Interfaces;
+using Ocelot.ServiceDiscovery;
 using System.Reflection;
 
 namespace Ocelot.UnitTests.Kubernetes;
@@ -33,10 +36,36 @@ public class OcelotBuilderExtensionsTests : UnitTest // No Chinese tests now!
     [Trait("Feat", "345")]
     public void AddKubernetes_NoExceptions_ShouldSetUpKubernetes()
     {
+        // Arrange
         var addOcelot = () => _ocelotBuilder = _services.AddOcelot(_configRoot);
         addOcelot.ShouldNotThrow();
 
+        // Act
         var addKubernetes = () => _ocelotBuilder.AddKubernetes();
+
+        // Assert
         addKubernetes.ShouldNotThrow();
+    }
+
+    [Fact]
+    [Trait("Bug", "977")]
+    [Trait("PR", "2180")]
+    public void AddKubernetes_DefaultServices_HappyPath()
+    {
+        // Arrange, Act
+        _ocelotBuilder = _services.AddOcelot(_configRoot).AddKubernetes();
+
+        // Assert
+        var descriptor = _services.SingleOrDefault(sd => sd.ServiceType == typeof(IKubeApiClient)).ShouldNotBeNull();
+        descriptor.Lifetime.ShouldBe(ServiceLifetime.Singleton); // 2180 scenario
+
+        descriptor = _services.SingleOrDefault(sd => sd.ServiceType == typeof(ServiceDiscoveryFinderDelegate)).ShouldNotBeNull();
+        descriptor.Lifetime.ShouldBe(ServiceLifetime.Singleton);
+
+        descriptor = _services.SingleOrDefault(sd => sd.ServiceType == typeof(IKubeServiceBuilder)).ShouldNotBeNull();
+        descriptor.Lifetime.ShouldBe(ServiceLifetime.Singleton);
+
+        descriptor = _services.SingleOrDefault(sd => sd.ServiceType == typeof(IKubeServiceCreator)).ShouldNotBeNull();
+        descriptor.Lifetime.ShouldBe(ServiceLifetime.Singleton);
     }
 }
