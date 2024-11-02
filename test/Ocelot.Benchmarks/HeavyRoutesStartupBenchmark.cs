982b901eecbc2be7baa332cc49a46d960f7ecb7b
Raman Maksimchuk <dotnet044@gmail.com>
2024-11-02 18:57:37 +0300

    be more asynchronous

--- test/Ocelot.Benchmarks/HeavyRoutesStartupBenchmark.cs
+++ test/Ocelot.Benchmarks/HeavyRoutesStartupBenchmark.cs
@@ -5,142 +5,137 @@ using Newtonsoft.Json;
 using Ocelot.Configuration.File;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
-using Ocelot.Testing;
-namespace Ocelot.Benchmarks
+
+namespace Ocelot.Benchmarks;
+
+[Config(typeof(HeavyRoutesStartupBenchmark))]
+public class HeavyRoutesStartupBenchmark : ManualConfig
 {
-    [Config(typeof(HeavyRoutesStartupBenchmark))]
-    public class HeavyRoutesStartupBenchmark : ManualConfig
+    private IWebHost _ocelot;
+
+    //7 files with 80+ routes
+    int startFileCount = 0;
+
+    //7 files with 80+ routes
+    int endFileCount = 90;
+    public HeavyRoutesStartupBenchmark()
     {
-        private IWebHost _ocelot;
+        AddColumn(StatisticColumn.AllStatistics);
+        AddDiagnoser(MemoryDiagnoser.Default);
+        AddValidator(BaselineValidator.FailOnError);
+    }
 
-        //7 files with 80+ routes
-        int startFileCount = 0;
-
-        //7 files with 80+ routes
-        int endFileCount = 90;
-        public HeavyRoutesStartupBenchmark()
+    [GlobalSetup]
+    public void SetUp()
+    {
+        for (int i = 1; i <= 7; i++)
         {
-            AddColumn(StatisticColumn.AllStatistics);
-            AddDiagnoser(MemoryDiagnoser.Default);
-            AddValidator(BaselineValidator.FailOnError);
+            CreateOcelotConfigFile(startFileCount, endFileCount, $"ocelot{i}.json");
+            startFileCount = endFileCount + 1;
+            endFileCount += 90;
         }
+    }
 
-        [GlobalSetup]
-        public void SetUp()
+    [Benchmark]
+    public async Task StartOcelotWithAddingNewConfigFile()
+    {
+        //Adding new file
+        CreateOcelotConfigFile(startFileCount, endFileCount, $"ocelot{8}.json");
+        await OcelotStartupAsync($"http://localhost:{TcpPortFinder.FindAvailablePort()}");
+    }
+
+    [Benchmark]
+    public async Task StartOcelotWithOverridingExistingFile()
+    {   
+        //Overriding with old file
+        CreateOcelotConfigFile(0, 90, $"ocelot{8}.json");
+        await OcelotStartupAsync($"http://localhost:{TcpPortFinder.FindAvailablePort()}");
+    }
+
+    [GlobalCleanup]
+    public async void Cleanup()
+    {
+        // Stop the web host after benchmarking
+        await _ocelot.StopAsync();
+    }
+
+    /// <summary>
+    /// To replicate large number of routes.
+    /// </summary>
+    /// <param name="start">Starting digit to create downstream/upstream files.</param>
+    /// <param name="end">Ending digit to create downstream/upstream files.</param>
+    /// <param name="fileName">Ocelot files names.</param>
+    private static void CreateOcelotConfigFile(int start, int end, string fileName)
+    {
+        var routes = new List<FileRoute>();
+
+        for (int i = start; i < end; i++)
         {
-            for (int i = 1; i <= 7; i++)
+            routes.Add(new()
             {
-                CreateOcelotConfigFile(startFileCount, endFileCount, $"ocelot{i}.json");
-                startFileCount = endFileCount + 1;
-                endFileCount += 90;
-            }
-        }
-
-        [Benchmark]
-        public void StartOcelotWithAddingNewConfigFile()
-        {
-            //Adding new file
-            CreateOcelotConfigFile(startFileCount, endFileCount, $"ocelot{8}.json");
-            OcelotStartup($"http://localhost:{TcpPortFinder.FindAvailablePort()}");
-        }
-
-        [Benchmark]
-        public void StartOcelotWithOverridingExistingFile()
-        {   
-            //Overriding with old file
-            CreateOcelotConfigFile(0, 90, $"ocelot{8}.json");
-            OcelotStartup($"http://localhost:{TcpPortFinder.FindAvailablePort()}");
-        }
-
-        [GlobalCleanup]
-        public async void Cleanup()
-        {
-            // Stop the web host after benchmarking
-            await _ocelot.StopAsync();
-        }
-
-        /// <summary>
-        /// To replicate large number of routes.
-        /// </summary>
-        /// <param name="start">Starting digit to create downstream/upstream files.</param>
-        /// <param name="end">Ending digit to create downstream/upstream files.</param>
-        /// <param name="fileName">Ocelot files names.</param>
-        private static void CreateOcelotConfigFile(int start, int end, string fileName)
-        {
-            var routes = new List<FileRoute>();
-
-            for (int i = start; i < end; i++)
-            {
-                routes.Add(new()
-                {
-                    DownstreamPathTemplate = $"/downstream{i}",
-                    DownstreamHostAndPorts = new List<FileHostAndPort>
+                DownstreamPathTemplate = $"/downstream{i}",
+                DownstreamHostAndPorts = new List<FileHostAndPort>
+                        {
+                            new()
                             {
-                                new()
-                                {
-                                    Host = "localhost",
-                                    Port = 51879,
-                                },
+                                Host = "localhost",
+                                Port = 51879,
                             },
-                    DownstreamScheme = "http",
-                    UpstreamPathTemplate = $"/upstream{i}",
-                    UpstreamHttpMethod = new List<string> { "Get" },
-                });
-            }
-
-            var configuration = new FileConfiguration { Routes = routes };
-            GivenThereIsAConfiguration(configuration, fileName);
+                        },
+                DownstreamScheme = "http",
+                UpstreamPathTemplate = $"/upstream{i}",
+                UpstreamHttpMethod = new List<string> { "Get" },
+            });
         }
 
-        private void OcelotStartup(string url)
-        {
-            _ocelot = new WebHostBuilder()
-                .UseKestrel()
-                .UseUrls(url)
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .ConfigureAppConfiguration((hostingContext, config) =>
-                {
-                    for (int i = 1; i <= 8; i++)
-                    {
-                        config.AddJsonFile($"ocelot{i}.json", false, false);
-                    }
+        var configuration = new FileConfiguration { Routes = routes };
+        GivenThereIsAConfiguration(configuration, fileName);
+    }
 
-                    config
-                        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
-                        .AddJsonFile("appsettings.json", true, true)
-                        .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
-                        .AddEnvironmentVariables();
-                })
-                .ConfigureServices(s =>
-                {
-                    s.AddOcelot();
-                })
-                .ConfigureLogging((hostingContext, logging) =>
-                {
-                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-                })
-                .UseIISIntegration()
-                .Configure(app =>
-                {
-                    app.UseOcelot().Wait();
-                })
-                .Build();
-
-            _ocelot.Start();
-        }
-
-        public static void GivenThereIsAConfiguration(FileConfiguration fileConfiguration, string fileName)
-        {
-            var configurationPath = Path.Combine(AppContext.BaseDirectory, fileName);
-
-            var jsonConfiguration = JsonConvert.SerializeObject(fileConfiguration);
-
-            if (File.Exists(configurationPath))
+    private Task OcelotStartupAsync(string url)
+    {
+        _ocelot = TestHostBuilder.Create()
+            .UseKestrel()
+            .UseUrls(url)
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .ConfigureAppConfiguration((hostingContext, config) =>
             {
-                File.Delete(configurationPath);
-            }
+                for (int i = 1; i <= 8; i++)
+                {
+                    config.AddJsonFile($"ocelot{i}.json", false, false);
+                }
 
-            File.WriteAllText(configurationPath, jsonConfiguration);
+                config
+                    .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                    .AddJsonFile("appsettings.json", true, true)
+                    .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                    .AddEnvironmentVariables();
+            })
+            .ConfigureServices(s =>
+            {
+                s.AddOcelot();
+            })
+            .ConfigureLogging((hostingContext, logging) =>
+            {
+                logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+            })
+            .UseIISIntegration()
+            .Configure(async app => await app.UseOcelot())
+            .Build();
+        return _ocelot.StartAsync();
+    }
+
+    public static void GivenThereIsAConfiguration(FileConfiguration fileConfiguration, string fileName)
+    {
+        var configurationPath = Path.Combine(AppContext.BaseDirectory, fileName);
+
+        var jsonConfiguration = JsonConvert.SerializeObject(fileConfiguration);
+
+        if (File.Exists(configurationPath))
+        {
+            File.Delete(configurationPath);
         }
+
+        File.WriteAllText(configurationPath, jsonConfiguration);
     }
 }
