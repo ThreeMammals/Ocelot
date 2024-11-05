06d2b38ae0b7b28369ff1a46692a5704dd043a66
Raman Maksimchuk <dotnet044@gmail.com>
2024-11-05 15:32:55 +0300

    Minor refactoring

---
+++ src/Ocelot/LoadBalancer/IStickySessionStorage.cs
@@ -0,0 +1,10 @@
+﻿namespace Ocelot.LoadBalancer;
+
+public interface IStickySessionStorage
+{
+    bool Contains(string key);
+    StickySession GetSession(string key);
+    void SetSession(string key, StickySession session);
+    bool TryGetSession(string key, out StickySession session);
+    bool TryRemove(string key, out StickySession session);
+}
