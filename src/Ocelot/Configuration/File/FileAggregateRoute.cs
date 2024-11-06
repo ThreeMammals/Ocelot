eccea0edeb455f7dc0bdd96589979d5906ff0a73
Raman Maksimchuk <dotnet044@gmail.com>
2024-11-06 17:31:20 +0300

    Refactor `FileAggregateRoute`: - sort lines and props - move initialization to def ctor

--- src/Ocelot/Configuration/File/FileAggregateRoute.cs
+++ src/Ocelot/Configuration/File/FileAggregateRoute.cs
@@ -1,25 +1,23 @@
-using Microsoft.AspNetCore.Http;
+namespace Ocelot.Configuration.File;
 
-namespace Ocelot.Configuration.File
+public class FileAggregateRoute : IRoute
 {
-    public class FileAggregateRoute : IRoute
-    {
-        public List<string> RouteKeys { get; set; }
-        public List<AggregateRouteConfig> RouteKeysConfig { get; set; }
-        public string UpstreamPathTemplate { get; set; }
-        public string UpstreamHost { get; set; }
-        public bool RouteIsCaseSensitive { get; set; }
-        public string Aggregator { get; set; }
-        public List<string> UpstreamHttpMethod { get; set; }
-        public IDictionary<string, string> UpstreamHeaderTemplates { get; set; }
-        public int Priority { get; set; } = 1;
+    public string Aggregator { get; set; }
+    public int Priority { get; set; }
+    public bool RouteIsCaseSensitive { get; set; }
+    public List<string> RouteKeys { get; set; }
+    public List<AggregateRouteConfig> RouteKeysConfig { get; set; }
+    public IDictionary<string, string> UpstreamHeaderTemplates { get; set; }
+    public string UpstreamHost { get; set; }
+    public List<string> UpstreamHttpMethod { get; set; }
+    public string UpstreamPathTemplate { get; set; }
 
-        public FileAggregateRoute()
-        {
-            RouteKeys = new();
-            RouteKeysConfig = new();
-            UpstreamHeaderTemplates = new Dictionary<string, string>();
-            UpstreamHttpMethod = new();
-        }
+    public FileAggregateRoute()
+    {
+        Priority = 1;
+        RouteKeys = new();
+        RouteKeysConfig = new();
+        UpstreamHeaderTemplates = new Dictionary<string, string>();
+        UpstreamHttpMethod = new();
     }
 }
