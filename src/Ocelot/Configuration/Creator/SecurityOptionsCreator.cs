4a37e8bee9552b6f3e3d6366872f549258f3c223
Fabrizio Mancin <fabrizio.mancin@esprinet.com>
2024-11-06 14:42:14 +0100

    simplify SecurityOptions Create method

--- src/Ocelot/Configuration/Creator/SecurityOptionsCreator.cs
+++ src/Ocelot/Configuration/Creator/SecurityOptionsCreator.cs
@@ -5,15 +5,8 @@ namespace Ocelot.Configuration.Creator
 {
     public class SecurityOptionsCreator : ISecurityOptionsCreator
     {
-        public SecurityOptions Create(FileSecurityOptions securityOptions, FileGlobalConfiguration globalConfiguration)
-        {
-            if (securityOptions.IsFullFilled())
-            {
-                return Create(securityOptions);
-            }
-
-            return Create(globalConfiguration.SecurityOptions);
-        }
+        public SecurityOptions Create(FileSecurityOptions securityOptions, FileGlobalConfiguration globalConfiguration) 
+            => Create(securityOptions.IsFullFilled() ? securityOptions : globalConfiguration.SecurityOptions);
 
         private static SecurityOptions Create(FileSecurityOptions securityOptions)
         {
