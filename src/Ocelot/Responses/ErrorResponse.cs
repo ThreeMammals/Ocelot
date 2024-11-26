958f7ec63a7ed5966c152cddd33f3a03c51b4394
Raman Maksimchuk <dotnet044@gmail.com>
2024-11-26 14:38:58 +0300

    IDE0079: Remove unnecessary suppression SA1649

--- src/Ocelot/Responses/ErrorResponse.cs
+++ src/Ocelot/Responses/ErrorResponse.cs
@@ -5,12 +5,21 @@ namespace Ocelot.Responses;
 public class ErrorResponse : Response
 {
     public ErrorResponse(Error error)
-        : base(new List<Error> { error })
-    {
-    }
+        : base(new() { error })
+    { }
 
     public ErrorResponse(List<Error> errors)
         : base(errors)
-    {
-    }
+    { }
+}
+
+public class ErrorResponse<T> : Response<T>
+{
+    public ErrorResponse(Error error)
+        : base(new List<Error> { error })
+    { }
+
+    public ErrorResponse(List<Error> errors)
+        : base(errors)
+    { }
 }
