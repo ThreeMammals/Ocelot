6d5830d93a368029e48f2c09e592b5571de2cd90
Raman Maksimchuk <dotnet044@gmail.com>
2025-05-02 14:33:31 +0300

    Task object management

--- src/Ocelot/WebSockets/WebSocketExtensions.cs
+++ src/Ocelot/WebSockets/WebSocketExtensions.cs
@@ -4,14 +4,8 @@ namespace Ocelot.WebSockets;
 
 internal static class WebSocketExtensions
 {
-    public static async Task<bool> TryCloseOutputAsync(this WebSocket webSocket, WebSocketCloseStatus closeStatus, string statusDescription, CancellationToken cancellation)
-    {
-        if (webSocket.State == WebSocketState.Open || webSocket.State == WebSocketState.CloseReceived)
-        {
-            await webSocket.CloseOutputAsync(closeStatus, statusDescription, cancellation);
-            return true;
-        }
-
-        return false;
-    }
+    public static Task TryCloseOutputAsync(this WebSocket webSocket, WebSocketCloseStatus closeStatus, string statusDescription, CancellationToken cancellation)
+        => (webSocket.State == WebSocketState.Open || webSocket.State == WebSocketState.CloseReceived)
+            ? webSocket.CloseOutputAsync(closeStatus, statusDescription, cancellation)
+            : Task.CompletedTask;
 }
