a106d057cb42dabf6305d4c7e7ec2f60d3b5a08b
kick2nick <kolya-budsk@yandex.ru>
2025-05-31 00:00:44 +0300

    minor fixes

--- src/Ocelot.Provider.Kubernetes/ObservableExtensions.cs
+++ src/Ocelot.Provider.Kubernetes/ObservableExtensions.cs
@@ -1,27 +1,26 @@
 ﻿using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 
-namespace Ocelot.Provider.Kubernetes
+namespace Ocelot.Provider.Kubernetes;
+
+public static class ObservableExtensions
 {
-    public static class ObservableExtensions
+    public static IObservable<TSource> RetryAfter<TSource>(this IObservable<TSource> source,
+        TimeSpan dueTime,
+        IScheduler scheduler)
     {
-        public static IObservable<TSource> RetryAfter<TSource>(this IObservable<TSource> source,
-            TimeSpan dueTime,
-            IScheduler scheduler)
-        {
-            return RepeatInfinite(source, dueTime, scheduler).Catch();
-        }
+        return RepeatInfinite(source, dueTime, scheduler).Catch();
+    }
 
-        private static IEnumerable<IObservable<TSource>> RepeatInfinite<TSource>(IObservable<TSource> source,
-            TimeSpan dueTime,
-            IScheduler scheduler)
-        {
-            yield return source;
+    private static IEnumerable<IObservable<TSource>> RepeatInfinite<TSource>(IObservable<TSource> source,
+        TimeSpan dueTime,
+        IScheduler scheduler)
+    {
+        yield return source;
 
-            while (true)
-            {
-                yield return source.DelaySubscription(dueTime, scheduler);
-            }
+        while (true)
+        {
+            yield return source.DelaySubscription(dueTime, scheduler);
         }
     }
 }
