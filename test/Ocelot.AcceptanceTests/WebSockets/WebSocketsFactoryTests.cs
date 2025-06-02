01cb43523ff644c6043109e2b7b8a9321e6feec0
Raman Maksimchuk <dotnet044@gmail.com>
2025-06-02 13:00:16 +0300

    Update old acceptance tests for the WebSocketsFactory class

--- test/Ocelot.AcceptanceTests/WebSockets/WebSocketsFactoryTests.cs
+++ test/Ocelot.AcceptanceTests/WebSockets/WebSocketsFactoryTests.cs
@@ -2,10 +2,10 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Ocelot.Configuration.File;
-using Ocelot.DependencyInjection;
+using Ocelot.LoadBalancer.LoadBalancers;
 using Ocelot.Middleware;
 using Ocelot.WebSockets;
 using System.Net.WebSockets;
@@ -13,161 +13,105 @@ using System.Text;
 
 namespace Ocelot.AcceptanceTests.WebSockets;
 
-public sealed class WebSocketsTests : Steps
+public sealed class WebSocketsFactoryTests : Steps
 {
     private readonly List<string> _secondRecieved;
     private readonly List<string> _firstRecieved;
     private readonly WebSocketsFactory _factory;
+    private IHost _ocelotHost;
 
-    public WebSocketsTests() : base()
+    public WebSocketsFactoryTests() : base()
     {
         _firstRecieved = new List<string>();
         _secondRecieved = new List<string>();
         _factory = new WebSocketsFactory(); // concrete class, so, we perform real integration testing
     }
 
-    [Fact]
-    public void ShouldProxyWebsocketInputToDownstreamService()
+    public override void Dispose()
     {
-        var downstreamPort = PortFinder.GetRandomPort();
-        var downstreamHost = "localhost";
-
-        var config = new FileConfiguration
-        {
-            Routes = new List<FileRoute>
-            {
-                new()
-                {
-                    UpstreamPathTemplate = "/",
-                    DownstreamPathTemplate = "/ws",
-                    DownstreamScheme = "ws",
-                    DownstreamHostAndPorts = new List<FileHostAndPort>
-                    {
-                        new()
-                        {
-                            Host = downstreamHost,
-                            Port = downstreamPort,
-                        },
-                    },
-                },
-            },
-        };
-
-        this.Given(_ => GivenThereIsAConfiguration(config))
-            .And(_ => StartFakeOcelotWithWebSockets())
-            .And(_ => StartFakeDownstreamService($"http://{downstreamHost}:{downstreamPort}", "/ws"))
-            .When(_ => StartClient("ws://localhost:5000/"))
-            .Then(_ => ThenTheReceivedCountIs(10))
-            .BDDfy();
+        _ocelotHost.Dispose();
+        base.Dispose();
     }
 
     [Fact]
+    [Trait("Feat", "212")]
+    [Trait("PR", "273")] // https://github.com/ThreeMammals/Ocelot/pull/273
+    public async Task ShouldProxyWebsocketInputToDownstreamService()
+    {
+        var port = PortFinder.GetRandomPort();
+        var route = GivenRoute("/ws", port);
+        var configuration = GivenConfiguration(route);
+        GivenThereIsAConfiguration(configuration);
+        int ocelotPort = PortFinder.GetRandomPort();
+        var ocelotUrl = new UriBuilder(Uri.UriSchemeWs, "localhost", ocelotPort).Uri;
+        await StartFakeOcelotWithWebSockets(ocelotPort);
+        await StartFakeDownstreamService(DownstreamUrl(port), "/ws");
+        await StartClient(ocelotUrl);
+        ThenTheReceivedCountIs(10);
+
+        void ThenTheReceivedCountIs(int count) => _firstRecieved.Count.ShouldBe(count);
+    }
+
+    [Fact]
+    [Trait("Feat", "212")]
+    [Trait("PR", "273")] // https://github.com/ThreeMammals/Ocelot/pull/273
     public void ShouldProxyWebsocketInputToDownstreamServiceAndUseLoadBalancer()
     {
-        var downstreamPort = PortFinder.GetRandomPort();
-        var downstreamHost = "localhost";
-        var secondDownstreamPort = PortFinder.GetRandomPort();
-        var secondDownstreamHost = "localhost";
-
-        var config = new FileConfiguration
-        {
-            Routes = new List<FileRoute>
-            {
-                new()
-                {
-                    UpstreamPathTemplate = "/",
-                    DownstreamPathTemplate = "/ws",
-                    DownstreamScheme = "ws",
-                    DownstreamHostAndPorts = new List<FileHostAndPort>
-                    {
-                        new()
-                        {
-                            Host = downstreamHost,
-                            Port = downstreamPort,
-                        },
-                        new()
-                        {
-                            Host = secondDownstreamHost,
-                            Port = secondDownstreamPort,
-                        },
-                    },
-                    LoadBalancerOptions = new FileLoadBalancerOptions { Type = "RoundRobin" },
-                },
-            },
-        };
-
-        this.Given(_ => GivenThereIsAConfiguration(config))
-            .And(_ => StartFakeOcelotWithWebSockets())
-            .And(_ => StartFakeDownstreamService($"http://{downstreamHost}:{downstreamPort}", "/ws"))
-            .And(_ => StartSecondFakeDownstreamService($"http://{secondDownstreamHost}:{secondDownstreamPort}", "/ws"))
-            .When(_ => WhenIStartTheClients())
+        int port1 = PortFinder.GetRandomPort();
+        int port2 = PortFinder.GetRandomPort();
+        var route = GivenRoute("/ws", port1, port2);
+        route.LoadBalancerOptions.Type = nameof(RoundRobin);
+        var configuration = GivenConfiguration(route);
+        int ocelotPort = PortFinder.GetRandomPort();
+        this.Given(_ => GivenThereIsAConfiguration(configuration))
+            .And(_ => StartFakeOcelotWithWebSockets(ocelotPort))
+            .And(_ => StartFakeDownstreamService(DownstreamUrl(port1), "/ws"))
+            .And(_ => StartSecondFakeDownstreamService(DownstreamUrl(port2), "/ws"))
+            .When(_ => WhenIStartTheClients(ocelotPort))
             .Then(_ => ThenBothDownstreamServicesAreCalled())
             .BDDfy();
-    }
-
-    private async Task StartFakeOcelotWithWebSockets()
-    {
-        var builder = TestHostBuilder.Create();
-        builder.ConfigureServices(s =>
-        {
-            s.AddSingleton(builder);
-            s.AddOcelot();
-        });
-        builder.UseKestrel()
-            .UseUrls("http://localhost:5000")
-            .UseContentRoot(Directory.GetCurrentDirectory())
-            .ConfigureAppConfiguration((hostingContext, config) =>
-            {
-                config.SetBasePath(hostingContext.HostingEnvironment.ContentRootPath);
-                var env = hostingContext.HostingEnvironment;
-                config.AddJsonFile("appsettings.json", true, false)
-                    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, false);
-                config.AddJsonFile(ocelotConfigFileName, false, false);
-                config.AddEnvironmentVariables();
-            })
-            .ConfigureLogging((hostingContext, logging) =>
-            {
-                logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-                logging.AddConsole();
-            })
-            .Configure(async app =>
-            {
-                app.UseWebSockets();
-                await app.UseOcelot();
-            })
-            .UseIISIntegration();
-        ocelotHost = builder.Build();
-        await ocelotHost.StartAsync();
     }
 
     private void ThenBothDownstreamServicesAreCalled()
     {
         _firstRecieved.Count.ShouldBe(10);
-        _firstRecieved.ForEach(x =>
-        {
-            x.ShouldBe("test");
-        });
-
+        _firstRecieved.ForEach(x => x.ShouldBe("test"));
         _secondRecieved.Count.ShouldBe(10);
-        _secondRecieved.ForEach(x =>
-        {
-            x.ShouldBe("chocolate");
-        });
+        _secondRecieved.ForEach(x => x.ShouldBe("chocolate"));
     }
 
-    private async Task WhenIStartTheClients()
+    private static FileRoute GivenRoute(string downstream = null, params int[] ports) => new()
     {
-        var firstClient = StartClient("ws://localhost:5000/");
+        UpstreamPathTemplate = "/",
+        DownstreamPathTemplate = downstream ?? "/ws",
+        DownstreamScheme = Uri.UriSchemeWs,
+        DownstreamHostAndPorts = ports.Select(Localhost).ToList(),
+    };
 
-        var secondClient = StartSecondClient("ws://localhost:5000/");
-
-        await Task.WhenAll(firstClient, secondClient);
+    private async Task StartFakeOcelotWithWebSockets(int port)
+    {
+        var url = new UriBuilder(Uri.UriSchemeHttp, "localhost", port).ToString();
+        static void WithWebSockets(IApplicationBuilder app) => app.UseWebSockets().UseOcelot().Wait();
+        void ConfigureWebHost(IWebHostBuilder b) => b
+            .UseUrls(url)
+            .ConfigureLogging((hosting, logging) => logging
+                .AddConfiguration(hosting.Configuration.GetSection("Logging"))
+                .AddConsole());
+        _ocelotHost = await GivenOcelotHostIsRunning(WithBasicConfiguration, WithAddOcelot, WithWebSockets, ConfigureWebHost);
     }
 
-    private async Task StartClient(string url)
+    private Task WhenIStartTheClients(int port)
+    {
+        var url = new UriBuilder(Uri.UriSchemeWs, "localhost", port).Uri;
+        var firstClient = StartClient(url);
+        var secondClient = StartSecondClient(url);
+        return Task.WhenAll(firstClient, secondClient);
+    }
+
+    private async Task StartClient(Uri url)
     {
         var client = _factory.CreateClient();
-        await client.ConnectAsync(new Uri(url), CancellationToken.None);
+        await client.ConnectAsync(url, CancellationToken.None);
 
         var sending = Task.Run(async () =>
         {
@@ -175,23 +119,18 @@ public sealed class WebSocketsTests : Steps
             for (var i = 0; i < 10; i++)
             {
                 var bytes = Encoding.UTF8.GetBytes(line);
-
-                await client.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true,
-                    CancellationToken.None);
+                await client.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true, CancellationToken.None);
                 await Task.Delay(10);
             }
-
             await client.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
         });
 
         var receiving = Task.Run(async () =>
         {
             var buffer = new byte[1024 * 4];
-
             while (true)
             {
                 var result = await client.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-
                 if (result.MessageType == WebSocketMessageType.Text)
                 {
                     _firstRecieved.Add(Encoding.UTF8.GetString(buffer, 0, result.Count));
@@ -204,7 +143,6 @@ public sealed class WebSocketsTests : Steps
                         // Valid states are: Open, CloseReceived, CloseSent
                         await client.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
                     }
-
                     break;
                 }
             }
@@ -213,12 +151,11 @@ public sealed class WebSocketsTests : Steps
         await Task.WhenAll(sending, receiving);
     }
 
-    private async Task StartSecondClient(string url)
+    private async Task StartSecondClient(Uri url)
     {
         await Task.Delay(500);
-
         var client = _factory.CreateClient();
-        await client.ConnectAsync(new Uri(url), CancellationToken.None);
+        await client.ConnectAsync(url, CancellationToken.None);
 
         var sending = Task.Run(async () =>
         {
@@ -226,23 +163,18 @@ public sealed class WebSocketsTests : Steps
             for (var i = 0; i < 10; i++)
             {
                 var bytes = Encoding.UTF8.GetBytes(line);
-
-                await client.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true,
-                    CancellationToken.None);
+                await client.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true, CancellationToken.None);
                 await Task.Delay(10);
             }
-
             await client.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
         });
 
         var receiving = Task.Run(async () =>
         {
             var buffer = new byte[1024 * 4];
-
             while (true)
             {
                 var result = await client.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-
                 if (result.MessageType == WebSocketMessageType.Text)
                 {
                     _secondRecieved.Add(Encoding.UTF8.GetString(buffer, 0, result.Count));
@@ -255,7 +187,6 @@ public sealed class WebSocketsTests : Steps
                         // Valid states are: Open, CloseReceived, CloseSent
                         await client.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
                     }
-
                     break;
                 }
             }
@@ -317,16 +248,12 @@ public sealed class WebSocketsTests : Steps
         try
         {
             var buffer = new byte[1024 * 4];
-
             var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-
             while (!result.CloseStatus.HasValue)
             {
                 await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
-
                 result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
             }
-
             await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
         }
         catch (Exception e)
@@ -340,28 +267,18 @@ public sealed class WebSocketsTests : Steps
         try
         {
             var buffer = new byte[1024 * 4];
-
             var bytes = Encoding.UTF8.GetBytes("chocolate");
-
             var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-
             while (!result.CloseStatus.HasValue)
             {
                 await webSocket.SendAsync(new ArraySegment<byte>(bytes), result.MessageType, result.EndOfMessage, CancellationToken.None);
-
                 result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
             }
-
             await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
         }
         catch (Exception e)
         {
             Console.WriteLine(e);
         }
-    }
-
-    private void ThenTheReceivedCountIs(int count)
-    {
-        _firstRecieved.Count.ShouldBe(count);
     }
 }
