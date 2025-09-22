067e06424ba208c34e4cc0a93f9bd56aeb978369
Raman Maksimchuk <dotnet044@gmail.com>
2025-09-22 12:12:25 +0300

    Guillaume likes records

--- src/Ocelot/RateLimiting/ClientRequestIdentity.cs
+++ src/Ocelot/RateLimiting/ClientRequestIdentity.cs
@@ -4,15 +4,3 @@ public readonly record struct ClientRequestIdentity(string ClientId, string Load
 {
     public override string ToString() => $"{ClientId}:{LoadBalancerKey}";
 }
-{
-    public ClientRequestIdentity(string clientId, string loadBalancerKey)
-    {
-        ClientId = clientId;
-        LoadBalancerKey = loadBalancerKey;
-    }
-
-    public string ClientId;
-    public string LoadBalancerKey;
-
-    public override readonly string ToString() => $"{ClientId}:{LoadBalancerKey}";
-}
