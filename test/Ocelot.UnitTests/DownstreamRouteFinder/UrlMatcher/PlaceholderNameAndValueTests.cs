55095d8ee0ec63424b0fc6979cb338809b78a63f
bhargav-polara <bhargav.polara@marutitech.com>
2026-01-21 21:56:55 +0530

    #2346 Fix problem with downstream URLs where query parameter names get messed up if they contain a placeholder that has a non-empty value (#2351)
    * Update DownstreamUrlCreatorMiddleware.cs
    * Update DownstreamUrlCreatorMiddlewareTests.cs
    * Added more test Cases and refactor RemoveQueryStringParametersThatHaveBeenUsedInTemplate Logic
    * Revert "Added more test Cases and refactor RemoveQueryStringParametersThatHaveBeenUsedInTemplate Logic"
      This reverts commit 21661a4eec7a53e32704de33987a95ce42ee6d1a.
    * Replaced the regex-based logic in RemoveQueryStringParametersThatHaveBeenUsedInTemplate
    * Revert "Replaced the regex-based logic in RemoveQueryStringParametersThatHaveBeenUsedInTemplate"
      This reverts commit 40358588bc995ab483b5963738bfaeafb09deab4.
    * Replaced the regex-based logic in RemoveQueryStringParametersThatHaveBeenUsedInTemplate
    * Update RoutingWithQueryStringTests.cs
    * Testing the "Catch All Query String" feat to make sure there is no regression
    * Refactor algorithm to take full advantage of QueryHelpers
    * Code review by @raman-m ~ improved testing
    * Roll back the status badge targets to develop
    ---------
    Co-authored-by: Raman Maksimchuk <dotnet044@gmail.com>

---
+++ test/Ocelot.UnitTests/DownstreamRouteFinder/UrlMatcher/PlaceholderNameAndValueTests.cs
@@ -0,0 +1,36 @@
+﻿using Ocelot.DownstreamRouteFinder.UrlMatcher;
+
+namespace Ocelot.UnitTests.DownstreamRouteFinder.UrlMatcher;
+
+public sealed class PlaceholderNameAndValueTests
+{
+    [Fact]
+    public void Key()
+    {
+        // Arrange
+        PlaceholderNameAndValue placeholder = new("{test}", "testing");
+
+        // Act
+        var actual = placeholder.Key;
+
+        // Assert
+        Assert.Equal("testing", placeholder.Value);
+        Assert.Equal("{test}", placeholder.Name);
+        Assert.Equal("test", actual);
+    }
+
+    [Fact]
+    public void ToString_Override()
+    {
+        // Arrange
+        PlaceholderNameAndValue placeholder = new("{test}", "testing");
+
+        // Act
+        var actual = placeholder.ToString();
+
+        // Assert
+        Assert.Equal("testing", placeholder.Value);
+        Assert.Equal("{test}", placeholder.Name);
+        Assert.Equal("[{test}=testing]", actual);
+    }
+}
