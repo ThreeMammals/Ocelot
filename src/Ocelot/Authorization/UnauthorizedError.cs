fff4df856b6f565a8af2056a252b56df9c5d0c79
Raman Maksimchuk <dotnet044@gmail.com>
2026-03-06 15:04:29 +0300

    Improve code coverage

--- src/Ocelot/Authorization/UnauthorizedError.cs
+++ src/Ocelot/Authorization/UnauthorizedError.cs
@@ -1,11 +1,12 @@
-﻿using Ocelot.Errors;
+﻿using Microsoft.AspNetCore.Http;
+using Ocelot.Errors;
 
 namespace Ocelot.Authorization;
 
 public class UnauthorizedError : Error
 {
     public UnauthorizedError(string message)
-        : base(message, OcelotErrorCode.UnauthorizedError, 403)
+        : base(message, OcelotErrorCode.UnauthorizedError, StatusCodes.Status403Forbidden)
     {
     }
 }
--- src/Ocelot/Authorization/UserDoesNotHaveClaimError.cs
+++ src/Ocelot/Authorization/UserDoesNotHaveClaimError.cs
@@ -1,11 +1,12 @@
-﻿using Ocelot.Errors;
+﻿using Microsoft.AspNetCore.Http;
+using Ocelot.Errors;
 
 namespace Ocelot.Authorization;
 
 public class UserDoesNotHaveClaimError : Error
 {
     public UserDoesNotHaveClaimError(string message)
-        : base(message, OcelotErrorCode.UserDoesNotHaveClaimError, 403)
+        : base(message, OcelotErrorCode.UserDoesNotHaveClaimError, StatusCodes.Status403Forbidden)
     {
     }
 }
