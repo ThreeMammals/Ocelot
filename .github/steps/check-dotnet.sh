75a5cc30be1fbc9191c46c02492fb6cadbf5acb6
Raman Maksimchuk <dotnet044@gmail.com>
2026-04-18 18:01:03 +0300

    Done

--- .github/steps/check-dotnet.sh
+++ .github/steps/check-dotnet.sh
@@ -5,16 +5,21 @@
 # Target major version (e.g. 10, 9, 8...)
 DOTNET_VERSION="${1:-8}"
 
+echo "All SDKs..."
+echo "-------------------------------------------------------------"
+dotnet --list-sdks
+echo "-------------------------------------------------------------"
+
 echo "Checking for .NET ${DOTNET_VERSION} SDK..."
 
 # Use dotnet --list-sdks and check if any SDK starts with the major version
 if dotnet --list-sdks | grep -q -E "^\s*${DOTNET_VERSION}\."; then
-    echo "checkdotnet_installed=true" >> $GITHUB_OUTPUT
+    echo "CHECKDOTNET_installed=true" >> $GITHUB_OUTPUT
     echo "✅ .NET ${DOTNET_VERSION} SDK is installed."    
     # Optional: Show the actual installed versions
     echo "Installed ${DOTNET_VERSION}.x SDKs:"
     dotnet --list-sdks | grep -E "^\s*${DOTNET_VERSION}\."
 else
-    echo "checkdotnet_installed=false" >> $GITHUB_OUTPUT
+    echo "CHECKDOTNET_installed=false" >> $GITHUB_OUTPUT
     echo "❌ .NET ${DOTNET_VERSION} SDK is NOT installed."
 fi
