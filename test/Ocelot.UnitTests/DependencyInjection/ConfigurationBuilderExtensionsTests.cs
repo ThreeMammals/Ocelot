7bd40734802357e0d723b122703b0fd86f5a2720
Raman Maksimchuk <dotnet044@gmail.com>
2026-05-11 19:32:01 +0300

    Migrate unit testing from VSTest framework to Microsoft.Testing.Platform

--- test/Ocelot.UnitTests/DependencyInjection/ConfigurationBuilderExtensionsTests.cs
+++ test/Ocelot.UnitTests/DependencyInjection/ConfigurationBuilderExtensionsTests.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Newtonsoft.Json;
 using Ocelot.Configuration.File;
 using Ocelot.DependencyInjection;
