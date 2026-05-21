730659d581c9e1995d17deda00e98b34454b70b9
Raman Maksimchuk <dotnet044@gmail.com>
2026-05-21 15:28:33 +0300

    Prepare Coveralls script v3

--- .github/steps/prepare-coveralls.sh
+++ .github/steps/prepare-coveralls.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-# Prepare Coveralls
+# Prepare Coveralls, with 1 optional argument
 echo "::group::Listing environment variables"
 env | sort
 echo "::endgroup::"
 
-# First argument: target .NET major version (digit)
-# Default to 8 if no argument is provided
-# Target major version (e.g. 10, 9, 8...)
+# First argument: target .NET TFM (string)
+# Default to "net10.0" if no argument is provided
 DOTNET_TFM="${1:-net10.0}"
 
 echo ------------ Detect coverage file ------------ 
@@ -14,8 +13,14 @@ ls -d ./test/Ocelot.UnitTests/bin/Debug/$DOTNET_TFM/*/
 coverage_folder=$(ls -d ./test/Ocelot.UnitTests/bin/Debug/$DOTNET_TFM/TestResults*/ | head -1)
 echo "Detected first folder : $coverage_folder"
 echo TestResults files are...
-ls $coverage_folder/*
-coverage_file="${coverage_folder%/}/coverage.cobertura.*.xml"
+ls $coverage_folder
+echo DONE
+
+coverage_pattern="${coverage_folder}/coverage.cobertura.*.xml"
+# Expand the pattern to an array
+coverage_files=($coverage_pattern)
+coverage_file="${coverage_files[0]}"
+
 echo "Detecting file $coverage_file ..."
 if [ -f "$coverage_file" ]; then
   echo "Coverage file exists."
