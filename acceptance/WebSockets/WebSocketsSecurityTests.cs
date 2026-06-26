e95bdbacc92faeb783c14f67faf7f3c38ec0cbd6
Raman Maksimchuk <dotnet044@gmail.com>
2026-06-26 20:10:46 +0300

    Add original bug user scenario

--- acceptance/WebSockets/WebSocketsSecurityTests.cs
+++ acceptance/WebSockets/WebSocketsSecurityTests.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
+using Ocelot.Configuration.File;
+using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Testing.Steps;
 using System.Net.WebSockets;
@@ -11,6 +13,82 @@ namespace Ocelot.AcceptanceTests.WebSockets;
 public sealed class WebSocketsSecurityTests : WebSocketsSteps
 {
     private Exception _connect;
+
+    [Fact]
+    [Trait("Bug", "2403")] // https://github.com/ThreeMammals/Ocelot/issues/2403
+    [Trait("PR", "2406")] // https://github.com/ThreeMammals/Ocelot/pull/2406
+    public async Task Bug2403_UserScenario()
+    {
+        // Downstream service on 127.0.0.1:6001 (HTTP + WebSocket echo)
+        var port = PortFinder.GetRandomPort();
+        var downstream = new UriBuilder("http", "127.0.0.1", port);
+        var downBuilder = WebApplication.CreateBuilder();
+        downBuilder.WebHost.UseUrls(downstream.ToString());
+        var down = downBuilder.Build();
+        down.UseWebSockets();
+        down.Use(async (ctx, next) =>
+        {
+            if (ctx.WebSockets.IsWebSocketRequest)
+            {
+                using var ws = await ctx.WebSockets.AcceptWebSocketAsync();
+                var buf = new byte[1024];
+                var res = await ws.ReceiveAsync(new ArraySegment<byte>(buf), CancellationToken.None);
+                var msg = Encoding.UTF8.GetString(buf, 0, res.Count);
+                var reply = Encoding.UTF8.GetBytes("DOWNSTREAM-WS-ECHO:" + msg);
+                await ws.SendAsync(new ArraySegment<byte>(reply), WebSocketMessageType.Text, true, CancellationToken.None);
+                await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "bye", CancellationToken.None);
+                return;
+            }
+            await next();
+        });
+        down.MapGet("/{**rest}", () => "DOWNSTREAM-HTTP-OK");
+        _ = down.RunAsync();
+
+        // Ocelot gateway on 127.0.0.1:6000, route blocks 127.0.0.1
+        var cfg = new FileConfiguration();
+        cfg.Routes.Add(new FileRoute
+        {
+            UpstreamPathTemplate = "/proxy/{everything}",
+            UpstreamHttpMethod = ["GET"],
+            DownstreamPathTemplate = "/{everything}",
+            DownstreamScheme = "http",
+            DownstreamHostAndPorts = [ new(downstream.Host, downstream.Port) ],
+            SecurityOptions = new() { IPBlockedList = [downstream.Host] },
+        });
+
+        var ocelotPort = PortFinder.GetRandomPort();
+        var gwBuilder = WebApplication.CreateBuilder();
+        gwBuilder.WebHost.UseUrls($"http://127.0.0.1:{ocelotPort}");
+        gwBuilder.Configuration.AddOcelot(cfg);
+        gwBuilder.Services.AddOcelot(gwBuilder.Configuration);
+        var gw = gwBuilder.Build();
+        await gw.UseOcelot();
+        _ = gw.RunAsync();
+        await Task.Delay(3000, CancelMe);
+
+        // 1) Normal HTTP request from the blocked IP
+        using var http = new HttpClient();
+        var r = await http.GetAsync($"http://127.0.0.1:{ocelotPort}/proxy/anything", CancelMe);
+
+        // Console.WriteLine($"[HTTP] status={(int)r.StatusCode}");
+        r.StatusCode.ShouldBe(HttpStatusCode.Forbidden);
+
+        // 2) WebSocket upgrade request from the same blocked IP
+        using var wsc = new ClientWebSocket();
+
+        // await wsc.ConnectAsync(new Uri($"ws://127.0.0.1:{ocelotPort}/proxy/anything"), CancelMe);
+        var ex = await Record.ExceptionAsync(()
+            => wsc.ConnectAsync(new Uri($"ws://127.0.0.1:{ocelotPort}/proxy/anything"), CancelMe));
+        ex.ShouldBeOfType<WebSocketException>();
+        ex.Message.ShouldContain("403");
+        ex.Message.ShouldBe("The server returned status code '403' when status code '101' was expected.");
+
+        //var send = Encoding.UTF8.GetBytes("hello-from-blocked-ip");
+        //await wsc.SendAsync(new ArraySegment<byte>(send), WebSocketMessageType.Text, true, CancelMe);
+        //var rbuf = new byte[1024];
+        //var rres = await wsc.ReceiveAsync(new ArraySegment<byte>(rbuf), CancelMe);
+        //Console.WriteLine($"[WS]   reply=\"{Encoding.UTF8.GetString(rbuf, 0, rres.Count)}\"");
+    }
 
     [Fact]
     [Trait("Bug", "2403")] // https://github.com/ThreeMammals/Ocelot/issues/2403
