5b155e4a4f4ac403427aaad4aa93337686bc42e9
Raman Maksimchuk <dotnet044@gmail.com>
2026-06-30 21:58:58 +0300

    Refactor MultiplexingMiddleware by @raman-m

--- src/Multiplexer/MultiplexingMiddleware.cs
+++ src/Multiplexer/MultiplexingMiddleware.cs
@@ -26,47 +26,39 @@ public class MultiplexingMiddleware : OcelotMiddleware
         _next = next;
     }
 
-    public async Task Invoke(HttpContext httpContext)
+    public async Task Invoke(HttpContext context)
     {
-        var downstreamRouteHolder = httpContext.Items.DownstreamRouteHolder();
+        var downstreamRouteHolder = context.Items.DownstreamRouteHolder();
         var route = downstreamRouteHolder.Route;
         var downstreamRoutes = route.DownstreamRoute;
 
         // Case 1: if websocket request or single downstream route
-        if (ShouldProcessSingleRoute(httpContext, downstreamRoutes))
+        if (ShouldProcessSingleRoute(context, downstreamRoutes))
         {
-            await ProcessSingleRouteAsync(httpContext, downstreamRoutes[0]);
+            await ProcessSingleRouteAsync(context, downstreamRoutes[0]);
             return;
         }
 
         // Case 2: if no downstream routes
         if (downstreamRoutes.Count == 0)
-        {
             return;
-        }
 
         // Case 3: if multiple downstream routes
         var routeKeysConfigs = route.DownstreamRouteConfig;
         if (routeKeysConfigs == null || routeKeysConfigs.Count == 0)
         {
-            await ProcessRoutesAsync(httpContext, route);
+            await ProcessRoutesAsync(context, route);
             return;
         }
 
         // Case 4: if multiple downstream routes with route keys
-        var mainResponseContext = await ProcessMainRouteAsync(httpContext, downstreamRoutes[0]);
-        if (mainResponseContext == null)
-        {
-            return;
-        }
-
-        var responsesContexts = await ProcessRoutesWithRouteKeysAsync(httpContext, downstreamRoutes, routeKeysConfigs, mainResponseContext);
+        var mainResponseContext = context;
+        await ProcessMainRouteAsync(context, downstreamRoutes[0]);
+        var responsesContexts = await ProcessRoutesWithRouteKeysAsync(context, downstreamRoutes, routeKeysConfigs, mainResponseContext);
         if (responsesContexts.Length == 0)
-        {
             return;
-        }
 
-        await MapResponsesAsync(httpContext, route, mainResponseContext, responsesContexts);
+        await MapResponsesAsync(context, route, mainResponseContext, responsesContexts);
     }
 
     /// <summary>
@@ -113,11 +105,10 @@ public class MultiplexingMiddleware : OcelotMiddleware
     /// <param name="context">The http context.</param>
     /// <param name="route">The first route, the main route.</param>
     /// <returns>The updated http context.</returns>
-    private async Task<HttpContext> ProcessMainRouteAsync(HttpContext context, DownstreamRoute route)
+    private Task ProcessMainRouteAsync(HttpContext context, DownstreamRoute route)
     {
         context.Items.UpsertDownstreamRoute(route);
-        await _next.Invoke(context);
-        return context;
+        return _next.Invoke(context);
     }
 
     /// <summary>
@@ -140,10 +131,11 @@ public class MultiplexingMiddleware : OcelotMiddleware
             if (matchAdvancedAgg != null)
             {
                 processing.AddRange(ProcessRouteWithComplexAggregation(matchAdvancedAgg, jObject, context, downstreamRoute));
-                continue;
             }
-
-            processing.Add(ProcessRouteAsync(context, downstreamRoute));
+            else
+            {
+                processing.Add(ProcessRouteAsync(context, downstreamRoute));
+            }
         }
 
         return await Task.WhenAll(processing);
@@ -169,9 +161,10 @@ public class MultiplexingMiddleware : OcelotMiddleware
         var values = jObject.SelectTokens(matchAdvancedAgg.JsonPath).Select(s => s.ToString()).Distinct();
         foreach (var value in values)
         {
-            var lPnv = new List<PlaceholderNameAndValue>(context.Items.TemplatePlaceholderNameAndValues())
+            var tPnv = context.Items.TemplatePlaceholderNameAndValues();
+            var lPnv = new List<PlaceholderNameAndValue>(tPnv)
             {
-                new(matchAdvancedAgg.Parameter, value, true),
+                new(matchAdvancedAgg.Parameter, value, true)
             };
             processing.Add(ProcessRouteAsync(context, route, lPnv));
         }
@@ -200,8 +193,7 @@ public class MultiplexingMiddleware : OcelotMiddleware
     {
         target.Items.Add(RequestIdString, source.Items[RequestIdString]);
         target.Items.SetIInternalConfiguration(source.Items.IInternalConfiguration());
-        target.Items.UpsertTemplatePlaceholderNameAndValues(placeholders ??
-                                                            source.Items.TemplatePlaceholderNameAndValues());
+        target.Items.UpsertTemplatePlaceholderNameAndValues(placeholders ?? source.Items.TemplatePlaceholderNameAndValues());
     }
 
     /// <summary>
