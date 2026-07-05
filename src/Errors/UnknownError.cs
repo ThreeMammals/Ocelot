66599cffb531ee6a649ac0814159874cc16cb865
Raman Maksimchuk <dotnet044@gmail.com>
2026-07-05 16:05:11 +0300

    Delete OcelotError in favor of UnknownError

--- src/Errors/UnknownError.cs
+++ src/Errors/UnknownError.cs
@@ -1,8 +1,18 @@
-﻿namespace Ocelot.Errors;
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Ocelot.Errors;
 
 public class UnknownError : Error
 {
+    public UnknownError()
+        : base(string.Empty, OcelotErrorCode.UnknownError, StatusCodes.Status500InternalServerError)
+    { }
+
     public UnknownError(string message)
-        : base(message, OcelotErrorCode.UnknownError, 0)
+        : base(message, OcelotErrorCode.UnknownError, StatusCodes.Status500InternalServerError)
+    { }
+
+    public UnknownError(Exception exception)
+        : base(exception, OcelotErrorCode.UnknownError, StatusCodes.Status500InternalServerError)
     { }
 }
