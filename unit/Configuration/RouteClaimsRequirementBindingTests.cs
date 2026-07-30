d1f2fca6e979a8d96a5743206212048cf14bd4fc
Ommmarr111 <ommmarr111@gmail.com>
2026-07-30 20:33:26 +0300

    Added RouteClaimsRequirementPostConfigureOptions coverage tests

--- unit/Configuration/RouteClaimsRequirementBindingTests.cs
+++ unit/Configuration/RouteClaimsRequirementBindingTests.cs
@@ -10,6 +10,43 @@ namespace Ocelot.UnitTests.Configuration;
 public class RouteClaimsRequirementBindingTests
 {
     [Fact]
+    public void PostConfigure_NullOptions_DoesNothing()
+    {
+        var configuration = new ConfigurationBuilder().Build();
+        var sut = new RouteClaimsRequirementPostConfigureOptions(configuration);
+
+        // Should not throw
+        sut.PostConfigure(null, null);
+    }
+
+    [Fact]
+    public void PostConfigure_RouteWithoutClaimsRequirement_LeavesRouteUntouched()
+    {
+        const string json = """
+    {
+      "Routes": [
+        {
+          "DownstreamPathTemplate": "/",
+          "DownstreamScheme": "http",
+          "UpstreamPathTemplate": "/"
+        }
+      ]
+    }
+    """;
+        var configuration = new ConfigurationBuilder()
+            .AddJsonStream(new MemoryStream(Encoding.UTF8.GetBytes(json)))
+            .Build();
+        var services = new ServiceCollection();
+        services.Configure<FileConfiguration>(configuration);
+        services.AddSingleton<IPostConfigureOptions<FileConfiguration>>(
+            new RouteClaimsRequirementPostConfigureOptions(configuration));
+        var provider = services.BuildServiceProvider();
+
+        var fileConfig = provider.GetRequiredService<IOptions<FileConfiguration>>().Value;
+
+        fileConfig.Routes[0].RouteClaimsRequirement.ShouldBeEmpty();
+    }
+    [Fact]
     public void ShouldPreserveColonInRouteClaimsRequirementKey()
     {
         // Arrange
@@ -43,5 +80,24 @@ public class RouteClaimsRequirementBindingTests
         // Assert
         Assert.True(requirement.ContainsKey("http://schemas.microsoft.com/ws/2008/06/identity/claims/role"));
         Assert.Equal("Admin", requirement["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"]);
+    }
+
+    [Fact]
+    public void PostConfigure_NullRoutes_InitializesRoutes()
+    {
+        // Arrange
+        var configuration = new ConfigurationBuilder().Build();
+        var sut = new RouteClaimsRequirementPostConfigureOptions(configuration);
+        var fileConfig = new FileConfiguration
+        {
+            Routes = null,
+        };
+
+        // Act
+        sut.PostConfigure(null, fileConfig);
+
+        // Assert
+        fileConfig.Routes.ShouldNotBeNull();
+        fileConfig.Routes.ShouldBeEmpty();
     }
 }
